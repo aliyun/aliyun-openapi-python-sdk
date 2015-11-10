16b260aba5d37a46bafe94a7f794f81ca4a968ea
malijiefoxmail <malijie@foxmail.com>
2015-11-10 17:42:18 +0800

    publish mts sdk
    Signed-off-by: malijiefoxmail <malijie@foxmail.com>

---
+++ aliyun-python-sdk-mts/aliyunsdkmts/request/v20140618/SubmitJobsRequest.py
@@ -0,0 +1,78 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class SubmitJobsRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Mts', '2014-06-18', 'SubmitJobs')
+
+	def get_OwnerId(self):
+		return self.get_query_params().get('OwnerId')
+
+	def set_OwnerId(self,OwnerId):
+		self.add_query_param('OwnerId',OwnerId)
+
+	def get_ResourceOwnerAccount(self):
+		return self.get_query_params().get('ResourceOwnerAccount')
+
+	def set_ResourceOwnerAccount(self,ResourceOwnerAccount):
+		self.add_query_param('ResourceOwnerAccount',ResourceOwnerAccount)
+
+	def get_ResourceOwnerId(self):
+		return self.get_query_params().get('ResourceOwnerId')
+
+	def set_ResourceOwnerId(self,ResourceOwnerId):
+		self.add_query_param('ResourceOwnerId',ResourceOwnerId)
+
+	def get_Input(self):
+		return self.get_query_params().get('Input')
+
+	def set_Input(self,Input):
+		self.add_query_param('Input',Input)
+
+	def get_Outputs(self):
+		return self.get_query_params().get('Outputs')
+
+	def set_Outputs(self,Outputs):
+		self.add_query_param('Outputs',Outputs)
+
+	def get_OutputBucket(self):
+		return self.get_query_params().get('OutputBucket')
+
+	def set_OutputBucket(self,OutputBucket):
+		self.add_query_param('OutputBucket',OutputBucket)
+
+	def get_OutputLocation(self):
+		return self.get_query_params().get('OutputLocation')
+
+	def set_OutputLocation(self,OutputLocation):
+		self.add_query_param('OutputLocation',OutputLocation)
+
+	def get_PipelineId(self):
+		return self.get_query_params().get('PipelineId')
+
+	def set_PipelineId(self,PipelineId):
+		self.add_query_param('PipelineId',PipelineId)
+
+	def get_OwnerAccount(self):
+		return self.get_query_params().get('OwnerAccount')
+
+	def set_OwnerAccount(self,OwnerAccount):
+		self.add_query_param('OwnerAccount',OwnerAccount)
---
+++ aliyun-python-sdk-mts/aliyunsdkmts/request/v20140618/UpdateTemplateRequest.py
@@ -0,0 +1,90 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class UpdateTemplateRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Mts', '2014-06-18', 'UpdateTemplate')
+
+	def get_OwnerId(self):
+		return self.get_query_params().get('OwnerId')
+
+	def set_OwnerId(self,OwnerId):
+		self.add_query_param('OwnerId',OwnerId)
+
+	def get_ResourceOwnerAccount(self):
+		return self.get_query_params().get('ResourceOwnerAccount')
+
+	def set_ResourceOwnerAccount(self,ResourceOwnerAccount):
+		self.add_query_param('ResourceOwnerAccount',ResourceOwnerAccount)
+
+	def get_ResourceOwnerId(self):
+		return self.get_query_params().get('ResourceOwnerId')
+
+	def set_ResourceOwnerId(self,ResourceOwnerId):
+		self.add_query_param('ResourceOwnerId',ResourceOwnerId)
+
+	def get_TemplateId(self):
+		return self.get_query_params().get('TemplateId')
+
+	def set_TemplateId(self,TemplateId):
+		self.add_query_param('TemplateId',TemplateId)
+
+	def get_Name(self):
+		return self.get_query_params().get('Name')
+
+	def set_Name(self,Name):
+		self.add_query_param('Name',Name)
+
+	def get_Container(self):
+		return self.get_query_params().get('Container')
+
+	def set_Container(self,Container):
+		self.add_query_param('Container',Container)
+
+	def get_Video(self):
+		return self.get_query_params().get('Video')
+
+	def set_Video(self,Video):
+		self.add_query_param('Video',Video)
+
+	def get_Audio(self):
+		return self.get_query_params().get('Audio')
+
+	def set_Audio(self,Audio):
+		self.add_query_param('Audio',Audio)
+
+	def get_MuxConfig(self):
+		return self.get_query_params().get('MuxConfig')
+
+	def set_MuxConfig(self,MuxConfig):
+		self.add_query_param('MuxConfig',MuxConfig)
+
+	def get_TransConfig(self):
+		return self.get_query_params().get('TransConfig')
+
+	def set_TransConfig(self,TransConfig):
+		self.add_query_param('TransConfig',TransConfig)
+
+	def get_OwnerAccount(self):
+		return self.get_query_params().get('OwnerAccount')
+
+	def set_OwnerAccount(self,OwnerAccount):
+		self.add_query_param('OwnerAccount',OwnerAccount)
