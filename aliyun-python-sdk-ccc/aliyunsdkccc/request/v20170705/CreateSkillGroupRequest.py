a40c6c18ac48934420f26db3b942556fe6a736eb
haowei.yao <haowei.yao@alibaba-inc.com>
2017-12-18 07:45:47 +0800

    由择仁发起的CCC SDK自动发布, BUILD_ID=281, 版本号：1.0.0 发布日志： 1, This is the first release of CCC Open API.

---
+++ aliyun-python-sdk-ccc/aliyunsdkccc/request/v20170705/CreateSkillGroupRequest.py
@@ -0,0 +1,66 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class CreateSkillGroupRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'CCC', '2017-07-05', 'CreateSkillGroup','ccc')
+
+	def get_SkillLevels(self):
+		return self.get_query_params().get('SkillLevels')
+
+	def set_SkillLevels(self,SkillLevels):
+		for i in range(len(SkillLevels)):	
+			if SkillLevels[i] is not None:
+				self.add_query_param('SkillLevel.' + bytes(i + 1) , SkillLevels[i]);
+
+	def get_InstanceId(self):
+		return self.get_query_params().get('InstanceId')
+
+	def set_InstanceId(self,InstanceId):
+		self.add_query_param('InstanceId',InstanceId)
+
+	def get_OutboundPhoneNumberIds(self):
+		return self.get_query_params().get('OutboundPhoneNumberIds')
+
+	def set_OutboundPhoneNumberIds(self,OutboundPhoneNumberIds):
+		for i in range(len(OutboundPhoneNumberIds)):	
+			if OutboundPhoneNumberIds[i] is not None:
+				self.add_query_param('OutboundPhoneNumberId.' + bytes(i + 1) , OutboundPhoneNumberIds[i]);
+
+	def get_Name(self):
+		return self.get_query_params().get('Name')
+
+	def set_Name(self,Name):
+		self.add_query_param('Name',Name)
+
+	def get_Description(self):
+		return self.get_query_params().get('Description')
+
+	def set_Description(self,Description):
+		self.add_query_param('Description',Description)
+
+	def get_UserIds(self):
+		return self.get_query_params().get('UserIds')
+
+	def set_UserIds(self,UserIds):
+		for i in range(len(UserIds)):	
+			if UserIds[i] is not None:
+				self.add_query_param('UserId.' + bytes(i + 1) , UserIds[i]);
---
+++ aliyun-python-sdk-ccc/aliyunsdkccc/request/v20170705/CreateUserRequest.py
@@ -0,0 +1,78 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class CreateUserRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'CCC', '2017-07-05', 'CreateUser','ccc')
+
+	def get_SkillLevels(self):
+		return self.get_query_params().get('SkillLevels')
+
+	def set_SkillLevels(self,SkillLevels):
+		for i in range(len(SkillLevels)):	
+			if SkillLevels[i] is not None:
+				self.add_query_param('SkillLevel.' + bytes(i + 1) , SkillLevels[i]);
+
+	def get_InstanceId(self):
+		return self.get_query_params().get('InstanceId')
+
+	def set_InstanceId(self,InstanceId):
+		self.add_query_param('InstanceId',InstanceId)
+
+	def get_LoginName(self):
+		return self.get_query_params().get('LoginName')
+
+	def set_LoginName(self,LoginName):
+		self.add_query_param('LoginName',LoginName)
+
+	def get_Phone(self):
+		return self.get_query_params().get('Phone')
+
+	def set_Phone(self,Phone):
+		self.add_query_param('Phone',Phone)
+
+	def get_RoleIds(self):
+		return self.get_query_params().get('RoleIds')
+
+	def set_RoleIds(self,RoleIds):
+		for i in range(len(RoleIds)):	
+			if RoleIds[i] is not None:
+				self.add_query_param('RoleId.' + bytes(i + 1) , RoleIds[i]);
+
+	def get_DisplayName(self):
+		return self.get_query_params().get('DisplayName')
+
+	def set_DisplayName(self,DisplayName):
+		self.add_query_param('DisplayName',DisplayName)
+
+	def get_SkillGroupIds(self):
+		return self.get_query_params().get('SkillGroupIds')
+
+	def set_SkillGroupIds(self,SkillGroupIds):
+		for i in range(len(SkillGroupIds)):	
+			if SkillGroupIds[i] is not None:
+				self.add_query_param('SkillGroupId.' + bytes(i + 1) , SkillGroupIds[i]);
+
+	def get_Email(self):
+		return self.get_query_params().get('Email')
+
+	def set_Email(self,Email):
+		self.add_query_param('Email',Email)
