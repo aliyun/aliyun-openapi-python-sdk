6dfc463c54052d36925b1cf1d36b7434d682777e
haowei.yao <haowei.yao@alibaba-inc.com>
2018-01-22 17:35:37 +0800

    由颍川发起的CSB SDK自动发布, BUILD_ID=359, 版本号：1.1.2 发布日志： 1, publish Project API. 2, publish Service API. 3, publish Service order API. 4, publish Credential API. 5, publish CAS API.

---
+++ aliyun-python-sdk-csb/aliyunsdkcsb/request/v20171118/DeleteUnionCasServiceRequest.py
@@ -0,0 +1,50 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class DeleteUnionCasServiceRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'CSB', '2017-11-18', 'DeleteUnionCasService','CSB')
+		self.set_protocol_type('https');
+		self.set_method('POST')
+
+	def get_LeafOnly(self):
+		return self.get_query_params().get('LeafOnly')
+
+	def set_LeafOnly(self,LeafOnly):
+		self.add_query_param('LeafOnly',LeafOnly)
+
+	def get_CasCsbName(self):
+		return self.get_query_params().get('CasCsbName')
+
+	def set_CasCsbName(self,CasCsbName):
+		self.add_query_param('CasCsbName',CasCsbName)
+
+	def get_SrcUserId(self):
+		return self.get_query_params().get('SrcUserId')
+
+	def set_SrcUserId(self,SrcUserId):
+		self.add_query_param('SrcUserId',SrcUserId)
+
+	def get_CasServiceId(self):
+		return self.get_query_params().get('CasServiceId')
+
+	def set_CasServiceId(self,CasServiceId):
+		self.add_query_param('CasServiceId',CasServiceId)
