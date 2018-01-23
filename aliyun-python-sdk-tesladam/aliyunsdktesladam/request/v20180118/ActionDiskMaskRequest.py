c0c58ea60d1c07b752188b06c716e929b17a78aa
haowei.yao <haowei.yao@alibaba-inc.com>
2018-01-23 14:33:57 +0800

    由荣旸发起的TESLADAM SDK自动发布, BUILD_ID=272, 版本号：1.0.0 发布日志： 1, Tesla Dam API release. 2, Add ActionDiskCheck, ActionDiskMask, ActionDiskRma, HostGets.

---
+++ aliyun-python-sdk-tesladam/aliyunsdktesladam/request/v20180118/ActionDiskMaskRequest.py
@@ -0,0 +1,42 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class ActionDiskMaskRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'TeslaDam', '2018-01-18', 'ActionDiskMask')
+
+	def get_Op(self):
+		return self.get_query_params().get('Op')
+
+	def set_Op(self,Op):
+		self.add_query_param('Op',Op)
+
+	def get_DiskMount(self):
+		return self.get_query_params().get('DiskMount')
+
+	def set_DiskMount(self,DiskMount):
+		self.add_query_param('DiskMount',DiskMount)
+
+	def get_Ip(self):
+		return self.get_query_params().get('Ip')
+
+	def set_Ip(self,Ip):
+		self.add_query_param('Ip',Ip)
