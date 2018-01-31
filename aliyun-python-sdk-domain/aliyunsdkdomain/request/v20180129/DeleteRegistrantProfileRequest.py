6a2e5d3eceec1119902b0941adcd5dc07c412b95
haowei.yao <haowei.yao@alibaba-inc.com>
2018-01-31 18:37:03 +0800

    由洛衡发起的DOMAIN SDK自动发布, BUILD_ID=380, 版本号：3.0.0 发布日志： 1, New version SDK for domains, standardizing api name. 2, Provide api for domain registration and management.

---
+++ aliyun-python-sdk-domain/aliyunsdkdomain/request/v20180129/DeleteRegistrantProfileRequest.py
@@ -0,0 +1,42 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class DeleteRegistrantProfileRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Domain', '2018-01-29', 'DeleteRegistrantProfile')
+
+	def get_UserClientIp(self):
+		return self.get_query_params().get('UserClientIp')
+
+	def set_UserClientIp(self,UserClientIp):
+		self.add_query_param('UserClientIp',UserClientIp)
+
+	def get_RegistrantProfileId(self):
+		return self.get_query_params().get('RegistrantProfileId')
+
+	def set_RegistrantProfileId(self,RegistrantProfileId):
+		self.add_query_param('RegistrantProfileId',RegistrantProfileId)
+
+	def get_Lang(self):
+		return self.get_query_params().get('Lang')
+
+	def set_Lang(self,Lang):
+		self.add_query_param('Lang',Lang)
---
+++ aliyun-python-sdk-domain/aliyunsdkdomain/request/v20180129/SaveBatchTaskForCreatingOrderRenewRequest.py
@@ -0,0 +1,49 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class SaveBatchTaskForCreatingOrderRenewRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Domain', '2018-01-29', 'SaveBatchTaskForCreatingOrderRenew')
+
+	def get_UserClientIp(self):
+		return self.get_query_params().get('UserClientIp')
+
+	def set_UserClientIp(self,UserClientIp):
+		self.add_query_param('UserClientIp',UserClientIp)
+
+	def get_OrderRenewParams(self):
+		return self.get_query_params().get('OrderRenewParams')
+
+	def set_OrderRenewParams(self,OrderRenewParams):
+		for i in range(len(OrderRenewParams)):	
+			if OrderRenewParams[i].get('DomainName') is not None:
+				self.add_query_param('OrderRenewParam.' + str(i + 1) + '.DomainName' , OrderRenewParams[i].get('DomainName'))
+			if OrderRenewParams[i].get('CurrentExpirationDate') is not None:
+				self.add_query_param('OrderRenewParam.' + str(i + 1) + '.CurrentExpirationDate' , OrderRenewParams[i].get('CurrentExpirationDate'))
+			if OrderRenewParams[i].get('SubscriptionDuration') is not None:
+				self.add_query_param('OrderRenewParam.' + str(i + 1) + '.SubscriptionDuration' , OrderRenewParams[i].get('SubscriptionDuration'))
+
+
+	def get_Lang(self):
+		return self.get_query_params().get('Lang')
+
+	def set_Lang(self,Lang):
+		self.add_query_param('Lang',Lang)
