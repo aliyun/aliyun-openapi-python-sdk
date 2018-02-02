1f235d4a9156baa1f94f4727070f6183aeec6128
haowei.yao <haowei.yao@alibaba-inc.com>
2018-02-02 10:54:41 +0800

    由谦正发起的EHPC SDK自动发布, BUILD_ID=384, 版本号：1.1.0 发布日志： 1, Allow passing "ImageId" in CreateCluster and AddNodes 2, Provide more information in ListClusters, ListNodes and DescribeCluster

---
+++ aliyun-python-sdk-ehpc/aliyunsdkehpc/request/v20170714/AddNodesRequest.py
@@ -0,0 +1,60 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class AddNodesRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'EHPC', '2017-07-14', 'AddNodes','ehs')
+
+	def get_ImageId(self):
+		return self.get_query_params().get('ImageId')
+
+	def set_ImageId(self,ImageId):
+		self.add_query_param('ImageId',ImageId)
+
+	def get_Count(self):
+		return self.get_query_params().get('Count')
+
+	def set_Count(self,Count):
+		self.add_query_param('Count',Count)
+
+	def get_ClusterId(self):
+		return self.get_query_params().get('ClusterId')
+
+	def set_ClusterId(self,ClusterId):
+		self.add_query_param('ClusterId',ClusterId)
+
+	def get_ComputeSpotStrategy(self):
+		return self.get_query_params().get('ComputeSpotStrategy')
+
+	def set_ComputeSpotStrategy(self,ComputeSpotStrategy):
+		self.add_query_param('ComputeSpotStrategy',ComputeSpotStrategy)
+
+	def get_ComputeSpotPriceLimit(self):
+		return self.get_query_params().get('ComputeSpotPriceLimit')
+
+	def set_ComputeSpotPriceLimit(self,ComputeSpotPriceLimit):
+		self.add_query_param('ComputeSpotPriceLimit',ComputeSpotPriceLimit)
+
+	def get_ImageOwnerAlias(self):
+		return self.get_query_params().get('ImageOwnerAlias')
+
+	def set_ImageOwnerAlias(self,ImageOwnerAlias):
+		self.add_query_param('ImageOwnerAlias',ImageOwnerAlias)
