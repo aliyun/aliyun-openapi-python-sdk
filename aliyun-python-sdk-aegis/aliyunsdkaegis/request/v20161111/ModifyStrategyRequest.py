9ce4a9d91529f3915452b2028c4af8fd318fa50f
haowei.yao <haowei.yao@alibaba-inc.com>
2018-02-07 15:01:45 +0800

    由君石发起的AEGIS SDK自动发布, BUILD_ID=397, 版本号：1.0.1 发布日志： 1, add the access interface of logs such as vulnerabilities, baselines, exceptions, etc.

---
+++ aliyun-python-sdk-aegis/aliyunsdkaegis/request/v20161111/ModifyStrategyRequest.py
@@ -0,0 +1,67 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class ModifyStrategyRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'aegis', '2016-11-11', 'ModifyStrategy','vipaegis')
+		self.set_method('POST')
+
+	def get_ResourceOwnerId(self):
+		return self.get_query_params().get('ResourceOwnerId')
+
+	def set_ResourceOwnerId(self,ResourceOwnerId):
+		self.add_query_param('ResourceOwnerId',ResourceOwnerId)
+
+	def get_RiskSubTypeName(self):
+		return self.get_query_params().get('RiskSubTypeName')
+
+	def set_RiskSubTypeName(self,RiskSubTypeName):
+		self.add_query_param('RiskSubTypeName',RiskSubTypeName)
+
+	def get_SourceIp(self):
+		return self.get_query_params().get('SourceIp')
+
+	def set_SourceIp(self,SourceIp):
+		self.add_query_param('SourceIp',SourceIp)
+
+	def get_CycleStartTime(self):
+		return self.get_query_params().get('CycleStartTime')
+
+	def set_CycleStartTime(self,CycleStartTime):
+		self.add_query_param('CycleStartTime',CycleStartTime)
+
+	def get_Name(self):
+		return self.get_query_params().get('Name')
+
+	def set_Name(self,Name):
+		self.add_query_param('Name',Name)
+
+	def get_CycleDays(self):
+		return self.get_query_params().get('CycleDays')
+
+	def set_CycleDays(self,CycleDays):
+		self.add_query_param('CycleDays',CycleDays)
+
+	def get_Id(self):
+		return self.get_query_params().get('Id')
+
+	def set_Id(self,Id):
+		self.add_query_param('Id',Id)
