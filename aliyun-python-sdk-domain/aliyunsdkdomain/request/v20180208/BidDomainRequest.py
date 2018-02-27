9eb35490cc201644f7ba8fc21f9c66fca4cf9244
haowei.yao <haowei.yao@alibaba-inc.com>
2018-02-27 12:17:56 +0800

    由铁琴发起的DOMAIN SDK自动发布, BUILD_ID=444, 版本号：3.1.0 发布日志： 1, Add auction APIs. 2, Add return field DomainNameVerificationStatus to QueryDomainByInstanceId and QueryFailReasonForDomainRealNameVerification.

---
+++ aliyun-python-sdk-domain/aliyunsdkdomain/request/v20180208/BidDomainRequest.py
@@ -0,0 +1,42 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class BidDomainRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Domain', '2018-02-08', 'BidDomain')
+
+	def get_AuctionId(self):
+		return self.get_body_params().get('AuctionId')
+
+	def set_AuctionId(self,AuctionId):
+		self.add_body_params('AuctionId', AuctionId)
+
+	def get_MaxBid(self):
+		return self.get_body_params().get('MaxBid')
+
+	def set_MaxBid(self,MaxBid):
+		self.add_body_params('MaxBid', MaxBid)
+
+	def get_Currency(self):
+		return self.get_body_params().get('Currency')
+
+	def set_Currency(self,Currency):
+		self.add_body_params('Currency', Currency)
