829bc8b13b4a69f1880e730e3a5fcf07c33e1003
haowei.yao <haowei.yao@alibaba-inc.com>
2018-04-03 14:24:38 +0800

    由洛衡发起的DOMAIN SDK自动发布, 版本号：3.3.0 发布日志： 1, Add APIs for domain transfer in and transfer out. 2, Add APIs for poll and acknowledge domain task. 3, Add API for query domain group list.

---
+++ aliyun-python-sdk-domain/aliyunsdkdomain/request/v20180129/SaveSingleTaskForUpdatingContactInfoRequest.py
@@ -0,0 +1,66 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class SaveSingleTaskForUpdatingContactInfoRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Domain', '2018-01-29', 'SaveSingleTaskForUpdatingContactInfo')
+
+	def get_InstanceId(self):
+		return self.get_query_params().get('InstanceId')
+
+	def set_InstanceId(self,InstanceId):
+		self.add_query_param('InstanceId',InstanceId)
+
+	def get_ContactType(self):
+		return self.get_query_params().get('ContactType')
+
+	def set_ContactType(self,ContactType):
+		self.add_query_param('ContactType',ContactType)
+
+	def get_UserClientIp(self):
+		return self.get_query_params().get('UserClientIp')
+
+	def set_UserClientIp(self,UserClientIp):
+		self.add_query_param('UserClientIp',UserClientIp)
+
+	def get_DomainName(self):
+		return self.get_query_params().get('DomainName')
+
+	def set_DomainName(self,DomainName):
+		self.add_query_param('DomainName',DomainName)
+
+	def get_RegistrantProfileId(self):
+		return self.get_query_params().get('RegistrantProfileId')
+
+	def set_RegistrantProfileId(self,RegistrantProfileId):
+		self.add_query_param('RegistrantProfileId',RegistrantProfileId)
+
+	def get_AddTransferLock(self):
+		return self.get_query_params().get('AddTransferLock')
+
+	def set_AddTransferLock(self,AddTransferLock):
+		self.add_query_param('AddTransferLock',AddTransferLock)
+
+	def get_Lang(self):
+		return self.get_query_params().get('Lang')
+
+	def set_Lang(self,Lang):
+		self.add_query_param('Lang',Lang)
