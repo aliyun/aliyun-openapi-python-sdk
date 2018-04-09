e6058cad072c3209dcc187962fd544fbca3c9c32
haowei.yao <haowei.yao@alibaba-inc.com>
2018-04-09 17:22:31 +0800

    由向召元发起的CMS SDK自动发布, 版本号：6.0.1 发布日志： 1, Add api: QueryMetricData, QueryMetricTop, TaskConfigCreate, TaskConfigDelete, TaskConfigEnable, TaskConfigList, TaskConfigModify, TaskConfigUnhealthy 2, Remove api: PutSystemEvent

---
+++ aliyun-python-sdk-cms/aliyunsdkcms/request/v20180308/DisableActiveAlertRequest.py
@@ -0,0 +1,36 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class DisableActiveAlertRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Cms', '2018-03-08', 'DisableActiveAlert','cms')
+
+	def get_Product(self):
+		return self.get_query_params().get('Product')
+
+	def set_Product(self,Product):
+		self.add_query_param('Product',Product)
+
+	def get_UserId(self):
+		return self.get_query_params().get('UserId')
+
+	def set_UserId(self,UserId):
+		self.add_query_param('UserId',UserId)
