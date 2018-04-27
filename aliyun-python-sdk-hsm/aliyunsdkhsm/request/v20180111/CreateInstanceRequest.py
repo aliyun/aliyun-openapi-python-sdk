1f3ff0358bec04bcc26167ec3eb7a131b3c8e5eb
haowei.yao <haowei.yao@alibaba-inc.com>
2018-04-27 19:39:08 +0800

    由鸿逸发起的HSM SDK自动发布, 版本号：1.0.0 发布日志： 1, release hsm open api 2, hsm open api includes DescribeRegions, DescribeInstances, ModityInstance, ConfigNetwork, ConfigWhiteList

---
+++ aliyun-python-sdk-hsm/aliyunsdkhsm/request/v20180111/CreateInstanceRequest.py
@@ -0,0 +1,73 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class CreateInstanceRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'hsm', '2018-01-11', 'CreateInstance','hsm')
+		self.set_method('POST')
+
+	def get_Period(self):
+		return self.get_query_params().get('Period')
+
+	def set_Period(self,Period):
+		self.add_query_param('Period',Period)
+
+	def get_PeriodUnit(self):
+		return self.get_query_params().get('PeriodUnit')
+
+	def set_PeriodUnit(self,PeriodUnit):
+		self.add_query_param('PeriodUnit',PeriodUnit)
+
+	def get_ResourceOwnerId(self):
+		return self.get_query_params().get('ResourceOwnerId')
+
+	def set_ResourceOwnerId(self,ResourceOwnerId):
+		self.add_query_param('ResourceOwnerId',ResourceOwnerId)
+
+	def get_Quantity(self):
+		return self.get_query_params().get('Quantity')
+
+	def set_Quantity(self,Quantity):
+		self.add_query_param('Quantity',Quantity)
+
+	def get_HsmDeviceType(self):
+		return self.get_query_params().get('HsmDeviceType')
+
+	def set_HsmDeviceType(self,HsmDeviceType):
+		self.add_query_param('HsmDeviceType',HsmDeviceType)
+
+	def get_ClientToken(self):
+		return self.get_query_params().get('ClientToken')
+
+	def set_ClientToken(self,ClientToken):
+		self.add_query_param('ClientToken',ClientToken)
+
+	def get_ZoneId(self):
+		return self.get_query_params().get('ZoneId')
+
+	def set_ZoneId(self,ZoneId):
+		self.add_query_param('ZoneId',ZoneId)
+
+	def get_HsmOem(self):
+		return self.get_query_params().get('HsmOem')
+
+	def set_HsmOem(self,HsmOem):
+		self.add_query_param('HsmOem',HsmOem)
