4d5e4ca98bb7f78a8a1b2203c277fb0a8d8dfb9b
haowei.yao <haowei.yao@alibaba-inc.com>
2018-05-23 11:36:46 +0800

    由谦正发起的EHPC SDK自动发布, 版本号：1.2.0 发布日志： 1, Update API version to 2018-04-12. 2, New APIs in this new version: StartCluster, StopCluster, RecoverCluster, StartNodes, StopNodes, ListCustomImages. Support stopping a post-paid cluster or partial nodes of a cluster to save cost. 3, New parameters for CreateCluster API: EcsChargeType. Support creating a pre-paid cluster. 4, New parameters for CreateCluster API: DeploymentMode. Support creating a cluster with fewer manager nodes.

---
+++ aliyun-python-sdk-ehpc/aliyunsdkehpc/request/v20180412/StartNodesRequest.py
@@ -0,0 +1,45 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class StartNodesRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'EHPC', '2018-04-12', 'StartNodes','ehs')
+
+	def get_Role(self):
+		return self.get_query_params().get('Role')
+
+	def set_Role(self,Role):
+		self.add_query_param('Role',Role)
+
+	def get_Instances(self):
+		return self.get_query_params().get('Instances')
+
+	def set_Instances(self,Instances):
+		for i in range(len(Instances)):	
+			if Instances[i].get('Id') is not None:
+				self.add_query_param('Instance.' + str(i + 1) + '.Id' , Instances[i].get('Id'))
+
+
+	def get_ClusterId(self):
+		return self.get_query_params().get('ClusterId')
+
+	def set_ClusterId(self,ClusterId):
+		self.add_query_param('ClusterId',ClusterId)
