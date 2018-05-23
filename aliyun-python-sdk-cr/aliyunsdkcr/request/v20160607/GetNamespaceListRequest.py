e02039e7ff03a0d3664c0b984323c03646e55c78
haowei.yao <haowei.yao@alibaba-inc.com>
2018-05-23 15:12:17 +0800

    由驹中发起的CR SDK自动发布, 版本号：3.0.0 发布日志： 1, Add namespace related interface. 2, Add repository related interface. 3, Add image related interface.

---
+++ aliyun-python-sdk-cr/aliyunsdkcr/request/v20160607/GetNamespaceListRequest.py
@@ -0,0 +1,38 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RoaRequest
+class GetNamespaceListRequest(RoaRequest):
+
+	def __init__(self):
+		RoaRequest.__init__(self, 'cr', '2016-06-07', 'GetNamespaceList')
+		self.set_uri_pattern('/namespace')
+		self.set_method('GET')
+
+	def get_Authorize(self):
+		return self.get_query_params().get('Authorize')
+
+	def set_Authorize(self,Authorize):
+		self.add_query_param('Authorize',Authorize)
+
+	def get_Status(self):
+		return self.get_query_params().get('Status')
+
+	def set_Status(self,Status):
+		self.add_query_param('Status',Status)
