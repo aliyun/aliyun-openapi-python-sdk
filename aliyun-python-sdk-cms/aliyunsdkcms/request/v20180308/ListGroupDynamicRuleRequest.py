ded4fb4f8296075eeeb66ce35786e48ad7e0f551
haowei.yao <haowei.yao@alibaba-inc.com>
2018-06-22 11:15:14 +0800

    CMS SDK Auto Released By wb-xzy262218,Version：6.0.8 发布日志： 1, Add new API: DeleteGroupDynamicRule,ListGroupDynamicRule,ApplyTemplate,PutGroupDynamicRule. 2, Add parameter(AlertIds) for CreateTask and ModifyTask.

--- aliyun-python-sdk-cms/aliyunsdkcms/request/v20180308/ListGroupDynamicRuleRequest.py
+++ aliyun-python-sdk-cms/aliyunsdkcms/request/v20180308/ListGroupDynamicRuleRequest.py
@@ -18,22 +18,10 @@
 # under the License.
 
 from aliyunsdkcore.request import RpcRequest
-class DeleteMyGroupInstancesRequest(RpcRequest):
+class ListGroupDynamicRuleRequest(RpcRequest):
 
 	def __init__(self):
-		RpcRequest.__init__(self, 'Cms', '2018-03-08', 'DeleteMyGroupInstances','cms')
-
-	def get_InstanceIds(self):
-		return self.get_query_params().get('InstanceIds')
-
-	def set_InstanceIds(self,InstanceIds):
-		self.add_query_param('InstanceIds',InstanceIds)
-
-	def get_InstanceIdList(self):
-		return self.get_query_params().get('InstanceIdList')
-
-	def set_InstanceIdList(self,InstanceIdList):
-		self.add_query_param('InstanceIdList',InstanceIdList)
+		RpcRequest.__init__(self, 'Cms', '2018-03-08', 'ListGroupDynamicRule','cms')
 
 	def get_GroupId(self):
 		return self.get_query_params().get('GroupId')
