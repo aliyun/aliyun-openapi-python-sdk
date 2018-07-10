4214732bb018767fb700bf761a80086717239e9f
haowei.yao <haowei.yao@alibaba-inc.com>
2018-07-10 21:40:56 +0800

    VOD SDK Auto Released By guzhaoyuan,Version：2.11.3 发布日志： 1, Add new apis called "CreateAudit","GetAuditHistory" and "GetAuditResult" which support auditing feature. 2, Add a new api called GetVideoInfos to bulk query video information. 3, Add a new api called UpdateVideoInfos to update video information. 4, Add a new api called SearchMedia to get video based on the specified filter. 5, Add a new api called ListSnapshots to get the snapshot data. 6, Add the field called VideoStreamList and AudioStreamList to GetMezzanineInfo api response.

---
+++ aliyun-python-sdk-vod/aliyunsdkvod/request/v20170321/GetAuditHistoryRequest.py
@@ -0,0 +1,48 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class GetAuditHistoryRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'vod', '2017-03-21', 'GetAuditHistory','vod')
+
+	def get_PageNo(self):
+		return self.get_query_params().get('PageNo')
+
+	def set_PageNo(self,PageNo):
+		self.add_query_param('PageNo',PageNo)
+
+	def get_PageSize(self):
+		return self.get_query_params().get('PageSize')
+
+	def set_PageSize(self,PageSize):
+		self.add_query_param('PageSize',PageSize)
+
+	def get_VideoId(self):
+		return self.get_query_params().get('VideoId')
+
+	def set_VideoId(self,VideoId):
+		self.add_query_param('VideoId',VideoId)
+
+	def get_SortBy(self):
+		return self.get_query_params().get('SortBy')
+
+	def set_SortBy(self,SortBy):
+		self.add_query_param('SortBy',SortBy)
