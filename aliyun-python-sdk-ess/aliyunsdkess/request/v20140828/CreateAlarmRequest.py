84deff50597ce643a5d07c567421235ba775401d
yixiong.jxy <yixiong.jxy@alibaba-inc.com>
2018-08-16 22:44:50 +0800

    ESS SDK Auto Released By wujin.lhr,Version：2.2.4 发布日志： 1, ModifyScalingConfiguration add imageName. 2, CreateScalingConfiguration add imageName.

---
+++ aliyun-python-sdk-ess/aliyunsdkess/request/v20140828/CreateAlarmRequest.py
@@ -0,0 +1,121 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class CreateAlarmRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Ess', '2014-08-28', 'CreateAlarm','ess')
+
+	def get_MetricType(self):
+		return self.get_query_params().get('MetricType')
+
+	def set_MetricType(self,MetricType):
+		self.add_query_param('MetricType',MetricType)
+
+	def get_Period(self):
+		return self.get_query_params().get('Period')
+
+	def set_Period(self,Period):
+		self.add_query_param('Period',Period)
+
+	def get_ResourceOwnerAccount(self):
+		return self.get_query_params().get('ResourceOwnerAccount')
+
+	def set_ResourceOwnerAccount(self,ResourceOwnerAccount):
+		self.add_query_param('ResourceOwnerAccount',ResourceOwnerAccount)
+
+	def get_ScalingGroupId(self):
+		return self.get_query_params().get('ScalingGroupId')
+
+	def set_ScalingGroupId(self,ScalingGroupId):
+		self.add_query_param('ScalingGroupId',ScalingGroupId)
+
+	def get_GroupId(self):
+		return self.get_query_params().get('GroupId')
+
+	def set_GroupId(self,GroupId):
+		self.add_query_param('GroupId',GroupId)
+
+	def get_Description(self):
+		return self.get_query_params().get('Description')
+
+	def set_Description(self,Description):
+		self.add_query_param('Description',Description)
+
+	def get_AlarmActions(self):
+		return self.get_query_params().get('AlarmActions')
+
+	def set_AlarmActions(self,AlarmActions):
+		for i in range(len(AlarmActions)):	
+			if AlarmActions[i] is not None:
+				self.add_query_param('AlarmAction.' + str(i + 1) , AlarmActions[i]);
+
+	def get_Threshold(self):
+		return self.get_query_params().get('Threshold')
+
+	def set_Threshold(self,Threshold):
+		self.add_query_param('Threshold',Threshold)
+
+	def get_OwnerId(self):
+		return self.get_query_params().get('OwnerId')
+
+	def set_OwnerId(self,OwnerId):
+		self.add_query_param('OwnerId',OwnerId)
+
+	def get_ComparisionOperator(self):
+		return self.get_query_params().get('ComparisionOperator')
+
+	def set_ComparisionOperator(self,ComparisionOperator):
+		self.add_query_param('ComparisionOperator',ComparisionOperator)
+
+	def get_Name(self):
+		return self.get_query_params().get('Name')
+
+	def set_Name(self,Name):
+		self.add_query_param('Name',Name)
+
+	def get_EvaluationCount(self):
+		return self.get_query_params().get('EvaluationCount')
+
+	def set_EvaluationCount(self,EvaluationCount):
+		self.add_query_param('EvaluationCount',EvaluationCount)
+
+	def get_MetricName(self):
+		return self.get_query_params().get('MetricName')
+
+	def set_MetricName(self,MetricName):
+		self.add_query_param('MetricName',MetricName)
+
+	def get_Dimensions(self):
+		return self.get_query_params().get('Dimensions')
+
+	def set_Dimensions(self,Dimensions):
+		for i in range(len(Dimensions)):	
+			if Dimensions[i].get('DimensionValue') is not None:
+				self.add_query_param('Dimension.' + str(i + 1) + '.DimensionValue' , Dimensions[i].get('DimensionValue'))
+			if Dimensions[i].get('DimensionKey') is not None:
+				self.add_query_param('Dimension.' + str(i + 1) + '.DimensionKey' , Dimensions[i].get('DimensionKey'))
+
+
+	def get_Statistics(self):
+		return self.get_query_params().get('Statistics')
+
+	def set_Statistics(self,Statistics):
+		self.add_query_param('Statistics',Statistics)
--- aliyun-python-sdk-ess/aliyunsdkess/request/v20140828/ModifyScalingConfigurationRequest.py
+++ aliyun-python-sdk-ess/aliyunsdkess/request/v20140828/ModifyScalingConfigurationRequest.py
@@ -21,13 +21,80 @@ from aliyunsdkcore.request import RpcRequest
 class ModifyScalingConfigurationRequest(RpcRequest):
 
 	def __init__(self):
-		RpcRequest.__init__(self, 'Ess', '2014-08-28', 'ModifyScalingConfiguration','ESS')
+		RpcRequest.__init__(self, 'Ess', '2014-08-28', 'ModifyScalingConfiguration','ess')
 
 	def get_ImageId(self):
 		return self.get_query_params().get('ImageId')
 
 	def set_ImageId(self,ImageId):
 		self.add_query_param('ImageId',ImageId)
+
+	def get_IoOptimized(self):
+		return self.get_query_params().get('IoOptimized')
+
+	def set_IoOptimized(self,IoOptimized):
+		self.add_query_param('IoOptimized',IoOptimized)
+
+	def get_InstanceTypes(self):
+		return self.get_query_params().get('InstanceTypes')
+
+	def set_InstanceTypes(self,InstanceTypes):
+		for i in range(len(InstanceTypes)):
+			if InstanceTypes[i] is not None:
+				self.add_query_param('InstanceTypes.' + str(i + 1) , InstanceTypes[i]);
+
+	def get_InternetMaxBandwidthOut(self):
+		return self.get_query_params().get('InternetMaxBandwidthOut')
+
+	def set_InternetMaxBandwidthOut(self,InternetMaxBandwidthOut):
+		self.add_query_param('InternetMaxBandwidthOut',InternetMaxBandwidthOut)
+
+	def get_KeyPairName(self):
+		return self.get_query_params().get('KeyPairName')
+
+	def set_KeyPairName(self,KeyPairName):
+		self.add_query_param('KeyPairName',KeyPairName)
+
+	def get_SpotPriceLimits(self):
+		return self.get_query_params().get('SpotPriceLimits')
+
+	def set_SpotPriceLimits(self,SpotPriceLimits):
+		for i in range(len(SpotPriceLimits)):	
+			if SpotPriceLimits[i].get('InstanceType') is not None:
+				self.add_query_param('SpotPriceLimit.' + str(i + 1) + '.InstanceType' , SpotPriceLimits[i].get('InstanceType'))
+			if SpotPriceLimits[i].get('PriceLimit') is not None:
+				self.add_query_param('SpotPriceLimit.' + str(i + 1) + '.PriceLimit' , SpotPriceLimits[i].get('PriceLimit'))
+
+
+	def get_SystemDiskCategory(self):
+		return self.get_query_params().get('SystemDisk.Category')
+
+	def set_SystemDiskCategory(self,SystemDiskCategory):
+		self.add_query_param('SystemDisk.Category',SystemDiskCategory)
+
+	def get_UserData(self):
+		return self.get_query_params().get('UserData')
+
+	def set_UserData(self,UserData):
+		self.add_query_param('UserData',UserData)
+
+	def get_HostName(self):
+		return self.get_query_params().get('HostName')
+
+	def set_HostName(self,HostName):
+		self.add_query_param('HostName',HostName)
+
+	def get_PasswordInherit(self):
+		return self.get_query_params().get('PasswordInherit')
+
+	def set_PasswordInherit(self,PasswordInherit):
+		self.add_query_param('PasswordInherit',PasswordInherit)
+
+	def get_ImageName(self):
+		return self.get_query_params().get('ImageName')
+
+	def set_ImageName(self,ImageName):
+		self.add_query_param('ImageName',ImageName)
 
 	def get_ResourceOwnerAccount(self):
 		return self.get_query_params().get('ResourceOwnerAccount')
@@ -41,31 +108,11 @@ class ModifyScalingConfigurationRequest(RpcRequest):
 	def set_OwnerAccount(self,OwnerAccount):
 		self.add_query_param('OwnerAccount',OwnerAccount)
 
-	def get_InstanceTypes(self):
-		return self.get_query_params().get('InstanceTypes')
-
-	def set_InstanceTypes(self,InstanceTypes):
-		for i in range(len(InstanceTypes)):	
-			if InstanceTypes[i] is not None:
-				self.add_query_param('InstanceTypes.' + str(i + 1) , InstanceTypes[i]);
-
-	def get_InternetMaxBandwidthOut(self):
-		return self.get_query_params().get('InternetMaxBandwidthOut')
-
-	def set_InternetMaxBandwidthOut(self,InternetMaxBandwidthOut):
-		self.add_query_param('InternetMaxBandwidthOut',InternetMaxBandwidthOut)
-
 	def get_RamRoleName(self):
 		return self.get_query_params().get('RamRoleName')
 
 	def set_RamRoleName(self,RamRoleName):
 		self.add_query_param('RamRoleName',RamRoleName)
-
-	def get_KeyPairName(self):
-		return self.get_query_params().get('KeyPairName')
-
-	def set_KeyPairName(self,KeyPairName):
-		self.add_query_param('KeyPairName',KeyPairName)
 
 	def get_OwnerId(self):
 		return self.get_query_params().get('OwnerId')
@@ -73,11 +120,22 @@ class ModifyScalingConfigurationRequest(RpcRequest):
 	def set_OwnerId(self,OwnerId):
 		self.add_query_param('OwnerId',OwnerId)
 
-	def get_SystemDiskCategory(self):
-		return self.get_query_params().get('SystemDisk.Category')
+	def get_DataDisks(self):
+		return self.get_query_params().get('DataDisks')
 
-	def set_SystemDiskCategory(self,SystemDiskCategory):
-		self.add_query_param('SystemDisk.Category',SystemDiskCategory)
+	def set_DataDisks(self,DataDisks):
+		for i in range(len(DataDisks)):	
+			if DataDisks[i].get('SnapshotId') is not None:
+				self.add_query_param('DataDisk.' + str(i + 1) + '.SnapshotId' , DataDisks[i].get('SnapshotId'))
+			if DataDisks[i].get('Size') is not None:
+				self.add_query_param('DataDisk.' + str(i + 1) + '.Size' , DataDisks[i].get('Size'))
+			if DataDisks[i].get('Category') is not None:
+				self.add_query_param('DataDisk.' + str(i + 1) + '.Category' , DataDisks[i].get('Category'))
+			if DataDisks[i].get('Device') is not None:
+				self.add_query_param('DataDisk.' + str(i + 1) + '.Device' , DataDisks[i].get('Device'))
+			if DataDisks[i].get('DeleteWithInstance') is not None:
+				self.add_query_param('DataDisk.' + str(i + 1) + '.DeleteWithInstance' , DataDisks[i].get('DeleteWithInstance'))
+
 
 	def get_ScalingConfigurationName(self):
 		return self.get_query_params().get('ScalingConfigurationName')
@@ -97,17 +155,11 @@ class ModifyScalingConfigurationRequest(RpcRequest):
 	def set_ScalingConfigurationId(self,ScalingConfigurationId):
 		self.add_query_param('ScalingConfigurationId',ScalingConfigurationId)
 
-	def get_UserData(self):
-		return self.get_query_params().get('UserData')
+	def get_SpotStrategy(self):
+		return self.get_query_params().get('SpotStrategy')
 
-	def set_UserData(self,UserData):
-		self.add_query_param('UserData',UserData)
-
-	def get_HostName(self):
-		return self.get_query_params().get('HostName')
-
-	def set_HostName(self,HostName):
-		self.add_query_param('HostName',HostName)
+	def set_SpotStrategy(self,SpotStrategy):
+		self.add_query_param('SpotStrategy',SpotStrategy)
 
 	def get_InstanceName(self):
 		return self.get_query_params().get('InstanceName')
@@ -120,12 +172,6 @@ class ModifyScalingConfigurationRequest(RpcRequest):
 
 	def set_LoadBalancerWeight(self,LoadBalancerWeight):
 		self.add_query_param('LoadBalancerWeight',LoadBalancerWeight)
-
-	def get_PasswordInherit(self):
-		return self.get_query_params().get('PasswordInherit')
-
-	def set_PasswordInherit(self,PasswordInherit):
-		self.add_query_param('PasswordInherit',PasswordInherit)
 
 	def get_SystemDiskSize(self):
 		return self.get_query_params().get('SystemDisk.Size')
