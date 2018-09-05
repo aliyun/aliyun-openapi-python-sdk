2ff032f99ee664cfd31c17a113df43f4161424af
yixiong.jxy <yixiong.jxy@alibaba-inc.com>
2018-09-06 01:04:24 +0800

    IOT SDK Auto Released By cheyun.lxf,Version：7.0.0 发布日志： 1, Add API deleteProduct. 2, Move all API to version 20180120.

---
+++ aliyun-python-sdk-iot/aliyunsdkiot/request/v20180120/CreateTopicRouteTableRequest.py
@@ -0,0 +1,38 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class CreateTopicRouteTableRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Iot', '2018-01-20', 'CreateTopicRouteTable')
+
+	def get_DstTopics(self):
+		return self.get_query_params().get('DstTopics')
+
+	def set_DstTopics(self,DstTopics):
+		for i in range(len(DstTopics)):	
+			if DstTopics[i] is not None:
+				self.add_query_param('DstTopic.' + str(i + 1) , DstTopics[i]);
+
+	def get_SrcTopic(self):
+		return self.get_query_params().get('SrcTopic')
+
+	def set_SrcTopic(self,SrcTopic):
+		self.add_query_param('SrcTopic',SrcTopic)
---
+++ aliyun-python-sdk-iot/aliyunsdkiot/request/v20180120/RRpcRequest.py
@@ -0,0 +1,54 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class RRpcRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Iot', '2018-01-20', 'RRpc')
+
+	def get_RequestBase64Byte(self):
+		return self.get_query_params().get('RequestBase64Byte')
+
+	def set_RequestBase64Byte(self,RequestBase64Byte):
+		self.add_query_param('RequestBase64Byte',RequestBase64Byte)
+
+	def get_Topic(self):
+		return self.get_query_params().get('Topic')
+
+	def set_Topic(self,Topic):
+		self.add_query_param('Topic',Topic)
+
+	def get_DeviceName(self):
+		return self.get_query_params().get('DeviceName')
+
+	def set_DeviceName(self,DeviceName):
+		self.add_query_param('DeviceName',DeviceName)
+
+	def get_ProductKey(self):
+		return self.get_query_params().get('ProductKey')
+
+	def set_ProductKey(self,ProductKey):
+		self.add_query_param('ProductKey',ProductKey)
+
+	def get_Timeout(self):
+		return self.get_query_params().get('Timeout')
+
+	def set_Timeout(self,Timeout):
+		self.add_query_param('Timeout',Timeout)
