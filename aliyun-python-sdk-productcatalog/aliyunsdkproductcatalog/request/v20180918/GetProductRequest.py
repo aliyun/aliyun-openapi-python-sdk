9ce5ffe73699691b06cab004674b460325add6a6
yixiong.jxy <yixiong.jxy@alibaba-inc.com>
2018-10-08 15:25:45 +0800

    PRODUCTCATALOG SDK Auto Released By yixiong.jxy,Version：1.0.0 发布日志： 1, This first release of ProductCatalog

---
+++ aliyun-python-sdk-productcatalog/aliyunsdkproductcatalog/request/v20180918/GetProductRequest.py
@@ -0,0 +1,38 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RoaRequest
+class GetProductRequest(RoaRequest):
+
+	def __init__(self):
+		RoaRequest.__init__(self, 'ProductCatalog', '2018-09-18', 'GetProduct')
+		self.set_uri_pattern('/products/v1/public/[ProductId]/')
+		self.set_method('GET')
+
+	def get_ProductId(self):
+		return self.get_path_params().get('ProductId')
+
+	def set_ProductId(self,ProductId):
+		self.add_path_param('ProductId',ProductId)
+
+	def get_Language(self):
+		return self.get_query_params().get('Language')
+
+	def set_Language(self,Language):
+		self.add_query_param('Language',Language)
