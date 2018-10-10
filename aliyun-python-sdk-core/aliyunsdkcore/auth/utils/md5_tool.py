7e8c7afe1347785e49d257961b27b4622ead3ff2
stmarier <stmarier@gmail.com>
2018-10-10 19:32:05 +0000

    auth/md5_tool: calculate content md5 from bytearray(content) instead of buffer(content)

--- aliyun-python-sdk-core/aliyunsdkcore/auth/utils/md5_tool.py
+++ aliyun-python-sdk-core/aliyunsdkcore/auth/utils/md5_tool.py
@@ -31,9 +31,9 @@ import base64
 
 def _get_md5(content):
     m = hashlib.md5()
-    m.update(buffer(content))
+    m.update(bytearray(content, "utf-8"))
     return m.digest()
 
 
 def get_md5_base64_str(content):
-    return base64.encodestring(_get_md5(content)).strip()
+    return str(base64.b64encode(_get_md5(content)))
