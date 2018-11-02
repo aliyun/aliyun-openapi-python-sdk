22a2fbb7acff3f4ccb1956aef7c192f63e071ddf
yixiong.jxy <yixiong.jxy@alibaba-inc.com>
2018-11-02 13:27:40 +0800

    LUBANCLOUD SDK Auto Released By benxiang.hhq,Version：1.0.0 发布日志： 1, first version

---
+++ aliyun-python-sdk-lubancloud/aliyunsdklubancloud/request/v20180509/BuyOriginPicturesRequest.py
@@ -0,0 +1,33 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class BuyOriginPicturesRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'lubancloud', '2018-05-09', 'BuyOriginPictures','luban')
+		self.set_method('POST')
+
+	def get_PictureIds(self):
+		return self.get_query_params().get('PictureIds')
+
+	def set_PictureIds(self,PictureIds):
+		for i in range(len(PictureIds)):	
+			if PictureIds[i] is not None:
+				self.add_query_param('PictureId.' + str(i + 1) , PictureIds[i]);
