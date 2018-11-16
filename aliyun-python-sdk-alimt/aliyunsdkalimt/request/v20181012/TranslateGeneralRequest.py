eb33c56a4f7d8c2d0e5ad73d66b2d493f5ea4f8e
yixiong.jxy <yixiong.jxy@alibaba-inc.com>
2018-11-16 14:31:28 +0800

    ALIMT SDK Auto Released By xulei.xl,Version：1.0.0 发布日志： 1, The Machine Translation API released two versions as follows: 2, Universal version API release 3, E-commerce version API release

---
+++ aliyun-python-sdk-alimt/aliyunsdkalimt/request/v20181012/TranslateGeneralRequest.py
@@ -0,0 +1,48 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class TranslateGeneralRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'alimt', '2018-10-12', 'TranslateGeneral','alimt')
+
+	def get_SourceLanguage(self):
+		return self.get_body_params().get('SourceLanguage')
+
+	def set_SourceLanguage(self,SourceLanguage):
+		self.add_body_params('SourceLanguage', SourceLanguage)
+
+	def get_SourceText(self):
+		return self.get_body_params().get('SourceText')
+
+	def set_SourceText(self,SourceText):
+		self.add_body_params('SourceText', SourceText)
+
+	def get_FormatType(self):
+		return self.get_body_params().get('FormatType')
+
+	def set_FormatType(self,FormatType):
+		self.add_body_params('FormatType', FormatType)
+
+	def get_TargetLanguage(self):
+		return self.get_body_params().get('TargetLanguage')
+
+	def set_TargetLanguage(self,TargetLanguage):
+		self.add_body_params('TargetLanguage', TargetLanguage)
