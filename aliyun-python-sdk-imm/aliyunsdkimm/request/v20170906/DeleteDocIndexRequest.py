f87028cbd5428cb5fad524076e11bbf7eb08ec6a
yixiong.jxy <yixiong.jxy@alibaba-inc.com>
2018-11-27 12:02:58 +0800

    IMM SDK Auto Released By kuta.zyb,Version：1.5.0 发布日志： 1, add doc index api

---
+++ aliyun-python-sdk-imm/aliyunsdkimm/request/v20170906/DeleteDocIndexRequest.py
@@ -0,0 +1,42 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class DeleteDocIndexRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'imm', '2017-09-06', 'DeleteDocIndex','imm')
+
+	def get_Set(self):
+		return self.get_query_params().get('Set')
+
+	def set_Set(self,Set):
+		self.add_query_param('Set',Set)
+
+	def get_Project(self):
+		return self.get_query_params().get('Project')
+
+	def set_Project(self,Project):
+		self.add_query_param('Project',Project)
+
+	def get_UniqueId(self):
+		return self.get_query_params().get('UniqueId')
+
+	def set_UniqueId(self,UniqueId):
+		self.add_query_param('UniqueId',UniqueId)
