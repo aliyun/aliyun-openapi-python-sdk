33d959e11cd6e42ce9a0de8ff127c23390a8883a
haowei.yao <haowei.yao@alibaba-inc.com>
2019-01-04 18:03:40 +0800

    NAS SDK Auto Released By hanjie.ghj,Version：3.2.0 发布日志： 1, Add new APIs for new feature of NAS Tiering

---
+++ aliyun-python-sdk-nas/aliyunsdknas/request/v20170626/CPFSCreateFileSystemRequest.py
@@ -0,0 +1,72 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class CPFSCreateFileSystemRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'NAS', '2017-06-26', 'CPFSCreateFileSystem','nas')
+
+	def get_FsSpec(self):
+		return self.get_query_params().get('FsSpec')
+
+	def set_FsSpec(self,FsSpec):
+		self.add_query_param('FsSpec',FsSpec)
+
+	def get_VSwitchId(self):
+		return self.get_query_params().get('VSwitchId')
+
+	def set_VSwitchId(self,VSwitchId):
+		self.add_query_param('VSwitchId',VSwitchId)
+
+	def get_Bandwidth(self):
+		return self.get_query_params().get('Bandwidth')
+
+	def set_Bandwidth(self,Bandwidth):
+		self.add_query_param('Bandwidth',Bandwidth)
+
+	def get_VpcId(self):
+		return self.get_query_params().get('VpcId')
+
+	def set_VpcId(self,VpcId):
+		self.add_query_param('VpcId',VpcId)
+
+	def get_ZoneId(self):
+		return self.get_query_params().get('ZoneId')
+
+	def set_ZoneId(self,ZoneId):
+		self.add_query_param('ZoneId',ZoneId)
+
+	def get_NetworkType(self):
+		return self.get_query_params().get('NetworkType')
+
+	def set_NetworkType(self,NetworkType):
+		self.add_query_param('NetworkType',NetworkType)
+
+	def get_FsDesc(self):
+		return self.get_query_params().get('FsDesc')
+
+	def set_FsDesc(self,FsDesc):
+		self.add_query_param('FsDesc',FsDesc)
+
+	def get_Capacity(self):
+		return self.get_query_params().get('Capacity')
+
+	def set_Capacity(self,Capacity):
+		self.add_query_param('Capacity',Capacity)
---
+++ aliyun-python-sdk-nas/aliyunsdknas/request/v20170626/ModifyTieringJobRequest.py
@@ -0,0 +1,78 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class ModifyTieringJobRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'NAS', '2017-06-26', 'ModifyTieringJob','nas')
+
+	def get_Volume(self):
+		return self.get_query_params().get('Volume')
+
+	def set_Volume(self,Volume):
+		self.add_query_param('Volume',Volume)
+
+	def get_Path(self):
+		return self.get_query_params().get('Path')
+
+	def set_Path(self,Path):
+		self.add_query_param('Path',Path)
+
+	def get_Hour(self):
+		return self.get_query_params().get('Hour')
+
+	def set_Hour(self,Hour):
+		self.add_query_param('Hour',Hour)
+
+	def get_Name(self):
+		return self.get_query_params().get('Name')
+
+	def set_Name(self,Name):
+		self.add_query_param('Name',Name)
+
+	def get_Weekday(self):
+		return self.get_query_params().get('Weekday')
+
+	def set_Weekday(self,Weekday):
+		self.add_query_param('Weekday',Weekday)
+
+	def get_Type(self):
+		return self.get_query_params().get('Type')
+
+	def set_Type(self,Type):
+		self.add_query_param('Type',Type)
+
+	def get_Recursive(self):
+		return self.get_query_params().get('Recursive')
+
+	def set_Recursive(self,Recursive):
+		self.add_query_param('Recursive',Recursive)
+
+	def get_Enabled(self):
+		return self.get_query_params().get('Enabled')
+
+	def set_Enabled(self,Enabled):
+		self.add_query_param('Enabled',Enabled)
+
+	def get_Policy(self):
+		return self.get_query_params().get('Policy')
+
+	def set_Policy(self,Policy):
+		self.add_query_param('Policy',Policy)
