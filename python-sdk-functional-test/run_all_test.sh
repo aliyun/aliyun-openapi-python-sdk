665a2876cab038ee5a117893efd382e170b2de7e
青塘 <haowei.yao@alibaba-inc.com>
2019-01-04 16:53:26 +0800

    use pytest instead of unittest to run functional test

--- python-sdk-functional-test/run_all_test.sh
+++ python-sdk-functional-test/run_all_test.sh
@@ -7,11 +7,4 @@ done
 
 cd -
 
-PYTHON_VERSION=`python --version 2>&1 | cut -d ' ' -f 2 | cut -d . -f 1,2`
-if [ $PYTHON_VERSION == '2.6' ]; then
-    echo "Using unittest2 for functional test ..."
-    coverage run --branch -m unittest2 discover -s . -p "*_test.py"
-else
-    echo "Using unittest for functional test ..."
-    coverage run --branch -m unittest discover -s . -p "*_test.py"
-fi
+coverage run --branch -m pytest
