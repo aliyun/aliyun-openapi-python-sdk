532584562454cf0b6e526893ef824c2784264c45
haowei.yao <haowei.yao@alibaba-inc.com>
2019-01-11 14:05:36 +0800

    add 2 test cases

--- python-sdk-functional-test/retry_test.py
+++ python-sdk-functional-test/retry_test.py
@@ -12,9 +12,38 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from mock import MagicMock, patch
 from base import SDKTestBase
+from aliyunsdkcore.client import AcsClient
+from aliyunsdkecs.request.v20140526.DescribeInstancesRequest import DescribeInstancesRequest
+from aliyunsdkcore.acs_exception.exceptions import ClientException
+import aliyunsdkcore.acs_exception.error_code as error_code
 
 
 class RetryTest(SDKTestBase):
 
-    pass
+    def test_no_retry(self):
+
+        client = AcsClient(self.access_key_id, self.access_key_secret, self.region_id,
+                           auto_retry=False)
+        request = DescribeInstancesRequest()
+        request.set_endpoint("somewhere.you.will.never.get")
+        with patch.object(client, "_handle_single_request") as monkey:
+            try:
+                client.do_action_with_exception(request)
+                assert False
+            except ClientException as e:
+                self.assertEqual(error_code.SDK_HTTP_ERROR, e.get_error_code())
+        self.assertEqual(1, monkey.call_count)
+
+    def test_default_retry(self):
+        client = AcsClient(self.access_key_id, self.access_key_secret, self.region_id)
+        request = DescribeInstancesRequest()
+        request.set_endpoint("somewhere.you.will.never.get")
+        with patch.object(client, "_handle_single_request") as monkey:
+            try:
+                client.do_action_with_exception(request)
+                assert False
+            except ClientException as e:
+                self.assertEqual(error_code.SDK_HTTP_ERROR, e.get_error_code())
+        self.assertEqual(4, monkey.call_count)
