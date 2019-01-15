491a2aba9df4c122e07c0a1ef139229a7e4afb3d
haowei.yao <haowei.yao@alibaba-inc.com>
2019-01-15 19:15:09 +0800

    CLOUDAUTH SDK Auto Released By gongpei.gp,Version：1.2.0 发布日志： 1, Add SubmitVerification API for RPMin solution. 2, Return authority comparison score in GetStatus, SubmitMaterials and SubmitVerification.

--- aliyun-python-sdk-cloudauth/aliyunsdkcloudauth/request/v20180916/DetectFaceAttributesRequest.py
+++ aliyun-python-sdk-cloudauth/aliyunsdkcloudauth/request/v20180916/DetectFaceAttributesRequest.py
@@ -21,7 +21,7 @@ from aliyunsdkcore.request import RpcRequest
 class DetectFaceAttributesRequest(RpcRequest):
 
 	def __init__(self):
-		RpcRequest.__init__(self, 'Cloudauth', '2018-09-16', 'DetectFaceAttributes','cloudauth')
+		RpcRequest.__init__(self, 'Cloudauth', '2018-09-16', 'DetectFaceAttributes','CloudAuth')
 		self.set_protocol_type('https');
 		self.set_method('POST')
 
