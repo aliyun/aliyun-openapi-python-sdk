f838801304e5d527591fcc66dbe7a60ad90d2637
wallisyan <wb-yxy487231@alibaba-inc.com>
2019-01-18 17:17:21 +0800

    修改提交脚本

---
+++ test_all.py
@@ -0,0 +1,15 @@
+import os
+cur_path = os.path.abspath('.')
+
+path_list = []
+for ret in os.walk(cur_path):
+    root_path = ret[0]
+    root_path_list = root_path.split('\\')
+    if root_path_list[-1].startswith('aliyun-python-sdk'):
+        path_list.append(root_path)
+
+os.environ.setdefault('PYTHONPATH', path_list)
+
+import subprocess
+ret = subprocess.call("coverage run --branch -m pytest python-sdk-functional-test/")
+print(ret)
