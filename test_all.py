b55b22b9d22d1689402b096d9476282907774707
wallisyan <wb-yxy487231@alibaba-inc.com>
2019-01-18 20:14:55 +0800

    关闭requests自身的max_retries;删除test_all.py多余的文件

--- test_all.py
+++ test_all.py
@@ -1,5 +1,4 @@
 import subprocess
-import sys
 import os
 cur_path = os.path.abspath('.')
 path_list = []
@@ -8,7 +7,7 @@ for ret in os.walk(cur_path):
     root_path_list = root_path.split('\\')
     if root_path_list[-1].startswith('aliyun-python-sdk'):
         path_list.append(root_path)
-        sys.path.append(root_path)
+
 os.environ.__setitem__('PYTHONPATH', ';'.join(path_list))
 
 ret = subprocess.call(
