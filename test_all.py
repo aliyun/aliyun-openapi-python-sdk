1972025810ecde0e2aba0ba9ff0d8cca4e15e5bb
wallisyan <wb-yxy487231@alibaba-inc.com>
2019-01-19 16:39:34 +0800

    add the judge of access_key_id and access_key_secert
    only add package requests;modify coverage

--- test_all.py
+++ test_all.py
@@ -1,20 +1,23 @@
 import os
 from subprocess import check_call
 
-cur_path = os.path.abspath('.')
-path_list = []
-for ret in os.walk(cur_path):
-    root_path = ret[0]
-    root_path_list = root_path.split('\\')
-    if root_path_list[-1].startswith('aliyun-python-sdk'):
-        path_list.append(root_path)
+access_key_id = os.environ.get('ACCESS_KEY_ID')
+access_key_secret = os.environ.get('ACCESS_KEY_SECRET')
+if access_key_id and access_key_secret:
+    cur_path = os.path.abspath('.')
+    path_list = []
+    for ret in os.walk(cur_path):
+        root_path = ret[0]
+        root_path_list = root_path.split('\\')
+        if root_path_list[-1].startswith('aliyun-python-sdk'):
+            path_list.append(root_path)
 
-os.environ.__setitem__('PYTHONPATH', ';'.join(path_list))
+    os.environ.__setitem__('PYTHONPATH', ';'.join(path_list))
 
-try:
-    check_call(
-        "coverage run --branch -m pytest python-sdk-functional-test/", shell=True)
+    try:
+        check_call(
+            "pytest python-sdk-functional-test/", shell=True)
 
-except Exception as e:
-    raise
+    except Exception as e:
+        raise
 
