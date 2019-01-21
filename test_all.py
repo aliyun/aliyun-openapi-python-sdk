62980cce9fa91bf246abb47228fe8025dc68f18c
Wallis Yan <wb-yxy487231@alibaba-inc.com>
2019-01-21 15:13:09 +0800

    基于appveyor构建Windows CI (#174)
    * add .appveyor.yml; add script test_all.py

---
+++ test_all.py
@@ -0,0 +1,20 @@
+import os
+from subprocess import check_call
+
+cur_path = os.path.abspath('.')
+path_list = []
+for ret in os.walk(cur_path):
+    root_path = ret[0]
+    root_path_list = root_path.split('\\')
+    if root_path_list[-1].startswith('aliyun-python-sdk'):
+        path_list.append(root_path)
+
+os.environ.__setitem__('PYTHONPATH', ';'.join(path_list))
+
+try:
+    check_call(
+        "coverage run --branch -m pytest python-sdk-functional-test/", shell=True)
+
+except Exception as e:
+    raise
+
