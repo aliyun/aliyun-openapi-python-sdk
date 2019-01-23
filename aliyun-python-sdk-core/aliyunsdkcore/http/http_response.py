b12e46d50d0f6fa22b5684e50e7189fabea85e1d
Wallis Yan <wb-yxy487231@alibaba-inc.com>
2019-01-23 16:04:00 +0800

    python requests  (#175)
    Use requests instead of

--- aliyun-python-sdk-core/aliyunsdkcore/http/http_response.py
+++ aliyun-python-sdk-core/aliyunsdkcore/http/http_response.py
@@ -18,13 +18,12 @@
 # coding=utf-8
 
 import os
-import base64
-from aliyunsdkcore.vendored.six.moves.urllib.parse import urlparse
-from aliyunsdkcore.vendored.six.moves.http_client import HTTPConnection
-from aliyunsdkcore.vendored.six.moves.http_client import HTTPSConnection
-
+from aliyunsdkcore.vendored.requests import Request, Session
+from aliyunsdkcore.vendored.requests.packages import urllib3
 from aliyunsdkcore.http.http_request import HttpRequest
 from aliyunsdkcore.http import protocol_type as PT
+
+DEFAULT_CONNECT_TIMEOUT = 5
 
 
 class HttpResponse(HttpRequest):
@@ -62,103 +61,29 @@ class HttpResponse(HttpRequest):
     def get_ssl_enabled(self):
         return self.__ssl_enable
 
-    def get_response(self):
-        status, headers, body = self.get_response_object()
-        return headers, body
-
     def get_response_object(self):
-        if self.get_ssl_enabled():
-            return self.get_https_response_object()
-        else:
-            return self.get_http_response_object()
+        with Session() as s:
+            current_protocol = 'https://' if self.get_ssl_enabled() else 'http://'
 
-    def get_http_response_object(self):
-        if self.__port is None or self.__port == "":
-            self.__port = 80
-        try:
-            self.__connection = self.__get_http_connection(
-                self.get_host(), self.__port, timeout=self._timeout)
-            self.__connection.connect()
-            self.__connection.request(
-                method=self.get_method(),
-                url=self.get_url(),
-                body=self.get_body(),
-                headers=self.get_headers())
-            response = self.__connection.getresponse()
-            return response.status, response.getheaders(), response.read()
-        finally:
-            self.__close_connection()
+            req = Request(method=self.get_method(),
+                          url=current_protocol + self.get_host() + self.get_url(),
+                          data=self.get_body(),
+                          headers=self.get_headers(),
+                          )
+            prepped = s.prepare_request(req)
 
-    def get_https_response_object(self):
-        if self.__port is None or self.__port == "":
-            self.__port = 443
-        try:
-            self.__port = 443
-            self.__connection = self.__get_https_connection(
-                self.get_host(),
-                self.__port,
-                cert_file=self.__cert_file,
-                key_file=self.__key_file,
-                timeout=self._timeout)
-            self.__connection.connect()
-            self.__connection.request(
-                method=self.get_method(),
-                url=self.get_url(),
-                body=self.get_body(),
-                headers=self.get_headers())
-            response = self.__connection.getresponse()
-            return response.status, response.getheaders(), response.read()
-        finally:
-            self.__close_connection()
+            proxy_https = os.environ.get('HTTPS_PROXY') or os.environ.get(
+                'https_proxy')
+            proxy_http = os.environ.get(
+                'HTTP_PROXY') or os.environ.get('http_proxy')
 
-    def __close_connection(self):
-        if self.__connection is not None:
-            self.__connection.close()
-            self.__connection = None
-
-    def __get_http_connection(self, host, port, **kwargs):
-        """kwargs maps HTTPConnection arguments"""
-        proxy_host, proxy_port, proxy_headers = self.__get_env_proxy(
-            is_https=False)
-        conn = None
-        if proxy_host and proxy_port:
-            conn = HTTPConnection(proxy_host, proxy_port, **kwargs)
-            conn.set_tunnel(host, port, proxy_headers)
-        else:
-            conn = HTTPConnection(host, port, **kwargs)
-        return conn
-
-    def __get_https_connection(self, host, port, **kwargs):
-        """kwargs maps HTTPConnection arguments"""
-        proxy_host, proxy_port, proxy_headers = self.__get_env_proxy(
-            is_https=True)
-        conn = None
-        if proxy_host and proxy_port:
-            conn = HTTPSConnection(proxy_host, proxy_port, **kwargs)
-            conn.set_tunnel(host, port, proxy_headers)
-        else:
-            conn = HTTPSConnection(host, port, **kwargs)
-        return conn
-
-    def __get_env_proxy(self, is_https):
-        proxy = None
-        if is_https:
-            proxy = os.environ.get('HTTPS_PROXY', None) or os.environ.get(
-                'https_proxy', None)
-        else:
-            proxy = os.environ.get(
-                'HTTP_PROXY', None) or os.environ.get('http_proxy', None)
-
-        if proxy is not None:
-            proxy_headers = {}
-            o = urlparse(proxy)
-            proxy_host = o.hostname
-            proxy_port = o.port
-            if o.username:
-                auth = bytes('%s:%s' % (o.username or '',
-                                        o.password or ''), encoding='utf-8')
-                proxy_headers['Proxy-Authorization'] = 'Basic ' + \
-                    base64.b64encode(auth).decode()
-            return proxy_host, proxy_port, proxy_headers
-
-        return None, None, None
+            proxies = {
+                "http": proxy_http,
+                "https": proxy_https,
+            }
+            # ignore the warning-InsecureRequestWarning
+            urllib3.disable_warnings()
+            response = s.send(prepped, proxies=proxies,
+                              timeout=(DEFAULT_CONNECT_TIMEOUT, self._timeout),
+                              allow_redirects=False, verify=None, cert=None)
+            return response.status_code, response.headers, response.content
