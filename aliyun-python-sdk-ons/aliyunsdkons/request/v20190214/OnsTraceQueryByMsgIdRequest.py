1a07c1bf785ef0a9949e567d55a6f4787a3467dd
haowei.yao <haowei.yao@alibaba-inc.com>
2019-02-18 14:19:06 +0800

    ONS SDK Auto Released By wenting.ywt,Version：3.1.0 发布日志： 1, Instantiation: adding the property of instance. 2, Replace the "ProducerId" and "ConsumerId" properties with "GROUP_ID". 3, Remove parameter OnsRegionId to simplified usage.

---
+++ aliyun-python-sdk-ons/aliyunsdkons/request/v20190214/OnsTraceQueryByMsgIdRequest.py
@@ -0,0 +1,60 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class OnsTraceQueryByMsgIdRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Ons', '2019-02-14', 'OnsTraceQueryByMsgId','ons')
+
+	def get_PreventCache(self):
+		return self.get_query_params().get('PreventCache')
+
+	def set_PreventCache(self,PreventCache):
+		self.add_query_param('PreventCache',PreventCache)
+
+	def get_InstanceId(self):
+		return self.get_query_params().get('InstanceId')
+
+	def set_InstanceId(self,InstanceId):
+		self.add_query_param('InstanceId',InstanceId)
+
+	def get_Topic(self):
+		return self.get_query_params().get('Topic')
+
+	def set_Topic(self,Topic):
+		self.add_query_param('Topic',Topic)
+
+	def get_MsgId(self):
+		return self.get_query_params().get('MsgId')
+
+	def set_MsgId(self,MsgId):
+		self.add_query_param('MsgId',MsgId)
+
+	def get_EndTime(self):
+		return self.get_query_params().get('EndTime')
+
+	def set_EndTime(self,EndTime):
+		self.add_query_param('EndTime',EndTime)
+
+	def get_BeginTime(self):
+		return self.get_query_params().get('BeginTime')
+
+	def set_BeginTime(self,BeginTime):
+		self.add_query_param('BeginTime',BeginTime)
---
+++ aliyun-python-sdk-ons/aliyunsdkons/request/v20190214/OnsWarnCreateRequest.py
@@ -0,0 +1,84 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class OnsWarnCreateRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Ons', '2019-02-14', 'OnsWarnCreate','ons')
+
+	def get_PreventCache(self):
+		return self.get_query_params().get('PreventCache')
+
+	def set_PreventCache(self,PreventCache):
+		self.add_query_param('PreventCache',PreventCache)
+
+	def get_InstanceId(self):
+		return self.get_query_params().get('InstanceId')
+
+	def set_InstanceId(self,InstanceId):
+		self.add_query_param('InstanceId',InstanceId)
+
+	def get_BlockTime(self):
+		return self.get_query_params().get('BlockTime')
+
+	def set_BlockTime(self,BlockTime):
+		self.add_query_param('BlockTime',BlockTime)
+
+	def get_Level(self):
+		return self.get_query_params().get('Level')
+
+	def set_Level(self,Level):
+		self.add_query_param('Level',Level)
+
+	def get_GroupId(self):
+		return self.get_query_params().get('GroupId')
+
+	def set_GroupId(self,GroupId):
+		self.add_query_param('GroupId',GroupId)
+
+	def get_DelayTime(self):
+		return self.get_query_params().get('DelayTime')
+
+	def set_DelayTime(self,DelayTime):
+		self.add_query_param('DelayTime',DelayTime)
+
+	def get_Topic(self):
+		return self.get_query_params().get('Topic')
+
+	def set_Topic(self,Topic):
+		self.add_query_param('Topic',Topic)
+
+	def get_Threshold(self):
+		return self.get_query_params().get('Threshold')
+
+	def set_Threshold(self,Threshold):
+		self.add_query_param('Threshold',Threshold)
+
+	def get_AlertTime(self):
+		return self.get_query_params().get('AlertTime')
+
+	def set_AlertTime(self,AlertTime):
+		self.add_query_param('AlertTime',AlertTime)
+
+	def get_Contacts(self):
+		return self.get_query_params().get('Contacts')
+
+	def set_Contacts(self,Contacts):
+		self.add_query_param('Contacts',Contacts)
