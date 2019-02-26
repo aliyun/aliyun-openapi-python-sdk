71f136079667c76b8dfbe6b4ef74d0911370ba49
haowei.yao <haowei.yao@alibaba-inc.com>
2019-02-26 09:51:09 +0800

    POLARDB SDK Auto Released By zhigang.xzg,Version：1.3.0 发布日志： 1, Add interface of tag. 2, Add tag info in DescribeDBClusters and DescribeDBClusterAttribute.

--- aliyun-python-sdk-polardb/aliyunsdkpolardb/request/v20170801/TagResourcesRequest.py
+++ aliyun-python-sdk-polardb/aliyunsdkpolardb/request/v20170801/TagResourcesRequest.py
@@ -18,10 +18,10 @@
 # under the License.
 
 from aliyunsdkcore.request import RpcRequest
-class DescribeDBClustersRequest(RpcRequest):
+class TagResourcesRequest(RpcRequest):
 
 	def __init__(self):
-		RpcRequest.__init__(self, 'polardb', '2017-08-01', 'DescribeDBClusters','polardb')
+		RpcRequest.__init__(self, 'polardb', '2017-08-01', 'TagResources','polardb')
 
 	def get_ResourceOwnerId(self):
 		return self.get_query_params().get('ResourceOwnerId')
@@ -29,17 +29,13 @@ class DescribeDBClustersRequest(RpcRequest):
 	def set_ResourceOwnerId(self,ResourceOwnerId):
 		self.add_query_param('ResourceOwnerId',ResourceOwnerId)
 
-	def get_DBClusterDescription(self):
-		return self.get_query_params().get('DBClusterDescription')
+	def get_ResourceIds(self):
+		return self.get_query_params().get('ResourceIds')
 
-	def set_DBClusterDescription(self,DBClusterDescription):
-		self.add_query_param('DBClusterDescription',DBClusterDescription)
-
-	def get_DBClusterStatus(self):
-		return self.get_query_params().get('DBClusterStatus')
-
-	def set_DBClusterStatus(self,DBClusterStatus):
-		self.add_query_param('DBClusterStatus',DBClusterStatus)
+	def set_ResourceIds(self,ResourceIds):
+		for i in range(len(ResourceIds)):	
+			if ResourceIds[i] is not None:
+				self.add_query_param('ResourceId.' + str(i + 1) , ResourceIds[i]);
 
 	def get_ResourceOwnerAccount(self):
 		return self.get_query_params().get('ResourceOwnerAccount')
@@ -53,30 +49,6 @@ class DescribeDBClustersRequest(RpcRequest):
 	def set_OwnerAccount(self,OwnerAccount):
 		self.add_query_param('OwnerAccount',OwnerAccount)
 
-	def get_OwnerId(self):
-		return self.get_query_params().get('OwnerId')
-
-	def set_OwnerId(self,OwnerId):
-		self.add_query_param('OwnerId',OwnerId)
-
-	def get_PageNumber(self):
-		return self.get_query_params().get('PageNumber')
-
-	def set_PageNumber(self,PageNumber):
-		self.add_query_param('PageNumber',PageNumber)
-
-	def get_DBType(self):
-		return self.get_query_params().get('DBType')
-
-	def set_DBType(self,DBType):
-		self.add_query_param('DBType',DBType)
-
-	def get_PageSize(self):
-		return self.get_query_params().get('PageSize')
-
-	def set_PageSize(self,PageSize):
-		self.add_query_param('PageSize',PageSize)
-
 	def get_Tags(self):
 		return self.get_query_params().get('Tags')
 
@@ -88,8 +60,14 @@ class DescribeDBClustersRequest(RpcRequest):
 				self.add_query_param('Tag.' + str(i + 1) + '.Key' , Tags[i].get('Key'))
 
 
-	def get_DBClusterIds(self):
-		return self.get_query_params().get('DBClusterIds')
+	def get_OwnerId(self):
+		return self.get_query_params().get('OwnerId')
 
-	def set_DBClusterIds(self,DBClusterIds):
-		self.add_query_param('DBClusterIds',DBClusterIds)
+	def set_OwnerId(self,OwnerId):
+		self.add_query_param('OwnerId',OwnerId)
+
+	def get_ResourceType(self):
+		return self.get_query_params().get('ResourceType')
+
+	def set_ResourceType(self,ResourceType):
+		self.add_query_param('ResourceType',ResourceType)
