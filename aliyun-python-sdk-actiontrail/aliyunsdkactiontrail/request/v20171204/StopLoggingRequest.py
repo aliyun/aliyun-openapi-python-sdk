3a14f0c76334cc80ea679a3bc39855d720262062
haowei.yao <haowei.yao@alibaba-inc.com>
2019-03-07 09:31:10 +0800

    ACTIONTRAIL SDK Auto Released By zhenhuan,Version：1.0.0 发布日志： 1, Release ActionTrail API

--- aliyun-python-sdk-actiontrail/aliyunsdkactiontrail/request/v20171204/StopLoggingRequest.py
+++ aliyun-python-sdk-actiontrail/aliyunsdkactiontrail/request/v20171204/StopLoggingRequest.py
@@ -23,32 +23,8 @@ class StopLoggingRequest(RpcRequest):
 	def __init__(self):
 		RpcRequest.__init__(self, 'Actiontrail', '2017-12-04', 'StopLogging','actiontrail')
 
-	def get_Help(self):
-		return self.get_query_params().get('Help')
-
-	def set_Help(self,Help):
-		self.add_query_param('Help',Help)
-
-	def get_Format(self):
-		return self.get_query_params().get('Format')
-
-	def set_Format(self,Format):
-		self.add_query_param('Format',Format)
-
 	def get_Name(self):
 		return self.get_query_params().get('Name')
 
 	def set_Name(self,Name):
 		self.add_query_param('Name',Name)
-
-	def get_Type(self):
-		return self.get_query_params().get('Type')
-
-	def set_Type(self,Type):
-		self.add_query_param('Type',Type)
-
-	def get_Version(self):
-		return self.get_query_params().get('Version')
-
-	def set_Version(self,Version):
-		self.add_query_param('Version',Version)
