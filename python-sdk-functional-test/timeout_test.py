a3d491a0031efd2ba0661bfa3dd4392cb9e01c3b
feiyang <wb-xzx515910@alibaba-inc.com>
2019-03-08 12:09:47 +0800

    timeout

--- python-sdk-functional-test/timeout_test.py
+++ python-sdk-functional-test/timeout_test.py
@@ -42,8 +42,8 @@ class TimeoutTest(SDKTestBase):
         def _make_http_response(endpoint, request, read_timeout, connect_timeout,
                                 specific_signer=None):
             global _test_patch_client_read_timeout
-            global _test_patch_client_connect_timeout
             _test_patch_client_read_timeout = read_timeout
+            global _test_patch_client_connect_timeout
             _test_patch_client_connect_timeout = connect_timeout
             return original_make_http_response(endpoint, request, read_timeout, connect_timeout,
                                                specific_signer=None)
