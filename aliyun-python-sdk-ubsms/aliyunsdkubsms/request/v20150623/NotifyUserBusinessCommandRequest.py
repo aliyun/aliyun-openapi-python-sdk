57c23fd1f5e9cd51a0c64f436a2dec4fa7b87996
haowei.yao <haowei.yao@alibaba-inc.com>
2019-03-15 16:31:55 +0800

    UBSMS SDK Auto Released By shenshi,Version：2.0.5 发布日志： 1,  Update Dependency

---
+++ aliyun-python-sdk-ubsms/aliyunsdkubsms/request/v20150623/NotifyUserBusinessCommandRequest.py
@@ -0,0 +1,66 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class NotifyUserBusinessCommandRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Ubsms', '2015-06-23', 'NotifyUserBusinessCommand','ubsms')
+
+	def get_Uid(self):
+		return self.get_query_params().get('Uid')
+
+	def set_Uid(self,Uid):
+		self.add_query_param('Uid',Uid)
+
+	def get_Password(self):
+		return self.get_query_params().get('Password')
+
+	def set_Password(self,Password):
+		self.add_query_param('Password',Password)
+
+	def get_InstanceId(self):
+		return self.get_query_params().get('InstanceId')
+
+	def set_InstanceId(self,InstanceId):
+		self.add_query_param('InstanceId',InstanceId)
+
+	def get_ServiceCode(self):
+		return self.get_query_params().get('ServiceCode')
+
+	def set_ServiceCode(self,ServiceCode):
+		self.add_query_param('ServiceCode',ServiceCode)
+
+	def get_ClientToken(self):
+		return self.get_query_params().get('ClientToken')
+
+	def set_ClientToken(self,ClientToken):
+		self.add_query_param('ClientToken',ClientToken)
+
+	def get_Cmd(self):
+		return self.get_query_params().get('Cmd')
+
+	def set_Cmd(self,Cmd):
+		self.add_query_param('Cmd',Cmd)
+
+	def get_Region(self):
+		return self.get_query_params().get('Region')
+
+	def set_Region(self,Region):
+		self.add_query_param('Region',Region)
