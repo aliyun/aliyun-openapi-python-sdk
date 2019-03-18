f85d75f2d3d9ccf4218dd879f969a3d1313a75e9
haowei.yao <haowei.yao@alibaba-inc.com>
2019-03-18 11:42:55 +0800

    OTS SDK Auto Released By shenshi,Version：4.0.1 发布日志： 1, Update Dependency

--- aliyun-python-sdk-ots/aliyunsdkots/request/v20160620/ListVpcInfoByVpcRequest.py
+++ aliyun-python-sdk-ots/aliyunsdkots/request/v20160620/ListVpcInfoByVpcRequest.py
@@ -58,7 +58,7 @@ class ListVpcInfoByVpcRequest(RpcRequest):
 
 	def set_TagInfos(self,TagInfos):
 		for i in range(len(TagInfos)):	
-			if TagInfos[i].get('TagKey') is not None:
-				self.add_query_param('TagInfo.' + str(i + 1) + '.TagKey' , TagInfos[i].get('TagKey'))
 			if TagInfos[i].get('TagValue') is not None:
 				self.add_query_param('TagInfo.' + str(i + 1) + '.TagValue' , TagInfos[i].get('TagValue'))
+			if TagInfos[i].get('TagKey') is not None:
+				self.add_query_param('TagInfo.' + str(i + 1) + '.TagKey' , TagInfos[i].get('TagKey'))
