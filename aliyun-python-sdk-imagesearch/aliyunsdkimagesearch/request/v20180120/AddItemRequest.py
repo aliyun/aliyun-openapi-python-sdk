9e350eef246103e7937320d1536a9effb5c7556a
haowei.yao <haowei.yao@alibaba-inc.com>
2019-03-18 15:39:13 +0800

    IMAGESEARCH SDK Auto Released By shenshi,Version：1.0.1 发布日志： 1, Update Dependency

--- aliyun-python-sdk-imagesearch/aliyunsdkimagesearch/request/v20180120/AddItemRequest.py
+++ aliyun-python-sdk-imagesearch/aliyunsdkimagesearch/request/v20180120/AddItemRequest.py
@@ -1,5 +1,3 @@
-# -- coding: utf-8 --
-
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -19,7 +17,6 @@
 # specific language governing permissions and limitations
 # under the License.
 
-import base64
 from aliyunsdkcore.request import RoaRequest
 class AddItemRequest(RoaRequest):
 
@@ -27,81 +24,9 @@ class AddItemRequest(RoaRequest):
 		RoaRequest.__init__(self, 'ImageSearch', '2018-01-20', 'AddItem','imagesearch')
 		self.set_uri_pattern('/item/add')
 		self.set_method('POST')
-		self.set_accept_format('JSON')
-		self.item_id = ''
-		self.cate_id = ''
-		self.cust_content = ''
-		self.pic_map = {}
 
-	def get_instance_name(self):
+	def get_instanceName(self):
 		return self.get_query_params().get('instanceName')
 
-	def set_instance_name(self,instanceName):
+	def set_instanceName(self,instanceName):
 		self.add_query_param('instanceName',instanceName)
-	
-	def set_item_id(self, item_id):
-		self.item_id = item_id
-	
-	def get_item_id(self):
-		return self.item_id
-
-	def set_cate_id(self, cate_id):
-		self.cate_id = cate_id
-	
-	def get_cate_id(self):
-		return self.cate_id
-	
-	def set_cust_content(self, cust_content):
-		self.cust_content = cust_content
-	
-	def get_cust_content(self):
-		return self.cust_content
-
-	def add_picture(self, pic_name, pic_content):
-		encoded_pic_name = base64.b64encode(pic_name)
-		encoded_pic_content = base64.b64encode(pic_content)
-		self.pic_map[encoded_pic_name] = encoded_pic_content
-
-	# Build Post Content
-	def build_post_content(self):
-		param_map = {}
-		# 参数判断
-		if not self.item_id or not self.cate_id or not self.pic_map:
-			return False
-		# 构建参数
-		param_map['item_id'] = self.item_id
-		param_map['cat_id'] = self.cate_id
-		param_map['cust_content'] = self.cust_content
-
-		# 遍历图片列表
-		pic_list_str = ''
-		for pic_name in self.pic_map:
-			if not pic_name or not self.pic_map[pic_name]:
-				return False
-			pic_list_str += pic_name + ','
-			param_map[pic_name] = self.pic_map[pic_name]
-		
-		param_map['pic_list'] = pic_list_str[:-1]
-
-		self.set_content(self.build_content(param_map))
-
-		return True
-
-	# 构建POST的Body内容
-	def build_content(self, param_map):
-		# 变量
-		meta = ''
-		body = ''
-		start = 0
-
-		# 遍历参数
-		for key in param_map:
-			if len(meta) > 0:
-				meta += '#'
-			meta += key + ',' + str(start) + ',' + str(start + len(param_map[key]))
-			body += param_map[key]
-			start += len(param_map[key])
-		return meta + '^' + body
-
-
-	
--- aliyun-python-sdk-imagesearch/aliyunsdkimagesearch/request/v20180120/SearchItemRequest.py
+++ aliyun-python-sdk-imagesearch/aliyunsdkimagesearch/request/v20180120/SearchItemRequest.py
@@ -1,5 +1,3 @@
-# -- coding: utf-8 --
-
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -19,7 +17,6 @@
 # specific language governing permissions and limitations
 # under the License.
 
-import base64
 from aliyunsdkcore.request import RoaRequest
 class SearchItemRequest(RoaRequest):
 
@@ -27,76 +24,9 @@ class SearchItemRequest(RoaRequest):
 		RoaRequest.__init__(self, 'ImageSearch', '2018-01-20', 'SearchItem','imagesearch')
 		self.set_uri_pattern('/item/search')
 		self.set_method('POST')
-		self.set_accept_format('JSON')
-		self.start = 0
-		self.num = 10
-		self.cate_id = ''
-		self.search_picture = ''
 
-	def get_instance_name(self):
+	def get_instanceName(self):
 		return self.get_query_params().get('instanceName')
 
-	def set_instance_name(self,instanceName):
+	def set_instanceName(self,instanceName):
 		self.add_query_param('instanceName',instanceName)
-
-	def set_start(self, start):
-		self.start = start
-	
-	def get_start(self):
-		return self.start
-	
-	def set_num(self, num):
-		self.num = num
-	
-	def get_num(self):
-		return self.num
-
-	def set_cate_id(self, cate_id):
-		self.cate_id = cate_id
-	
-	def get_cate_id(self):
-		return self.cate_id
-	
-	def set_search_picture(self, search_picture):
-		self.search_picture = search_picture
-
-	def get_search_picture(self):
-		return self.search_picture
-
-	# Build Post Content
-	def build_post_content(self):
-		param_map = {}
-		# 参数判断
-		if not self.search_picture :
-			return False
-		# 构建参数
-		param_map['s'] = str(self.start)
-		param_map['n'] = str(self.num)
-		if self.cate_id and len(self.cate_id) > 0:
-			param_map['cat_id'] = self.cate_id
-
-		encoded_pic_name = base64.b64encode("searchPic")
-		encoded_pic_content = base64.b64encode(self.search_picture)
-
-		param_map['pic_list'] = encoded_pic_name
-		param_map[encoded_pic_name] = encoded_pic_content
-
-		self.set_content(self.build_content(param_map))
-
-		return True
-
-	# 构建POST的Body内容
-	def build_content(self, param_map):
-		# 变量
-		meta = ''
-		body = ''
-		start = 0
-
-		# 遍历参数
-		for key in param_map:
-			if len(meta) > 0:
-				meta += '#'
-			meta += key + ',' + str(start) + ',' + str(start + len(param_map[key]))
-			body += param_map[key]
-			start += len(param_map[key])
-		return meta + '^' + body
