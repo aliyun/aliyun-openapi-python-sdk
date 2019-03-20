29a6453c70c30536fcf9357a3f91e19980729a4e
haowei.yao <haowei.yao@alibaba-inc.com>
2019-03-20 13:56:21 +0800

    JAQ SDK Auto Released By shenshi,Version：2.0.7 发布日志： 1, Update the Dependency

---
+++ aliyun-python-sdk-jaq/aliyunsdkjaq/request/v20160412/ShieldRequest.py
@@ -0,0 +1,36 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class ShieldRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'jaq', '2016-04-12', 'Shield','jaq')
+
+	def get_Channel(self):
+		return self.get_query_params().get('Channel')
+
+	def set_Channel(self,Channel):
+		self.add_query_param('Channel',Channel)
+
+	def get_AppInfo(self):
+		return self.get_query_params().get('AppInfo')
+
+	def set_AppInfo(self,AppInfo):
+		self.add_query_param('AppInfo',AppInfo)
