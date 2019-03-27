3c031ef4582b998e3263b9e157036b67ff794575
haowei.yao <haowei.yao@alibaba-inc.com>
2019-03-27 11:16:54 +0800

    ONS SDK Auto Released By wenting.ywt,Version：3.1.1 发布日志： 1, Add DLQ message openAPI. 2, Add Query the subscription relational openAPI. 3, Remove white list restrictions.

---
+++ aliyun-python-sdk-ons/aliyunsdkons/request/v20190214/OnsDLQMessagePageQueryByGroupIdRequest.py
@@ -0,0 +1,72 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class OnsDLQMessagePageQueryByGroupIdRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Ons', '2019-02-14', 'OnsDLQMessagePageQueryByGroupId','ons')
+
+	def get_PreventCache(self):
+		return self.get_query_params().get('PreventCache')
+
+	def set_PreventCache(self,PreventCache):
+		self.add_query_param('PreventCache',PreventCache)
+
+	def get_InstanceId(self):
+		return self.get_query_params().get('InstanceId')
+
+	def set_InstanceId(self,InstanceId):
+		self.add_query_param('InstanceId',InstanceId)
+
+	def get_GroupId(self):
+		return self.get_query_params().get('GroupId')
+
+	def set_GroupId(self,GroupId):
+		self.add_query_param('GroupId',GroupId)
+
+	def get_PageSize(self):
+		return self.get_query_params().get('PageSize')
+
+	def set_PageSize(self,PageSize):
+		self.add_query_param('PageSize',PageSize)
+
+	def get_EndTime(self):
+		return self.get_query_params().get('EndTime')
+
+	def set_EndTime(self,EndTime):
+		self.add_query_param('EndTime',EndTime)
+
+	def get_BeginTime(self):
+		return self.get_query_params().get('BeginTime')
+
+	def set_BeginTime(self,BeginTime):
+		self.add_query_param('BeginTime',BeginTime)
+
+	def get_CurrentPage(self):
+		return self.get_query_params().get('CurrentPage')
+
+	def set_CurrentPage(self,CurrentPage):
+		self.add_query_param('CurrentPage',CurrentPage)
+
+	def get_TaskId(self):
+		return self.get_query_params().get('TaskId')
+
+	def set_TaskId(self,TaskId):
+		self.add_query_param('TaskId',TaskId)
