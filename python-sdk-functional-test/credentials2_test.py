4914a5e44813106ae6e5c1b8b099bb6fe3fca7f7
feiyang0210 <wb-xzx515910@alibaba-inc.com>
2019-04-03 23:17:07 +0800

    test_# encoding:utf-8

--- python-sdk-functional-test/credentials2_test.py
+++ python-sdk-functional-test/credentials2_test.py
@@ -1,10 +1,14 @@
 # encoding:utf-8
 import json
 import os
+from http.server import HTTPServer
+from unittest import mock
+
+import requests
 
 from aliyunsdkcore.client import AcsClient
 from aliyunsdkcore.auth.credentials import StsTokenCredential, RamRoleArnCredential, \
-    EcsRamRoleCredential
+    EcsRamRoleCredential, AccessKeyCredential
 from aliyunsdkros.request.v20150901.DescribeResourceTypesRequest import \
     DescribeResourceTypesRequest
 from aliyunsdkecs.request.v20140526.DescribeRegionsRequest import DescribeRegionsRequest
@@ -12,7 +16,14 @@ from aliyunsdksts.request.v20150401.AssumeRoleRequest import AssumeRoleRequest
 from aliyunsdkram.request.v20150501.CreateRoleRequest import CreateRoleRequest
 
 from base import SDKTestBase
-from base import disabled
+
+from aliyunsdkcore.vendored.requests import get
+from aliyunsdkcore.vendored.requests.packages.urllib3 import HTTPConnectionPool
+from tests import MyServer
+
+
+data = {'result': 'this is a test'}
+host = ('100.100.100.200',)
 
 
 class CredentialsTest(SDKTestBase):
@@ -58,6 +69,40 @@ class CredentialsTest(SDKTestBase):
         response_key_secret = response.http_request.credentials.access_key_secret
         self.assertEqual(env_credential_id, response_key_id)
         self.assertEqual(env_credential_secret, response_key_secret)
+
+        response = response.http_response.content
+        ret = self.get_dict_response(response)
+        self.assertTrue(ret.get("Regions"))
+        self.assertTrue(ret.get("RequestId"))
+
+    def test_call_request_with_env_role_name_priority(self):
+        os.environ["ALIBABA_CLOUD_ACCESS_KEY_ID"] = self.access_key_id
+        os.environ["ALIBABA_CLOUD_ACCESS_KEY_SECRET"] = self.access_key_secret
+
+        os.environ["ALIBABA_CLOUD_ROLE_NAME"] = self.default_ram_role_name
+        client = AcsClient()
+        request = DescribeRegionsRequest()
+        response = client._handle_request_in_new_style(request)
+        env_credential_id = os.environ.get("ALIBABA_CLOUD_ACCESS_KEY_ID")
+        env_credential_secret = os.environ.get("ALIBABA_CLOUD_ACCESS_KEY_SECRET")
+        response_key_id = response.http_request.credentials.access_key_id
+        response_key_secret = response.http_request.credentials.access_key_secret
+        self.assertEqual(env_credential_id, response_key_id)
+        self.assertEqual(env_credential_secret, response_key_secret)
+
+        response = response.http_response.content
+        ret = self.get_dict_response(response)
+        self.assertTrue(ret.get("Regions"))
+        self.assertTrue(ret.get("RequestId"))
+
+    def test_call_request_with_config_role_name_priority(self):
+        os.environ["ALIBABA_CLOUD_ROLE_NAME"] = self.default_ram_role_name
+
+        client = AcsClient()
+        request = DescribeRegionsRequest()
+        response = client._handle_request_in_new_style(request)
+        response_key_id = response.http_request.credentials.access_key_id
+        self.assertFalse(response_key_id.startswith("TST."))
 
         response = response.http_response.content
         ret = self.get_dict_response(response)
@@ -183,6 +228,38 @@ class CredentialsTest(SDKTestBase):
         ret = self.get_dict_response(response)
         self.assertTrue(ret.get("ResourceTypes"))
 
+    @mock.patch("alibabacloud.credentials.provider.InstanceProfileCredentialsProvider")
+    def test_call_rpc_request_with_role_name(self, InstanceProfileCredentialsProvider):
+        with MyServer() as f:
+            ecs_ram_role_credential = EcsRamRoleCredential("EcsRamRoleTest")
+            InstanceProfileCredentialsProvider.rotate_credentials.return_value = \
+                requests.get(url="http://localhost:51352")
+            instance_value =InstanceProfileCredentialsProvider.rotate_credentials.\
+                return_value
+            # content = instance_value.content
+
+            client = self.init_sub_client()
+            self._create_default_ram_role()
+            request = AssumeRoleRequest()
+            request.set_RoleArn(self.ram_role_arn)
+            request.set_RoleSessionName(self.default_role_session_name)
+            response = client.do_action_with_exception(request)
+            response = self.get_dict_response(response)
+            credentials = response.get("Credentials")
+
+            sts_token_credential = StsTokenCredential(
+                credentials.get("AccessKeyId"),
+                credentials.get("AccessKeySecret"),
+                credentials.get("SecurityToken")
+            )
+            roa_client = AcsClient(
+                region_id=self.region_id,
+                credential=sts_token_credential)
+            request = DescribeResourceTypesRequest()
+            response = roa_client.do_action_with_exception(request)
+            ret = self.get_dict_response(response)
+            self.assertTrue(ret.get("ResourceTypes"))
+
     def test_call_rpc_request_with_env_ak(self):
         os.environ["ALIBABA_CLOUD_ACCESS_KEY_ID"] = self.access_key_id
         os.environ["ALIBABA_CLOUD_ACCESS_KEY_SECRET"] = self.access_key_secret
@@ -225,7 +302,7 @@ class CredentialsTest(SDKTestBase):
     #     ret = self.get_dict_response(response)
     #     self.assertTrue(ret.get("Regions"))
     #     self.assertTrue(ret.get("RequestId"))
-    #
+
     # def test_call_roa_request_with_config_ram_role_arn(self):
     #     client = AcsClient()
     #     client._credentials_provider = DefaultChainedCredentialsProvider("ram_role_arn")
