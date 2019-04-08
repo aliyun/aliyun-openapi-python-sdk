efe452164f695a4b45c3983471fe8617d3d036ea
haowei.yao <haowei.yao@alibaba-inc.com>
2019-04-08 17:01:29 +0800

    DBS SDK Auto Released By wanxin,Version：1.0.1 发布日志： 1, Add StopBackupPlan DBS interface. 2, Add DescribeBackupPlanList DBS interface. 3, Add DescribeFullBackupList DBS interface. 4, Add DescribeIncrementBackupList DBS interface. 5, Add DescribeBackupGatewayList DBS interface.

---
+++ aliyun-python-sdk-dbs/aliyunsdkdbs/request/v20190306/CreateBackupPlanRequest.py
@@ -0,0 +1,72 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class CreateBackupPlanRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Dbs', '2019-03-06', 'CreateBackupPlan','cbs')
+
+	def get_BackupMethod(self):
+		return self.get_query_params().get('BackupMethod')
+
+	def set_BackupMethod(self,BackupMethod):
+		self.add_query_param('BackupMethod',BackupMethod)
+
+	def get_DatabaseType(self):
+		return self.get_query_params().get('DatabaseType')
+
+	def set_DatabaseType(self,DatabaseType):
+		self.add_query_param('DatabaseType',DatabaseType)
+
+	def get_Period(self):
+		return self.get_query_params().get('Period')
+
+	def set_Period(self,Period):
+		self.add_query_param('Period',Period)
+
+	def get_ClientToken(self):
+		return self.get_query_params().get('ClientToken')
+
+	def set_ClientToken(self,ClientToken):
+		self.add_query_param('ClientToken',ClientToken)
+
+	def get_Region(self):
+		return self.get_query_params().get('Region')
+
+	def set_Region(self,Region):
+		self.add_query_param('Region',Region)
+
+	def get_OwnerId(self):
+		return self.get_query_params().get('OwnerId')
+
+	def set_OwnerId(self,OwnerId):
+		self.add_query_param('OwnerId',OwnerId)
+
+	def get_UsedTime(self):
+		return self.get_query_params().get('UsedTime')
+
+	def set_UsedTime(self,UsedTime):
+		self.add_query_param('UsedTime',UsedTime)
+
+	def get_InstanceClass(self):
+		return self.get_query_params().get('InstanceClass')
+
+	def set_InstanceClass(self,InstanceClass):
+		self.add_query_param('InstanceClass',InstanceClass)
