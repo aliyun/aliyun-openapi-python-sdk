6c52949f1f3f225f7e2fe4e10d80ce0b7b25e278
haowei.yao <haowei.yao@alibaba-inc.com>
2019-04-13 21:56:16 +0800

    CMS SDK Auto Released By kun.dang,Version：7.0.2 发布日志： 1, Adding Modified Instance Management Interface.

---
+++ aliyun-python-sdk-cms/aliyunsdkcms/request/v20190101/DescribeAlertHistoryListRequest.py
@@ -0,0 +1,96 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class DescribeAlertHistoryListRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Cms', '2019-01-01', 'DescribeAlertHistoryList','cms')
+
+	def get_GroupId(self):
+		return self.get_query_params().get('GroupId')
+
+	def set_GroupId(self,GroupId):
+		self.add_query_param('GroupId',GroupId)
+
+	def get_AlertStatus(self):
+		return self.get_query_params().get('AlertStatus')
+
+	def set_AlertStatus(self,AlertStatus):
+		self.add_query_param('AlertStatus',AlertStatus)
+
+	def get_Namespace(self):
+		return self.get_query_params().get('Namespace')
+
+	def set_Namespace(self,Namespace):
+		self.add_query_param('Namespace',Namespace)
+
+	def get_PageSize(self):
+		return self.get_query_params().get('PageSize')
+
+	def set_PageSize(self,PageSize):
+		self.add_query_param('PageSize',PageSize)
+
+	def get_EndTime(self):
+		return self.get_query_params().get('EndTime')
+
+	def set_EndTime(self,EndTime):
+		self.add_query_param('EndTime',EndTime)
+
+	def get_RuleName(self):
+		return self.get_query_params().get('RuleName')
+
+	def set_RuleName(self,RuleName):
+		self.add_query_param('RuleName',RuleName)
+
+	def get_State(self):
+		return self.get_query_params().get('State')
+
+	def set_State(self,State):
+		self.add_query_param('State',State)
+
+	def get_StartTime(self):
+		return self.get_query_params().get('StartTime')
+
+	def set_StartTime(self,StartTime):
+		self.add_query_param('StartTime',StartTime)
+
+	def get_Page(self):
+		return self.get_query_params().get('Page')
+
+	def set_Page(self,Page):
+		self.add_query_param('Page',Page)
+
+	def get_RuleId(self):
+		return self.get_query_params().get('RuleId')
+
+	def set_RuleId(self,RuleId):
+		self.add_query_param('RuleId',RuleId)
+
+	def get_MetricName(self):
+		return self.get_query_params().get('MetricName')
+
+	def set_MetricName(self,MetricName):
+		self.add_query_param('MetricName',MetricName)
+
+	def get_Ascending(self):
+		return self.get_query_params().get('Ascending')
+
+	def set_Ascending(self,Ascending):
+		self.add_query_param('Ascending',Ascending)
