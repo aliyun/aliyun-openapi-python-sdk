1a60db93a3e4f1ce5d647c9c7a2d5bfa157bc689
haowei.yao <haowei.yao@alibaba-inc.com>
2019-04-18 12:06:19 +0800

    ECS SDK Auto Released By shaoyi.qusy,Version：4.16.7 发布日志： 1, add ReservedInstance API.

---
+++ aliyun-python-sdk-ecs/aliyunsdkecs/request/v20140526/DescribeReservedInstancesRequest.py
@@ -0,0 +1,118 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class DescribeReservedInstancesRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Ecs', '2014-05-26', 'DescribeReservedInstances','ecs')
+
+	def get_ResourceOwnerId(self):
+		return self.get_query_params().get('ResourceOwnerId')
+
+	def set_ResourceOwnerId(self,ResourceOwnerId):
+		self.add_query_param('ResourceOwnerId',ResourceOwnerId)
+
+	def get_PageNumber(self):
+		return self.get_query_params().get('PageNumber')
+
+	def set_PageNumber(self,PageNumber):
+		self.add_query_param('PageNumber',PageNumber)
+
+	def get_LockReason(self):
+		return self.get_query_params().get('LockReason')
+
+	def set_LockReason(self,LockReason):
+		self.add_query_param('LockReason',LockReason)
+
+	def get_Scope(self):
+		return self.get_query_params().get('Scope')
+
+	def set_Scope(self,Scope):
+		self.add_query_param('Scope',Scope)
+
+	def get_PageSize(self):
+		return self.get_query_params().get('PageSize')
+
+	def set_PageSize(self,PageSize):
+		self.add_query_param('PageSize',PageSize)
+
+	def get_InstanceType(self):
+		return self.get_query_params().get('InstanceType')
+
+	def set_InstanceType(self,InstanceType):
+		self.add_query_param('InstanceType',InstanceType)
+
+	def get_ResourceOwnerAccount(self):
+		return self.get_query_params().get('ResourceOwnerAccount')
+
+	def set_ResourceOwnerAccount(self,ResourceOwnerAccount):
+		self.add_query_param('ResourceOwnerAccount',ResourceOwnerAccount)
+
+	def get_OwnerAccount(self):
+		return self.get_query_params().get('OwnerAccount')
+
+	def set_OwnerAccount(self,OwnerAccount):
+		self.add_query_param('OwnerAccount',OwnerAccount)
+
+	def get_InstanceTypeFamily(self):
+		return self.get_query_params().get('InstanceTypeFamily')
+
+	def set_InstanceTypeFamily(self,InstanceTypeFamily):
+		self.add_query_param('InstanceTypeFamily',InstanceTypeFamily)
+
+	def get_OwnerId(self):
+		return self.get_query_params().get('OwnerId')
+
+	def set_OwnerId(self,OwnerId):
+		self.add_query_param('OwnerId',OwnerId)
+
+	def get_ReservedInstanceIds(self):
+		return self.get_query_params().get('ReservedInstanceIds')
+
+	def set_ReservedInstanceIds(self,ReservedInstanceIds):
+		for i in range(len(ReservedInstanceIds)):	
+			if ReservedInstanceIds[i] is not None:
+				self.add_query_param('ReservedInstanceId.' + str(i + 1) , ReservedInstanceIds[i]);
+
+	def get_OfferingType(self):
+		return self.get_query_params().get('OfferingType')
+
+	def set_OfferingType(self,OfferingType):
+		self.add_query_param('OfferingType',OfferingType)
+
+	def get_ZoneId(self):
+		return self.get_query_params().get('ZoneId')
+
+	def set_ZoneId(self,ZoneId):
+		self.add_query_param('ZoneId',ZoneId)
+
+	def get_ReservedInstanceName(self):
+		return self.get_query_params().get('ReservedInstanceName')
+
+	def set_ReservedInstanceName(self,ReservedInstanceName):
+		self.add_query_param('ReservedInstanceName',ReservedInstanceName)
+
+	def get_Statuss(self):
+		return self.get_query_params().get('Statuss')
+
+	def set_Statuss(self,Statuss):
+		for i in range(len(Statuss)):	
+			if Statuss[i] is not None:
+				self.add_query_param('Status.' + str(i + 1) , Statuss[i]);
---
+++ aliyun-python-sdk-ecs/aliyunsdkecs/request/v20140526/PurchaseReservedInstancesOfferingRequest.py
@@ -0,0 +1,114 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class PurchaseReservedInstancesOfferingRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Ecs', '2014-05-26', 'PurchaseReservedInstancesOffering','ecs')
+
+	def get_ResourceOwnerId(self):
+		return self.get_query_params().get('ResourceOwnerId')
+
+	def set_ResourceOwnerId(self,ResourceOwnerId):
+		self.add_query_param('ResourceOwnerId',ResourceOwnerId)
+
+	def get_ClientToken(self):
+		return self.get_query_params().get('ClientToken')
+
+	def set_ClientToken(self,ClientToken):
+		self.add_query_param('ClientToken',ClientToken)
+
+	def get_Description(self):
+		return self.get_query_params().get('Description')
+
+	def set_Description(self,Description):
+		self.add_query_param('Description',Description)
+
+	def get_ResourceGroupId(self):
+		return self.get_query_params().get('ResourceGroupId')
+
+	def set_ResourceGroupId(self,ResourceGroupId):
+		self.add_query_param('ResourceGroupId',ResourceGroupId)
+
+	def get_Scope(self):
+		return self.get_query_params().get('Scope')
+
+	def set_Scope(self,Scope):
+		self.add_query_param('Scope',Scope)
+
+	def get_InstanceType(self):
+		return self.get_query_params().get('InstanceType')
+
+	def set_InstanceType(self,InstanceType):
+		self.add_query_param('InstanceType',InstanceType)
+
+	def get_Period(self):
+		return self.get_query_params().get('Period')
+
+	def set_Period(self,Period):
+		self.add_query_param('Period',Period)
+
+	def get_ResourceOwnerAccount(self):
+		return self.get_query_params().get('ResourceOwnerAccount')
+
+	def set_ResourceOwnerAccount(self,ResourceOwnerAccount):
+		self.add_query_param('ResourceOwnerAccount',ResourceOwnerAccount)
+
+	def get_OwnerAccount(self):
+		return self.get_query_params().get('OwnerAccount')
+
+	def set_OwnerAccount(self,OwnerAccount):
+		self.add_query_param('OwnerAccount',OwnerAccount)
+
+	def get_OwnerId(self):
+		return self.get_query_params().get('OwnerId')
+
+	def set_OwnerId(self,OwnerId):
+		self.add_query_param('OwnerId',OwnerId)
+
+	def get_PeriodUnit(self):
+		return self.get_query_params().get('PeriodUnit')
+
+	def set_PeriodUnit(self,PeriodUnit):
+		self.add_query_param('PeriodUnit',PeriodUnit)
+
+	def get_OfferingType(self):
+		return self.get_query_params().get('OfferingType')
+
+	def set_OfferingType(self,OfferingType):
+		self.add_query_param('OfferingType',OfferingType)
+
+	def get_ZoneId(self):
+		return self.get_query_params().get('ZoneId')
+
+	def set_ZoneId(self,ZoneId):
+		self.add_query_param('ZoneId',ZoneId)
+
+	def get_ReservedInstanceName(self):
+		return self.get_query_params().get('ReservedInstanceName')
+
+	def set_ReservedInstanceName(self,ReservedInstanceName):
+		self.add_query_param('ReservedInstanceName',ReservedInstanceName)
+
+	def get_InstanceAmount(self):
+		return self.get_query_params().get('InstanceAmount')
+
+	def set_InstanceAmount(self,InstanceAmount):
+		self.add_query_param('InstanceAmount',InstanceAmount)
