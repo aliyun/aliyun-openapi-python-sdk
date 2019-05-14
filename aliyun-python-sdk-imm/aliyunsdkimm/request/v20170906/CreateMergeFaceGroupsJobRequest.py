89be6211ef3243f837984e639250379a1fce4adc
sdk-team <sdk-team@alibabacloud.com>
2019-05-14 14:32:26 +0800

    IMM SDK Auto Released By zhengrui.lj,Version：1.9.2 发布日志： 1, Support body detect.

---
+++ aliyun-python-sdk-imm/aliyunsdkimm/request/v20170906/CreateMergeFaceGroupsJobRequest.py
@@ -0,0 +1,60 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class CreateMergeFaceGroupsJobRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'imm', '2017-09-06', 'CreateMergeFaceGroupsJob','imm')
+
+	def get_GroupIdFrom(self):
+		return self.get_query_params().get('GroupIdFrom')
+
+	def set_GroupIdFrom(self,GroupIdFrom):
+		self.add_query_param('GroupIdFrom',GroupIdFrom)
+
+	def get_GroupIdTo(self):
+		return self.get_query_params().get('GroupIdTo')
+
+	def set_GroupIdTo(self,GroupIdTo):
+		self.add_query_param('GroupIdTo',GroupIdTo)
+
+	def get_NotifyTopicName(self):
+		return self.get_query_params().get('NotifyTopicName')
+
+	def set_NotifyTopicName(self,NotifyTopicName):
+		self.add_query_param('NotifyTopicName',NotifyTopicName)
+
+	def get_NotifyEndpoint(self):
+		return self.get_query_params().get('NotifyEndpoint')
+
+	def set_NotifyEndpoint(self,NotifyEndpoint):
+		self.add_query_param('NotifyEndpoint',NotifyEndpoint)
+
+	def get_Project(self):
+		return self.get_query_params().get('Project')
+
+	def set_Project(self,Project):
+		self.add_query_param('Project',Project)
+
+	def get_SetId(self):
+		return self.get_query_params().get('SetId')
+
+	def set_SetId(self,SetId):
+		self.add_query_param('SetId',SetId)
