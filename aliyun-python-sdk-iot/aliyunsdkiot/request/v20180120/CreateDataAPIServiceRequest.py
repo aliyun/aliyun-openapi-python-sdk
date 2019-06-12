395da174a8af20ce01046097ced3ba9d0d79e94a
sdk-team <sdk-team@alibabacloud.com>
2019-06-12 13:48:09 +0800

    1, Add InvokeDataAPIService interface, support invoke service of data api to get sql query result.

---
+++ aliyun-python-sdk-iot/aliyunsdkiot/request/v20180120/CreateDataAPIServiceRequest.py
@@ -0,0 +1,94 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class CreateDataAPIServiceRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Iot', '2018-01-20', 'CreateDataAPIService','iot')
+
+	def get_ResponseParams(self):
+		return self.get_body_params().get('ResponseParams')
+
+	def set_ResponseParams(self,ResponseParams):
+		for i in range(len(ResponseParams)):	
+			if ResponseParams[i].get('Name') is not None:
+				self.add_body_params('ResponseParam.' + str(i + 1) + '.Name' , ResponseParams[i].get('Name'))
+			if ResponseParams[i].get('Type') is not None:
+				self.add_body_params('ResponseParam.' + str(i + 1) + '.Type' , ResponseParams[i].get('Type'))
+			if ResponseParams[i].get('Required') is not None:
+				self.add_body_params('ResponseParam.' + str(i + 1) + '.Required' , ResponseParams[i].get('Required'))
+			if ResponseParams[i].get('Desc') is not None:
+				self.add_body_params('ResponseParam.' + str(i + 1) + '.Desc' , ResponseParams[i].get('Desc'))
+			if ResponseParams[i].get('Example') is not None:
+				self.add_body_params('ResponseParam.' + str(i + 1) + '.Example' , ResponseParams[i].get('Example'))
+
+
+	def get_OriginSql(self):
+		return self.get_query_params().get('OriginSql')
+
+	def set_OriginSql(self,OriginSql):
+		self.add_query_param('OriginSql',OriginSql)
+
+	def get_DisplayName(self):
+		return self.get_query_params().get('DisplayName')
+
+	def set_DisplayName(self,DisplayName):
+		self.add_query_param('DisplayName',DisplayName)
+
+	def get_ApiPath(self):
+		return self.get_query_params().get('ApiPath')
+
+	def set_ApiPath(self,ApiPath):
+		self.add_query_param('ApiPath',ApiPath)
+
+	def get_RequestParams(self):
+		return self.get_body_params().get('RequestParams')
+
+	def set_RequestParams(self,RequestParams):
+		for i in range(len(RequestParams)):	
+			if RequestParams[i].get('Name') is not None:
+				self.add_body_params('RequestParam.' + str(i + 1) + '.Name' , RequestParams[i].get('Name'))
+			if RequestParams[i].get('Type') is not None:
+				self.add_body_params('RequestParam.' + str(i + 1) + '.Type' , RequestParams[i].get('Type'))
+			if RequestParams[i].get('Required') is not None:
+				self.add_body_params('RequestParam.' + str(i + 1) + '.Required' , RequestParams[i].get('Required'))
+			if RequestParams[i].get('Desc') is not None:
+				self.add_body_params('RequestParam.' + str(i + 1) + '.Desc' , RequestParams[i].get('Desc'))
+			if RequestParams[i].get('Example') is not None:
+				self.add_body_params('RequestParam.' + str(i + 1) + '.Example' , RequestParams[i].get('Example'))
+
+
+	def get_FolderId(self):
+		return self.get_query_params().get('FolderId')
+
+	def set_FolderId(self,FolderId):
+		self.add_query_param('FolderId',FolderId)
+
+	def get_TemplateSql(self):
+		return self.get_query_params().get('TemplateSql')
+
+	def set_TemplateSql(self,TemplateSql):
+		self.add_query_param('TemplateSql',TemplateSql)
+
+	def get_Desc(self):
+		return self.get_query_params().get('Desc')
+
+	def set_Desc(self,Desc):
+		self.add_query_param('Desc',Desc)
---
+++ aliyun-python-sdk-iot/aliyunsdkiot/request/v20180120/InvokeDataAPIServiceRequest.py
@@ -0,0 +1,45 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class InvokeDataAPIServiceRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Iot', '2018-01-20', 'InvokeDataAPIService','iot')
+
+	def get_ApiSrn(self):
+		return self.get_query_params().get('ApiSrn')
+
+	def set_ApiSrn(self,ApiSrn):
+		self.add_query_param('ApiSrn',ApiSrn)
+
+	def get_Params(self):
+		return self.get_body_params().get('Params')
+
+	def set_Params(self,Params):
+		for i in range(len(Params)):	
+			if Params[i].get('ListParamType') is not None:
+				self.add_body_params('Param.' + str(i + 1) + '.ListParamType' , Params[i].get('ListParamType'))
+			for j in range(len(Params[i].get('ListParamValues'))):
+				if Params[i].get('ListParamValues')[j] is not None:
+					self.add_body_params('Param.' + str(i + 1) + '.ListParamValue.'+str(j + 1), Params[i].get('ListParamValues')[j])
+			if Params[i].get('ParamValue') is not None:
+				self.add_body_params('Param.' + str(i + 1) + '.ParamValue' , Params[i].get('ParamValue'))
+			if Params[i].get('ParamName') is not None:
+				self.add_body_params('Param.' + str(i + 1) + '.ParamName' , Params[i].get('ParamName'))
