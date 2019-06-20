f305a29f7a016c5b923e0aeccbff1392e1c0d963
wb-hx510875 <wb-hx510875@alibaba-inc.com>
2019-06-20 15:17:33 +0800

    feature: supported endpoint rules

---
+++ aliyun-python-sdk-core/aliyunsdkcore/endpoint/endpoint_resolver_rules.py
@@ -0,0 +1,65 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with self work for additional information
+# regarding copyright ownership.  The ASF licenses self file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use self file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+from aliyunsdkcore.endpoint.endpoint_resolver_base import EndpointResolverBase
+
+
+class EndpointResolverRules():
+    def __init__(self, *args, **kwargs):
+        self.product_code_valid = False
+        self.region_id_valid = False
+        self.endpoint_data = None
+
+    def set_endpoint_data(self, endpoint_data):
+        self.endpoint_data = endpoint_data
+    
+    def get_endpoint_data(self):
+        return self.endpoint_data
+
+    def import_endpoint_data(self, request):
+        return __import__(
+            '.'.join(['aliyunsdk'+request.product_code,
+                      'endpoint', 'endpoint_data']), globals(), locals(),
+            ['endpoint', 'end'], 0)
+
+    def resolve(self, request):
+        try:
+            if self.endpoint_data == None:
+                endpoint_data = self.import_endpoint_data(request)
+                self.set_endpoint_data(endpoint_data)
+        except ModuleNotFoundError:
+            return None
+        else:
+            endpoint_data = self.get_endpoint_data()
+            endpoint = endpoint_data.getEndpoint(
+                request.region_id, request.request_network)
+            if endpoint != None and endpoint != "":
+                self.product_code_valid = True
+                self.region_id_valid = True
+                return endpoint
+            return None
+
+    def is_product_code_valid(self, request):
+        return self.product_code_valid
+
+    def is_region_id_valid(self, request):
+        return self.region_id_valid
+
+    def get_valid_region_ids_by_product(self, product_code):
+        return []
---
+++ aliyun-python-sdk-core/tests/endpoint/test_endpoint_resolver_rules.py
@@ -0,0 +1,98 @@
+
+from tests import unittest
+
+from mock import MagicMock, patch, Mock
+
+from aliyunsdkcore.endpoint.endpoint_resolver_rules import EndpointResolverRules
+from aliyunsdkcore.endpoint.resolver_endpoint_request import ResolveEndpointRequest
+from aliyunsdkcore.acs_exception.exceptions import ClientException
+
+
+class TestDefaultEndpointResolver(unittest.TestCase):
+    def test_resolver(self):
+        # clinet = Mock()
+        resolver = EndpointResolverRules(None)
+        # can not be resolved
+        request = ResolveEndpointRequest("foo", "test", "", "", "public")
+        endpoint = resolver.resolve(request)
+        self.assertEqual(None, endpoint)
+
+    def test_resolver_has_endpoint(self):
+        resolver = EndpointResolverRules(None)
+        request = ResolveEndpointRequest("foo", "test", "", "", "public")
+        endpoint_data = EndpointData()
+        resolver.set_endpoint_data(endpoint_data)
+        endpoint = resolver.resolve(request)
+        self.assertEqual("bar", endpoint)
+
+    def test_resolver_reginoal(self):
+        resolver = EndpointResolverRules(None)
+        endpoint_data = EndpointData()
+        resolver.set_endpoint_data(endpoint_data)
+        request = ResolveEndpointRequest(
+            "cn-hangzhou", "test", "", "", "public")
+        endpoint = resolver.resolve(request)
+        self.assertEqual("test.cn-hangzhou.aliyuncs.com", endpoint)
+    
+    def test_resolver_central(self):
+        resolver = EndpointResolverRules(None)
+        endpoint_data = EndpointData()
+        endpoint_data.endpoint_regional = "central"
+        resolver.set_endpoint_data(endpoint_data)
+        request = ResolveEndpointRequest(
+            "cn-hangzhou", "test", "", "", "public")
+        endpoint = resolver.resolve(request)
+        self.assertEqual("test.aliyuncs.com", endpoint)
+
+    def test_resolver_network(self):
+        resolver = EndpointResolverRules(None)
+        endpoint_data = EndpointData()
+        resolver.set_endpoint_data(endpoint_data)
+        request = ResolveEndpointRequest(
+            "cn-hangzhou", "test", "", "", "vpc")
+        endpoint = resolver.resolve(request)
+        self.assertEqual("test-vpc.cn-hangzhou.aliyuncs.com", endpoint)
+
+
+class EndpointData():
+    def __init__(self):
+        self.endpoint_map = None
+        self.endpoint_regional = None
+        self.iniEndpointData()
+
+    def getEndpointMap(self):
+        return self.endpoint_map
+
+    def getEndpointRegional(self):
+        return self.endpoint_regional
+
+    def iniEndpointData(self):
+        self.endpoint_map = {
+            "foo": "bar",
+        }
+        self.endpoint_regional = "regional"
+
+    def getEndpoint(self, region_id, network=None):
+        if(network == None or network == ""):
+            network = "public"
+
+        endpoint_map = self.getEndpointMap()
+        endpoint_regional = self.getEndpointRegional()
+        endpoint = ""
+        for key in endpoint_map:
+            if (key == region_id):
+                endpoint = endpoint_map[key]
+                break
+
+        if endpoint == "":
+            if endpoint_regional == "regional":
+                endpoint = "test<network>.<region_id>.aliyuncs.com"
+            else:
+                endpoint = "test<network>.aliyuncs.com"
+            if network != "public":
+                endpoint = endpoint.replace("<network>", "-"+network)
+            else:
+                endpoint = endpoint.replace("<network>", "")
+            endpoint = endpoint.replace("<region_id>", region_id.lower())
+
+        return endpoint
