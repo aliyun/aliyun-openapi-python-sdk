1d806283a51dab02bc52ceee95995f72e5767314
sdk-team <sdk-team@alibabacloud.com>
2019-07-18 20:24:44 +0800

    Add CreateInstance.

---
+++ aliyun-python-sdk-ens/aliyunsdkens/request/v20171110/DescribeImagesRequest.py
@@ -0,0 +1,72 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class DescribeImagesRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Ens', '2017-11-10', 'DescribeImages','ens')
+
+	def get_product(self):
+		return self.get_query_params().get('product')
+
+	def set_product(self,product):
+		self.add_query_param('product',product)
+
+	def get_ImageId(self):
+		return self.get_query_params().get('ImageId')
+
+	def set_ImageId(self,ImageId):
+		self.add_query_param('ImageId',ImageId)
+
+	def get_ImageName(self):
+		return self.get_query_params().get('ImageName')
+
+	def set_ImageName(self,ImageName):
+		self.add_query_param('ImageName',ImageName)
+
+	def get_EnsRegionId(self):
+		return self.get_query_params().get('EnsRegionId')
+
+	def set_EnsRegionId(self,EnsRegionId):
+		self.add_query_param('EnsRegionId',EnsRegionId)
+
+	def get_PageSize(self):
+		return self.get_query_params().get('PageSize')
+
+	def set_PageSize(self,PageSize):
+		self.add_query_param('PageSize',PageSize)
+
+	def get_Version(self):
+		return self.get_query_params().get('Version')
+
+	def set_Version(self,Version):
+		self.add_query_param('Version',Version)
+
+	def get_PageNumber(self):
+		return self.get_query_params().get('PageNumber')
+
+	def set_PageNumber(self,PageNumber):
+		self.add_query_param('PageNumber',PageNumber)
+
+	def get_Status(self):
+		return self.get_query_params().get('Status')
+
+	def set_Status(self,Status):
+		self.add_query_param('Status',Status)
