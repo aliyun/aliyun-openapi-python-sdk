7c8d88bc74536510e44db781800a0706af57af00
sdk-team <sdk-team@alibabacloud.com>
2019-07-19 17:51:24 +0800

    QueryMonthlyBillResponse add roundDownDiscount.

--- aliyun-python-sdk-bssopenapi/aliyunsdkbssopenapi/request/v20171214/ConvertChargeTypeRequest.py
+++ aliyun-python-sdk-bssopenapi/aliyunsdkbssopenapi/request/v20171214/ConvertChargeTypeRequest.py
@@ -6,8 +6,8 @@
 # "License"); you may not use this file except in compliance
 # with the License.  You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
 #
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 #
 # Unless required by applicable law or agreed to in writing,
@@ -17,11 +17,19 @@
 # specific language governing permissions and limitations
 # under the License.
 
+from aliyunsdkbssopenapi.endpoint import endpoint_data
 from aliyunsdkcore.request import RpcRequest
+
+
 class ConvertChargeTypeRequest(RpcRequest):
 
 	def __init__(self):
 		RpcRequest.__init__(self, 'BssOpenApi', '2017-12-14', 'ConvertChargeType')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
 
 	def get_Period(self):
 		return self.get_query_params().get('Period')
