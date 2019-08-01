8bbdc195f5dc45179cdcdfb083b6118d0bbd3738
sdk-team <sdk-team@alibabacloud.com>
2019-08-01 19:55:52 +0800

    Generated 2017-08-01 for polardb.

--- aliyun-python-sdk-polardb/aliyunsdkpolardb/request/v20170801/ModifyAutoRenewAttributeRequest.py
+++ aliyun-python-sdk-polardb/aliyunsdkpolardb/request/v20170801/ModifyAutoRenewAttributeRequest.py
@@ -22,11 +22,6 @@ class ModifyAutoRenewAttributeRequest(RpcRequest):
 
 	def __init__(self):
 		RpcRequest.__init__(self, 'polardb', '2017-08-01', 'ModifyAutoRenewAttribute','polardb')
-		if hasattr(self, "endpoint_map"):
-			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
-		if hasattr(self, "endpoint_regional"):
-			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
-
 
 	def get_Duration(self):
 		return self.get_query_params().get('Duration')
