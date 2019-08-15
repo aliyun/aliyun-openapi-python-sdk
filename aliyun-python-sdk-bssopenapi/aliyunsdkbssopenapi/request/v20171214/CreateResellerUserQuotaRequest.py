fe4ae1c1ca4f71cb7447100d3768d095d4204c21
sdk-team <sdk-team@alibabacloud.com>
2019-08-15 10:48:49 +0800

    QueryInstanceBillResponse add ServicePeriod.

--- aliyun-python-sdk-bssopenapi/aliyunsdkbssopenapi/request/v20171214/CreateResellerUserQuotaRequest.py
+++ aliyun-python-sdk-bssopenapi/aliyunsdkbssopenapi/request/v20171214/CreateResellerUserQuotaRequest.py
@@ -23,7 +23,7 @@ from aliyunsdkbssopenapi.endpoint import endpoint_data
 class CreateResellerUserQuotaRequest(RpcRequest):
 
 	def __init__(self):
-		RpcRequest.__init__(self, 'BssOpenApi', '2017-12-14', 'CreateResellerUserQuota','bssopenapi')
+		RpcRequest.__init__(self, 'BssOpenApi', '2017-12-14', 'CreateResellerUserQuota')
 		if hasattr(self, "endpoint_map"):
 			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
 		if hasattr(self, "endpoint_regional"):
