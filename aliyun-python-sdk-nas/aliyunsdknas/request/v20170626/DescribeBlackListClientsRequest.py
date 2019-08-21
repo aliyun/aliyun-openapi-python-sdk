0923df3a72cd9a8fc7239032c12fc0c5a2948048
sdk-team <sdk-team@alibabacloud.com>
2019-08-21 21:45:09 +0800

    Supported for CPFS new feature of client evict.

--- aliyun-python-sdk-nas/aliyunsdknas/request/v20170626/DescribeBlackListClientsRequest.py
+++ aliyun-python-sdk-nas/aliyunsdknas/request/v20170626/DescribeBlackListClientsRequest.py
@@ -6,8 +6,8 @@
 # "License"); you may not use this file except in compliance
 # with the License.  You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
 #
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 #
 # Unless required by applicable law or agreed to in writing,
@@ -18,13 +18,26 @@
 # under the License.
 
 from aliyunsdkcore.request import RpcRequest
-class CPFSDeleteFileSystemRequest(RpcRequest):
+from aliyunsdknas.endpoint import endpoint_data
+
+class DescribeBlackListClientsRequest(RpcRequest):
 
 	def __init__(self):
-		RpcRequest.__init__(self, 'NAS', '2017-06-26', 'CPFSDeleteFileSystem','nas')
+		RpcRequest.__init__(self, 'NAS', '2017-06-26', 'DescribeBlackListClients','nas')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
 
-	def get_FsId(self):
-		return self.get_query_params().get('FsId')
 
-	def set_FsId(self,FsId):
-		self.add_query_param('FsId',FsId)
+	def get_ClientIP(self):
+		return self.get_query_params().get('ClientIP')
+
+	def set_ClientIP(self,ClientIP):
+		self.add_query_param('ClientIP',ClientIP)
+
+	def get_FileSystemId(self):
+		return self.get_query_params().get('FileSystemId')
+
+	def set_FileSystemId(self,FileSystemId):
+		self.add_query_param('FileSystemId',FileSystemId)
