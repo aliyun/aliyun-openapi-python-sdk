9eea24c5768879f5464c04ebd08bf05667390525
wb-hx510875 <wb-hx510875@alibaba-inc.com>
2019-08-28 14:35:29 +0800

    fix endpoint rules condition

--- aliyun-python-sdk-core/aliyunsdkcore/endpoint/endpoint_resolver_rules.py
+++ aliyun-python-sdk-core/aliyunsdkcore/endpoint/endpoint_resolver_rules.py
@@ -28,8 +28,10 @@ class EndpointResolverRules():
         self.product_suffix = ''
 
     def resolve(self, request):
-        if self.endpoint_map is None or self.endpoint_regional is None:
+        if request.endpoint_map is None or request.endpoint_regional is None:
             return None
+        self.endpoint_map = request.endpoint_map
+        self.endpoint_regional = request.endpoint_regional
         product_id = request.product_code_lower
         region_id = request.region_id
         network = request.request_network
