3811cbe65430aa468eeeab0300298dcd606314ec
sdk-team <sdk-team@alibabacloud.com>
2019-11-12 22:41:02 +0800

    Generated 2018-08-21, 2018-08-16 for Rdc.

---
+++ aliyun-python-sdk-rdc/aliyunsdkrdc/request/v20180816/CreateProjectRequest.py
@@ -0,0 +1,56 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkrdc.endpoint import endpoint_data
+
+class CreateProjectRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Rdc', '2018-08-16', 'CreateProject','rdc')
+		self.set_method('POST')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+
+	def get_CorpIdentifier(self):
+		return self.get_query_params().get('CorpIdentifier')
+
+	def set_CorpIdentifier(self,CorpIdentifier):
+		self.add_query_param('CorpIdentifier',CorpIdentifier)
+
+	def get_ParamJson(self):
+		return self.get_body_params().get('ParamJson')
+
+	def set_ParamJson(self,ParamJson):
+		self.add_body_params('ParamJson', ParamJson)
+
+	def get_Region(self):
+		return self.get_body_params().get('Region')
+
+	def set_Region(self,Region):
+		self.add_body_params('Region', Region)
+
+	def get_StaffId(self):
+		return self.get_body_params().get('StaffId')
+
+	def set_StaffId(self,StaffId):
+		self.add_body_params('StaffId', StaffId)
