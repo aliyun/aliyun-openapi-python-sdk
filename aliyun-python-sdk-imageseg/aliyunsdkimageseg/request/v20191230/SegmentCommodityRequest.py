89482b9c9812100ac07f94af99fd7709beb913e9
sdk-team <sdk-team@alibabacloud.com>
2019-12-29 19:18:05 +0800

    Add new api.

--- aliyun-python-sdk-imageseg/aliyunsdkimageseg/request/v20191230/SegmentCommodityRequest.py
+++ aliyun-python-sdk-imageseg/aliyunsdkimageseg/request/v20191230/SegmentCommodityRequest.py
@@ -20,10 +20,10 @@
 from aliyunsdkcore.request import RpcRequest
 from aliyunsdkimageseg.endpoint import endpoint_data
 
-class SegmentCommonImageRequest(RpcRequest):
+class SegmentCommodityRequest(RpcRequest):
 
 	def __init__(self):
-		RpcRequest.__init__(self, 'imageseg', '2019-12-30', 'SegmentCommonImage','imageseg')
+		RpcRequest.__init__(self, 'imageseg', '2019-12-30', 'SegmentCommodity','imageseg')
 		if hasattr(self, "endpoint_map"):
 			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
 		if hasattr(self, "endpoint_regional"):
