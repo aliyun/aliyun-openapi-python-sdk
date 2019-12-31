75e54472e0d82448ed30df28d41cc70fd22a1381
sdk-team <sdk-team@alibabacloud.com>
2019-12-31 13:56:20 +0800

    Supported image for process.

---
+++ aliyun-python-sdk-imm/aliyunsdkimm/request/v20170906/CreateImageProcessTaskRequest.py
@@ -0,0 +1,55 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+
+class CreateImageProcessTaskRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'imm', '2017-09-06', 'CreateImageProcessTask','imm')
+
+	def get_Project(self):
+		return self.get_query_params().get('Project')
+
+	def set_Project(self,Project):
+		self.add_query_param('Project',Project)
+
+	def get_NotifyEndpoint(self):
+		return self.get_query_params().get('NotifyEndpoint')
+
+	def set_NotifyEndpoint(self,NotifyEndpoint):
+		self.add_query_param('NotifyEndpoint',NotifyEndpoint)
+
+	def get_NotifyTopicName(self):
+		return self.get_query_params().get('NotifyTopicName')
+
+	def set_NotifyTopicName(self,NotifyTopicName):
+		self.add_query_param('NotifyTopicName',NotifyTopicName)
+
+	def get_TargetList(self):
+		return self.get_query_params().get('TargetList')
+
+	def set_TargetList(self,TargetList):
+		self.add_query_param('TargetList',TargetList)
+
+	def get_ImageUri(self):
+		return self.get_query_params().get('ImageUri')
+
+	def set_ImageUri(self,ImageUri):
+		self.add_query_param('ImageUri',ImageUri)
---
+++ aliyun-python-sdk-imm/aliyunsdkimm/request/v20170906/GetOfficePreviewURLRequest.py
@@ -0,0 +1,55 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+
+class GetOfficePreviewURLRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'imm', '2017-09-06', 'GetOfficePreviewURL','imm')
+
+	def get_SrcType(self):
+		return self.get_query_params().get('SrcType')
+
+	def set_SrcType(self,SrcType):
+		self.add_query_param('SrcType',SrcType)
+
+	def get_Expires(self):
+		return self.get_query_params().get('Expires')
+
+	def set_Expires(self,Expires):
+		self.add_query_param('Expires',Expires)
+
+	def get_Project(self):
+		return self.get_query_params().get('Project')
+
+	def set_Project(self,Project):
+		self.add_query_param('Project',Project)
+
+	def get_UseHTTPS(self):
+		return self.get_query_params().get('UseHTTPS')
+
+	def set_UseHTTPS(self,UseHTTPS):
+		self.add_query_param('UseHTTPS',UseHTTPS)
+
+	def get_SrcUri(self):
+		return self.get_query_params().get('SrcUri')
+
+	def set_SrcUri(self,SrcUri):
+		self.add_query_param('SrcUri',SrcUri)
