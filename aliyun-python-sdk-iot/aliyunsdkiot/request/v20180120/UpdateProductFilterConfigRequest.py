490f9829a75dd096021e9d5fba22f092f93c2edb
sdk-team <sdk-team@alibabacloud.com>
2019-12-31 23:12:31 +0800

    Add OTA APIs, including GenerateOTAUploadURL, CreateOTAFirmware, etc.

---
+++ aliyun-python-sdk-iot/aliyunsdkiot/request/v20180120/UpdateProductFilterConfigRequest.py
@@ -0,0 +1,61 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkiot.endpoint import endpoint_data
+
+class UpdateProductFilterConfigRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Iot', '2018-01-20', 'UpdateProductFilterConfig','iot')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+
+	def get_PropertyTimestampFilter(self):
+		return self.get_query_params().get('PropertyTimestampFilter')
+
+	def set_PropertyTimestampFilter(self,PropertyTimestampFilter):
+		self.add_query_param('PropertyTimestampFilter',PropertyTimestampFilter)
+
+	def get_ProductKey(self):
+		return self.get_query_params().get('ProductKey')
+
+	def set_ProductKey(self,ProductKey):
+		self.add_query_param('ProductKey',ProductKey)
+
+	def get_ApiProduct(self):
+		return self.get_body_params().get('ApiProduct')
+
+	def set_ApiProduct(self,ApiProduct):
+		self.add_body_params('ApiProduct', ApiProduct)
+
+	def get_ApiRevision(self):
+		return self.get_body_params().get('ApiRevision')
+
+	def set_ApiRevision(self,ApiRevision):
+		self.add_body_params('ApiRevision', ApiRevision)
+
+	def get_PropertyValueFilter(self):
+		return self.get_query_params().get('PropertyValueFilter')
+
+	def set_PropertyValueFilter(self,PropertyValueFilter):
+		self.add_query_param('PropertyValueFilter',PropertyValueFilter)
