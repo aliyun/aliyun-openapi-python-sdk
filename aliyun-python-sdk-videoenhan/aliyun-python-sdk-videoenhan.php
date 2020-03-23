412b74155d05b5627b42d62211bed8ffdfd4dd96
sdk-team <sdk-team@alibabacloud.com>
2020-03-23 10:30:40 +0800

    SDK versioin 100.

---
+++ aliyun-python-sdk-videoenhan/aliyun-python-sdk-videoenhan.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace AlibabaCloud\aliyun-python-sdk-videoenhan;
+
+use AlibabaCloud\Client\Resolver\VersionResolver;
+
+/**
+ */
+class aliyun-python-sdk-videoenhan extends VersionResolver
+{
+}
