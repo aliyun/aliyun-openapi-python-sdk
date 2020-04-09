848e972b53cb89dae5ba1ea8634946140fcde439
sdk-team <sdk-team@alibabacloud.com>
2020-04-09 11:03:44 +0800

    WAF OpenApi SDK Release.

---
+++ aliyun-python-sdk-waf-openapi/aliyunsdkwaf_openapi/request/v20190910/DescribeInstanceInfoRequest.py
@@ -0,0 +1,49 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkwaf_openapi.endpoint import endpoint_data
+
+class DescribeInstanceInfoRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'waf-openapi', '2019-09-10', 'DescribeInstanceInfo','waf')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+
+	def get_ResourceGroupId(self):
+		return self.get_query_params().get('ResourceGroupId')
+
+	def set_ResourceGroupId(self,ResourceGroupId):
+		self.add_query_param('ResourceGroupId',ResourceGroupId)
+
+	def get_InstanceId(self):
+		return self.get_query_params().get('InstanceId')
+
+	def set_InstanceId(self,InstanceId):
+		self.add_query_param('InstanceId',InstanceId)
+
+	def get_InstanceSource(self):
+		return self.get_query_params().get('InstanceSource')
+
+	def set_InstanceSource(self,InstanceSource):
+		self.add_query_param('InstanceSource',InstanceSource)
