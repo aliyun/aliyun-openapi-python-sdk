79119d361d64f401aad3fcd96cd7582c8088a454
sdk-team <sdk-team@alibabacloud.com>
2020-04-27 20:40:00 +0800

    Support watermark.

--- aliyun-python-sdk-imm/aliyunsdkimm/request/v20170906/GetOfficePreviewURLRequest.py
+++ aliyun-python-sdk-imm/aliyunsdkimm/request/v20170906/GetOfficePreviewURLRequest.py
@@ -36,14 +36,50 @@ class GetOfficePreviewURLRequest(RpcRequest):
 	def set_Project(self,Project):
 		self.add_query_param('Project',Project)
 
-	def get_UseOldURL(self):
-		return self.get_query_params().get('UseOldURL')
+	def get_WatermarkVertical(self):
+		return self.get_query_params().get('WatermarkVertical')
 
-	def set_UseOldURL(self,UseOldURL):
-		self.add_query_param('UseOldURL',UseOldURL)
+	def set_WatermarkVertical(self,WatermarkVertical):
+		self.add_query_param('WatermarkVertical',WatermarkVertical)
+
+	def get_WatermarkType(self):
+		return self.get_query_params().get('WatermarkType')
+
+	def set_WatermarkType(self,WatermarkType):
+		self.add_query_param('WatermarkType',WatermarkType)
+
+	def get_WatermarkRotate(self):
+		return self.get_query_params().get('WatermarkRotate')
+
+	def set_WatermarkRotate(self,WatermarkRotate):
+		self.add_query_param('WatermarkRotate',WatermarkRotate)
+
+	def get_WatermarkValue(self):
+		return self.get_query_params().get('WatermarkValue')
+
+	def set_WatermarkValue(self,WatermarkValue):
+		self.add_query_param('WatermarkValue',WatermarkValue)
+
+	def get_WatermarkFont(self):
+		return self.get_query_params().get('WatermarkFont')
+
+	def set_WatermarkFont(self,WatermarkFont):
+		self.add_query_param('WatermarkFont',WatermarkFont)
+
+	def get_WatermarkHorizontal(self):
+		return self.get_query_params().get('WatermarkHorizontal')
+
+	def set_WatermarkHorizontal(self,WatermarkHorizontal):
+		self.add_query_param('WatermarkHorizontal',WatermarkHorizontal)
 
 	def get_SrcUri(self):
 		return self.get_query_params().get('SrcUri')
 
 	def set_SrcUri(self,SrcUri):
 		self.add_query_param('SrcUri',SrcUri)
+
+	def get_WatermarkFillStyle(self):
+		return self.get_query_params().get('WatermarkFillStyle')
+
+	def set_WatermarkFillStyle(self,WatermarkFillStyle):
+		self.add_query_param('WatermarkFillStyle',WatermarkFillStyle)
