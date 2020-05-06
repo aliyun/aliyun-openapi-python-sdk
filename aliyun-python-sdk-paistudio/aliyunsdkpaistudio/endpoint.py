76f8a2a0719e03ea86ee0f52024c7981fc0c54bb
sdk-team <sdk-team@alibabacloud.com>
2020-05-06 15:35:01 +0800

    Init.

---
+++ aliyun-python-sdk-paistudio/aliyunsdkpaistudio/endpoint.py
@@ -0,0 +1,49 @@
+# # Licensed to the Apache Software Foundation (ASF) under one
+# # or more contributor license agreements.  See the NOTICE file
+# # distributed with this work for additional information
+# # regarding copyright ownership.  The ASF licenses this file
+# # to you under the Apache License, Version 2.0 (the
+# # "License"); you may not use this file except in compliance
+# # with the License.  You may obtain a copy of the License at
+# #
+# #
+# #     http://www.apache.org/licenses/LICENSE-2.0
+# #
+# #
+# # Unless required by applicable law or agreed to in writing,
+# # software distributed under the License is distributed on an
+# # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# # KIND, either express or implied.  See the License for the
+# # specific language governing permissions and limitations
+# # under the License.
+
+
+class EndpointData():
+	def __init__(self):
+		self.endpoint_map = {
+			"cn-shenzhen": "pai.cn-shenzhen.aliyuncs.com",
+			"cn-beijing": "pai.cn-beijing.aliyuncs.com",
+			"ap-south-1": "pai.ap-south-1.aliyuncs.com",
+			"me-east-1": "pai.me-east-1.aliyuncs.com",
+			"cn-qingdao": "pai.cn-qingdao.aliyuncs.com",
+			"cn-shanghai": "pai.cn-shanghai.aliyuncs.com",
+			"cn-hongkong": "pai.cn-hongkong.aliyuncs.com",
+			"ap-southeast-1": "pai.ap-southeast-1.aliyuncs.com",
+			"ap-southeast-2": "pai.ap-southeast-2.aliyuncs.com",
+			"ap-southeast-3": "pai.ap-southeast-3.aliyuncs.com",
+			"eu-central-1": "pai.eu-central-1.aliyuncs.com",
+			"ap-southeast-5": "pai.ap-southeast-5.aliyuncs.com",
+			"cn-zhangjiakou": "pai.cn-zhangjiakou.aliyuncs.com",
+			"us-west-1": "pai.us-west-1.aliyuncs.com",
+			"cn-hangzhou": "pai-cn-hangzhou.data.aliyun.com",
+		}
+		self.endpoint_regional = "regional"
+
+	def getEndpointMap(self):
+		return self.endpoint_map
+
+	def getEndpointRegional(self):
+		return self.endpoint_regional
+
+
+endpoint_data = EndpointData()
