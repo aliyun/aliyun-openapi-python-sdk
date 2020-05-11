ae9d51b2d92359023a626803898213654399a6c7
824750130 <e824750130@qq.com>
2020-05-11 09:46:37 +0800

    fix functional test

--- python-sdk-functional-test/credentials_test.py
+++ python-sdk-functional-test/credentials_test.py
@@ -2,13 +2,13 @@
 import json
 import os
 
-# from aliyunsdkcore.acs_exception.exceptions import ClientException
-# from aliyunsdkcore.acs_exception.exceptions import ServerException
+from aliyunsdkcore.acs_exception.exceptions import ClientException
+from aliyunsdkcore.acs_exception.exceptions import ServerException
 from aliyunsdkcore.client import AcsClient
-# from aliyunsdkcore.auth.credentials import StsTokenCredential
+from aliyunsdkcore.auth.credentials import StsTokenCredential
 
 from aliyunsdkecs.request.v20140526.DescribeRegionsRequest import DescribeRegionsRequest
-# from aliyunsdksts.request.v20150401.AssumeRoleRequest import AssumeRoleRequest
+from aliyunsdksts.request.v20150401.AssumeRoleRequest import AssumeRoleRequest
 
 from base import SDKTestBase
 from base import disabled
@@ -23,56 +23,56 @@ class CredentialsTest(SDKTestBase):
         _, url = signer.sign(client.get_region_id(), request)
         return url
 
-    # def test_call_rpc_request_with_sts_token(self):
-    #     client = self.init_sub_client()
-    #     self._create_default_ram_role()
-    #
-    #     request = AssumeRoleRequest()
-    #     request.set_RoleArn(self.ram_role_arn)
-    #     request.set_RoleSessionName(self.default_role_session_name)
-    #     response = client.do_action_with_exception(request)
-    #     response = self.get_dict_response(response)
-    #     credentials = response.get("Credentials")
-    #
-    #     # Using temporary AK + STS for authentication
-    #     sts_token_credential = StsTokenCredential(
-    #         credentials.get("AccessKeyId"),
-    #         credentials.get("AccessKeySecret"),
-    #         credentials.get("SecurityToken")
-    #     )
-    #     acs_client = AcsClient(
-    #         region_id=self.region_id,
-    #         credential=sts_token_credential)
-    #     request = DescribeRegionsRequest()
-    #     url = self.get_http_request(acs_client, request)
-    #     self.assertTrue(url.find("AccessKeyId=STS."))
-    #     response = acs_client.do_action_with_exception(request)
-    #     ret = self.get_dict_response(response)
-    #     self.assertTrue(ret.get("Regions"))
-    #     self.assertTrue(ret.get("RequestId"))
+    def test_call_rpc_request_with_sts_token(self):
+        client = self.init_sub_client()
+        self._create_default_ram_role()
 
-    # def test_call_roa_request_with_sts_token(self):
-    #     from aliyunsdkcore.auth.credentials import RamRoleArnCredential
-    #     self._create_default_ram_user()
-    #     self._attach_default_policy()
-    #     self._create_access_key()
-    #     self._create_default_ram_role()
-    #
-    #     ram_role_arn_credential = RamRoleArnCredential(
-    #         self.ram_user_access_key_id,
-    #         self.ram_user_access_key_secret,
-    #         self.ram_role_arn,
-    #         "alice_test")
-    #     acs_client = AcsClient(
-    #         region_id="cn-hangzhou",
-    #         credential=ram_role_arn_credential)
-    #     request = DescribeRegionsRequest()
-    #     url = self.get_http_request(acs_client, request)
-    #     self.assertTrue(url.find("AccessKeyId=STS."))
-    #     response = acs_client.do_action_with_exception(request)
-    #     ret = self.get_dict_response(response)
-    #     self.assertTrue(ret.get("Regions"))
-    #     self.assertTrue(ret.get("RequestId"))
+        request = AssumeRoleRequest()
+        request.set_RoleArn(self.ram_role_arn)
+        request.set_RoleSessionName(self.default_role_session_name)
+        response = client.do_action_with_exception(request)
+        response = self.get_dict_response(response)
+        credentials = response.get("Credentials")
+
+        # Using temporary AK + STS for authentication
+        sts_token_credential = StsTokenCredential(
+            credentials.get("AccessKeyId"),
+            credentials.get("AccessKeySecret"),
+            credentials.get("SecurityToken")
+        )
+        acs_client = AcsClient(
+            region_id=self.region_id,
+            credential=sts_token_credential)
+        request = DescribeRegionsRequest()
+        url = self.get_http_request(acs_client, request)
+        self.assertTrue(url.find("AccessKeyId=STS."))
+        response = acs_client.do_action_with_exception(request)
+        ret = self.get_dict_response(response)
+        self.assertTrue(ret.get("Regions"))
+        self.assertTrue(ret.get("RequestId"))
+
+    def test_call_roa_request_with_sts_token(self):
+        from aliyunsdkcore.auth.credentials import RamRoleArnCredential
+        self._create_default_ram_user()
+        self._attach_default_policy()
+        self._create_access_key()
+        self._create_default_ram_role()
+
+        ram_role_arn_credential = RamRoleArnCredential(
+            self.ram_user_access_key_id,
+            self.ram_user_access_key_secret,
+            self.ram_role_arn,
+            "alice_test")
+        acs_client = AcsClient(
+            region_id="cn-hangzhou",
+            credential=ram_role_arn_credential)
+        request = DescribeRegionsRequest()
+        url = self.get_http_request(acs_client, request)
+        self.assertTrue(url.find("AccessKeyId=STS."))
+        response = acs_client.do_action_with_exception(request)
+        ret = self.get_dict_response(response)
+        self.assertTrue(ret.get("Regions"))
+        self.assertTrue(ret.get("RequestId"))
 
     @disabled
     def test_ecs_ram_role(self):
