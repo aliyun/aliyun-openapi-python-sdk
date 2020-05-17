a69909366cbf1376f10ccffd9ef46eb963d76aff
sdk-team <sdk-team@alibabacloud.com>
2020-05-17 11:28:06 +0800

    Open API publish.

--- aliyun-python-sdk-vcs/aliyunsdkvcs/request/v20200515/DeleteDeviceRequest.py
+++ aliyun-python-sdk-vcs/aliyunsdkvcs/request/v20200515/DeleteDeviceRequest.py
@@ -23,7 +23,7 @@ from aliyunsdkvcs.endpoint import endpoint_data
 class DeleteDeviceRequest(RpcRequest):
 
 	def __init__(self):
-		RpcRequest.__init__(self, 'Vcs', '2020-05-15', 'DeleteDevice','VCS')
+		RpcRequest.__init__(self, 'Vcs', '2020-05-15', 'DeleteDevice','vcs')
 		self.set_method('POST')
 		if hasattr(self, "endpoint_map"):
 			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
