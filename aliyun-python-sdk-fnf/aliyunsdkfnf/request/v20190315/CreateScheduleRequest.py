51fd45951c785ec3453dd2fa0d50814339a4e44a
sdk-team <sdk-team@alibabacloud.com>
2020-05-22 11:01:30 +0800

    Add schedule crud api.

---
+++ aliyun-python-sdk-fnf/aliyunsdkfnf/request/v20190315/CreateScheduleRequest.py
@@ -0,0 +1,74 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkfnf.endpoint import endpoint_data
+
+class CreateScheduleRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'fnf', '2019-03-15', 'CreateSchedule')
+		self.set_method('POST')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+
+	def get_ScheduleName(self):
+		return self.get_body_params().get('ScheduleName')
+
+	def set_ScheduleName(self,ScheduleName):
+		self.add_body_params('ScheduleName', ScheduleName)
+
+	def get_CronExpression(self):
+		return self.get_body_params().get('CronExpression')
+
+	def set_CronExpression(self,CronExpression):
+		self.add_body_params('CronExpression', CronExpression)
+
+	def get_Payload(self):
+		return self.get_body_params().get('Payload')
+
+	def set_Payload(self,Payload):
+		self.add_body_params('Payload', Payload)
+
+	def get_RequestId(self):
+		return self.get_query_params().get('RequestId')
+
+	def set_RequestId(self,RequestId):
+		self.add_query_param('RequestId',RequestId)
+
+	def get_Enable(self):
+		return self.get_body_params().get('Enable')
+
+	def set_Enable(self,Enable):
+		self.add_body_params('Enable', Enable)
+
+	def get_Description(self):
+		return self.get_body_params().get('Description')
+
+	def set_Description(self,Description):
+		self.add_body_params('Description', Description)
+
+	def get_FlowName(self):
+		return self.get_body_params().get('FlowName')
+
+	def set_FlowName(self,FlowName):
+		self.add_body_params('FlowName', FlowName)
---
+++ aliyun-python-sdk-fnf/aliyunsdkfnf/request/v20190315/ListSchedulesRequest.py
@@ -0,0 +1,56 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkfnf.endpoint import endpoint_data
+
+class ListSchedulesRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'fnf', '2019-03-15', 'ListSchedules')
+		self.set_method('GET')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+
+	def get_NextToken(self):
+		return self.get_query_params().get('NextToken')
+
+	def set_NextToken(self,NextToken):
+		self.add_query_param('NextToken',NextToken)
+
+	def get_RequestId(self):
+		return self.get_query_params().get('RequestId')
+
+	def set_RequestId(self,RequestId):
+		self.add_query_param('RequestId',RequestId)
+
+	def get_Limit(self):
+		return self.get_query_params().get('Limit')
+
+	def set_Limit(self,Limit):
+		self.add_query_param('Limit',Limit)
+
+	def get_FlowName(self):
+		return self.get_query_params().get('FlowName')
+
+	def set_FlowName(self,FlowName):
+		self.add_query_param('FlowName',FlowName)
