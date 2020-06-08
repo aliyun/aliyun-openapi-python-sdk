208d401f7ff8b9cb630ca577e37e9e045e1a9956
sdk-team <sdk-team@alibabacloud.com>
2020-06-08 15:04:48 +0800

    Generate dbfs sdk.

---
+++ aliyun-python-sdk-dbfs/aliyunsdkdbfs/request/v20200219/CreateDbfsRequest.py
@@ -0,0 +1,56 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+
+class CreateDbfsRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'DBFS', '2020-02-19', 'CreateDbfs')
+		self.set_method('POST')
+
+	def get_SizeG(self):
+		return self.get_query_params().get('SizeG')
+
+	def set_SizeG(self,SizeG):
+		self.add_query_param('SizeG',SizeG)
+
+	def get_ClientToken(self):
+		return self.get_query_params().get('ClientToken')
+
+	def set_ClientToken(self,ClientToken):
+		self.add_query_param('ClientToken',ClientToken)
+
+	def get_FsName(self):
+		return self.get_query_params().get('FsName')
+
+	def set_FsName(self,FsName):
+		self.add_query_param('FsName',FsName)
+
+	def get_ZoneId(self):
+		return self.get_query_params().get('ZoneId')
+
+	def set_ZoneId(self,ZoneId):
+		self.add_query_param('ZoneId',ZoneId)
+
+	def get_Category(self):
+		return self.get_query_params().get('Category')
+
+	def set_Category(self,Category):
+		self.add_query_param('Category',Category)
---
+++ aliyun-python-sdk-dbfs/aliyunsdkdbfs/request/v20200219/ResizeDbfsRequest.py
@@ -0,0 +1,44 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+
+class ResizeDbfsRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'DBFS', '2020-02-19', 'ResizeDbfs')
+		self.set_method('POST')
+
+	def get_ClientToken(self):
+		return self.get_query_params().get('ClientToken')
+
+	def set_ClientToken(self,ClientToken):
+		self.add_query_param('ClientToken',ClientToken)
+
+	def get_NewSizeG(self):
+		return self.get_query_params().get('NewSizeG')
+
+	def set_NewSizeG(self,NewSizeG):
+		self.add_query_param('NewSizeG',NewSizeG)
+
+	def get_FsId(self):
+		return self.get_query_params().get('FsId')
+
+	def set_FsId(self,FsId):
+		self.add_query_param('FsId',FsId)
