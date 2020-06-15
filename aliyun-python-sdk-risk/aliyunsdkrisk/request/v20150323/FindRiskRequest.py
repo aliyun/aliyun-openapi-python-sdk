74a930d53503077006c26e77d36c6f10ab673dad
sdk-team <sdk-team@alibabacloud.com>
2020-06-15 18:10:31 +0800

    Change dependency to aliyun-python-sdk-core.

--- aliyun-python-sdk-risk/aliyunsdkrisk/request/v20150323/FindRiskRequest.py
+++ aliyun-python-sdk-risk/aliyunsdkrisk/request/v20150323/FindRiskRequest.py
@@ -6,8 +6,8 @@
 # "License"); you may not use this file except in compliance
 # with the License.  You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
 #
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 #
 # Unless required by applicable law or agreed to in writing,
@@ -18,22 +18,12 @@
 # under the License.
 
 from aliyunsdkcore.request import RpcRequest
+
 class FindRiskRequest(RpcRequest):
 
 	def __init__(self):
 		RpcRequest.__init__(self, 'Risk', '2015-03-23', 'FindRisk')
-
-	def get_MteeCode(self):
-		return self.get_query_params().get('MteeCode')
-
-	def set_MteeCode(self,MteeCode):
-		self.add_query_param('MteeCode',MteeCode)
-
-	def get_CodeType(self):
-		return self.get_query_params().get('CodeType')
-
-	def set_CodeType(self,CodeType):
-		self.add_query_param('CodeType',CodeType)
+		self.set_method('POST')
 
 	def get_IdType(self):
 		return self.get_query_params().get('IdType')
@@ -41,17 +31,17 @@ class FindRiskRequest(RpcRequest):
 	def set_IdType(self,IdType):
 		self.add_query_param('IdType',IdType)
 
-	def get_UserId(self):
-		return self.get_query_params().get('UserId')
+	def get_CodeType(self):
+		return self.get_query_params().get('CodeType')
 
-	def set_UserId(self,UserId):
-		self.add_query_param('UserId',UserId)
+	def set_CodeType(self,CodeType):
+		self.add_query_param('CodeType',CodeType)
 
-	def get_Collina(self):
-		return self.get_query_params().get('Collina')
+	def get_Phone(self):
+		return self.get_query_params().get('Phone')
 
-	def set_Collina(self,Collina):
-		self.add_query_param('Collina',Collina)
+	def set_Phone(self,Phone):
+		self.add_query_param('Phone',Phone)
 
 	def get_UmidToken(self):
 		return self.get_query_params().get('UmidToken')
@@ -59,20 +49,32 @@ class FindRiskRequest(RpcRequest):
 	def set_UmidToken(self,UmidToken):
 		self.add_query_param('UmidToken',UmidToken)
 
+	def get_Collina(self):
+		return self.get_query_params().get('Collina')
+
+	def set_Collina(self,Collina):
+		self.add_query_param('Collina',Collina)
+
 	def get_Ip(self):
 		return self.get_query_params().get('Ip')
 
 	def set_Ip(self,Ip):
 		self.add_query_param('Ip',Ip)
 
+	def get_UserId(self):
+		return self.get_query_params().get('UserId')
+
+	def set_UserId(self,UserId):
+		self.add_query_param('UserId',UserId)
+
+	def get_MteeCode(self):
+		return self.get_query_params().get('MteeCode')
+
+	def set_MteeCode(self,MteeCode):
+		self.add_query_param('MteeCode',MteeCode)
+
 	def get_Email(self):
 		return self.get_query_params().get('Email')
 
 	def set_Email(self,Email):
 		self.add_query_param('Email',Email)
-
-	def get_Phone(self):
-		return self.get_query_params().get('Phone')
-
-	def set_Phone(self,Phone):
-		self.add_query_param('Phone',Phone)
--- aliyun-python-sdk-risk/aliyunsdkrisk/request/v20150323/ValidateVerifyCodeRequest.py
+++ aliyun-python-sdk-risk/aliyunsdkrisk/request/v20150323/ValidateVerifyCodeRequest.py
@@ -6,8 +6,8 @@
 # "License"); you may not use this file except in compliance
 # with the License.  You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
 #
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 #
 # Unless required by applicable law or agreed to in writing,
@@ -18,28 +18,12 @@
 # under the License.
 
 from aliyunsdkcore.request import RpcRequest
+
 class ValidateVerifyCodeRequest(RpcRequest):
 
 	def __init__(self):
 		RpcRequest.__init__(self, 'Risk', '2015-03-23', 'ValidateVerifyCode')
-
-	def get_RequestId(self):
-		return self.get_query_params().get('RequestId')
-
-	def set_RequestId(self,RequestId):
-		self.add_query_param('RequestId',RequestId)
-
-	def get_MteeCode(self):
-		return self.get_query_params().get('MteeCode')
-
-	def set_MteeCode(self,MteeCode):
-		self.add_query_param('MteeCode',MteeCode)
-
-	def get_CodeType(self):
-		return self.get_query_params().get('CodeType')
-
-	def set_CodeType(self,CodeType):
-		self.add_query_param('CodeType',CodeType)
+		self.set_method('POST')
 
 	def get_IdType(self):
 		return self.get_query_params().get('IdType')
@@ -47,23 +31,23 @@ class ValidateVerifyCodeRequest(RpcRequest):
 	def set_IdType(self,IdType):
 		self.add_query_param('IdType',IdType)
 
-	def get_UserId(self):
-		return self.get_query_params().get('UserId')
-
-	def set_UserId(self,UserId):
-		self.add_query_param('UserId',UserId)
-
-	def get_ChannelType(self):
-		return self.get_query_params().get('ChannelType')
-
-	def set_ChannelType(self,ChannelType):
-		self.add_query_param('ChannelType',ChannelType)
-
 	def get_VerifyCode(self):
 		return self.get_query_params().get('VerifyCode')
 
 	def set_VerifyCode(self,VerifyCode):
 		self.add_query_param('VerifyCode',VerifyCode)
+
+	def get_CodeType(self):
+		return self.get_query_params().get('CodeType')
+
+	def set_CodeType(self,CodeType):
+		self.add_query_param('CodeType',CodeType)
+
+	def get_RequestId(self):
+		return self.get_query_params().get('RequestId')
+
+	def set_RequestId(self,RequestId):
+		self.add_query_param('RequestId',RequestId)
 
 	def get_UmidToken(self):
 		return self.get_query_params().get('UmidToken')
@@ -82,3 +66,21 @@ class ValidateVerifyCodeRequest(RpcRequest):
 
 	def set_Ip(self,Ip):
 		self.add_query_param('Ip',Ip)
+
+	def get_ChannelType(self):
+		return self.get_query_params().get('ChannelType')
+
+	def set_ChannelType(self,ChannelType):
+		self.add_query_param('ChannelType',ChannelType)
+
+	def get_UserId(self):
+		return self.get_query_params().get('UserId')
+
+	def set_UserId(self,UserId):
+		self.add_query_param('UserId',UserId)
+
+	def get_MteeCode(self):
+		return self.get_query_params().get('MteeCode')
+
+	def set_MteeCode(self,MteeCode):
+		self.add_query_param('MteeCode',MteeCode)
