baa51d823e15d2b0c12c3184be6a9bcc2ceaa3c1
sdk-team <sdk-team@alibabacloud.com>
2020-06-17 10:31:04 +0800

    Change dependency to aliyun-python-sdk-core.

--- aliyun-python-sdk-arms4finance/aliyunsdkarms4finance/request/v20171130/ARMSQueryDataSetRequest.py
+++ aliyun-python-sdk-arms4finance/aliyunsdkarms4finance/request/v20171130/ARMSQueryDataSetRequest.py
@@ -6,8 +6,8 @@
 # "License"); you may not use this file except in compliance
 # with the License.  You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
 #
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 #
 # Unless required by applicable law or agreed to in writing,
@@ -18,24 +18,12 @@
 # under the License.
 
 from aliyunsdkcore.request import RpcRequest
+
 class ARMSQueryDataSetRequest(RpcRequest):
 
 	def __init__(self):
 		RpcRequest.__init__(self, 'ARMS4FINANCE', '2017-11-30', 'ARMSQueryDataSet','arms4finance')
-
-	def get_Measuress(self):
-		return self.get_query_params().get('Measuress')
-
-	def set_Measuress(self,Measuress):
-		for i in range(len(Measuress)):	
-			if Measuress[i] is not None:
-				self.add_query_param('Measures.' + str(i + 1) , Measuress[i]);
-
-	def get_IntervalInSec(self):
-		return self.get_query_params().get('IntervalInSec')
-
-	def set_IntervalInSec(self,IntervalInSec):
-		self.add_query_param('IntervalInSec',IntervalInSec)
+		self.set_method('POST')
 
 	def get_DateStr(self):
 		return self.get_query_params().get('DateStr')
@@ -43,23 +31,11 @@ class ARMSQueryDataSetRequest(RpcRequest):
 	def set_DateStr(self,DateStr):
 		self.add_query_param('DateStr',DateStr)
 
-	def get_IsDrillDown(self):
-		return self.get_query_params().get('IsDrillDown')
-
-	def set_IsDrillDown(self,IsDrillDown):
-		self.add_query_param('IsDrillDown',IsDrillDown)
-
 	def get_MinTime(self):
 		return self.get_query_params().get('MinTime')
 
 	def set_MinTime(self,MinTime):
 		self.add_query_param('MinTime',MinTime)
-
-	def get_DatasetId(self):
-		return self.get_query_params().get('DatasetId')
-
-	def set_DatasetId(self,DatasetId):
-		self.add_query_param('DatasetId',DatasetId)
 
 	def get_MaxTime(self):
 		return self.get_query_params().get('MaxTime')
@@ -67,12 +43,38 @@ class ARMSQueryDataSetRequest(RpcRequest):
 	def set_MaxTime(self,MaxTime):
 		self.add_query_param('MaxTime',MaxTime)
 
+	def get_Measuress(self):
+		return self.get_query_params().get('Measuress')
+
+	def set_Measuress(self, Measuress):
+		for depth1 in range(len(Measuress)):
+			if Measuress[depth1] is not None:
+				self.add_query_param('Measures.' + str(depth1 + 1) , Measuress[depth1])
+
+	def get_IntervalInSec(self):
+		return self.get_query_params().get('IntervalInSec')
+
+	def set_IntervalInSec(self,IntervalInSec):
+		self.add_query_param('IntervalInSec',IntervalInSec)
+
+	def get_IsDrillDown(self):
+		return self.get_query_params().get('IsDrillDown')
+
+	def set_IsDrillDown(self,IsDrillDown):
+		self.add_query_param('IsDrillDown',IsDrillDown)
+
+	def get_DatasetId(self):
+		return self.get_query_params().get('DatasetId')
+
+	def set_DatasetId(self,DatasetId):
+		self.add_query_param('DatasetId',DatasetId)
+
 	def get_Dimensionss(self):
 		return self.get_query_params().get('Dimensionss')
 
-	def set_Dimensionss(self,Dimensionss):
-		for i in range(len(Dimensionss)):	
-			if Dimensionss[i].get('Value') is not None:
-				self.add_query_param('Dimensions.' + str(i + 1) + '.Value' , Dimensionss[i].get('Value'))
-			if Dimensionss[i].get('Key') is not None:
-				self.add_query_param('Dimensions.' + str(i + 1) + '.Key' , Dimensionss[i].get('Key'))
+	def set_Dimensionss(self, Dimensionss):
+		for depth1 in range(len(Dimensionss)):
+			if Dimensionss[depth1].get('Value') is not None:
+				self.add_query_param('Dimensions.' + str(depth1 + 1) + '.Value', Dimensionss[depth1].get('Value'))
+			if Dimensionss[depth1].get('Key') is not None:
+				self.add_query_param('Dimensions.' + str(depth1 + 1) + '.Key', Dimensionss[depth1].get('Key'))
--- aliyun-python-sdk-arms4finance/aliyunsdkarms4finance/request/v20171130/WhereInDimQueryRequest.py
+++ aliyun-python-sdk-arms4finance/aliyunsdkarms4finance/request/v20171130/WhereInDimQueryRequest.py
@@ -6,8 +6,8 @@
 # "License"); you may not use this file except in compliance
 # with the License.  You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
 #
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 #
 # Unless required by applicable law or agreed to in writing,
@@ -18,10 +18,30 @@
 # under the License.
 
 from aliyunsdkcore.request import RpcRequest
+
 class WhereInDimQueryRequest(RpcRequest):
 
 	def __init__(self):
 		RpcRequest.__init__(self, 'ARMS4FINANCE', '2017-11-30', 'WhereInDimQuery','arms4finance')
+		self.set_method('POST')
+
+	def get_DateStr(self):
+		return self.get_query_params().get('DateStr')
+
+	def set_DateStr(self,DateStr):
+		self.add_query_param('DateStr',DateStr)
+
+	def get_MinTime(self):
+		return self.get_query_params().get('MinTime')
+
+	def set_MinTime(self,MinTime):
+		self.add_query_param('MinTime',MinTime)
+
+	def get_MaxTime(self):
+		return self.get_query_params().get('MaxTime')
+
+	def set_MaxTime(self,MaxTime):
+		self.add_query_param('MaxTime',MaxTime)
 
 	def get_WhereInKey(self):
 		return self.get_query_params().get('WhereInKey')
@@ -32,10 +52,10 @@ class WhereInDimQueryRequest(RpcRequest):
 	def get_Measuress(self):
 		return self.get_query_params().get('Measuress')
 
-	def set_Measuress(self,Measuress):
-		for i in range(len(Measuress)):	
-			if Measuress[i] is not None:
-				self.add_query_param('Measures.' + str(i + 1) , Measuress[i]);
+	def set_Measuress(self, Measuress):
+		for depth1 in range(len(Measuress)):
+			if Measuress[depth1] is not None:
+				self.add_query_param('Measures.' + str(depth1 + 1) , Measuress[depth1])
 
 	def get_IntervalInSec(self):
 		return self.get_query_params().get('IntervalInSec')
@@ -43,23 +63,11 @@ class WhereInDimQueryRequest(RpcRequest):
 	def set_IntervalInSec(self,IntervalInSec):
 		self.add_query_param('IntervalInSec',IntervalInSec)
 
-	def get_DateStr(self):
-		return self.get_query_params().get('DateStr')
-
-	def set_DateStr(self,DateStr):
-		self.add_query_param('DateStr',DateStr)
-
 	def get_IsDrillDown(self):
 		return self.get_query_params().get('IsDrillDown')
 
 	def set_IsDrillDown(self,IsDrillDown):
 		self.add_query_param('IsDrillDown',IsDrillDown)
-
-	def get_MinTime(self):
-		return self.get_query_params().get('MinTime')
-
-	def set_MinTime(self,MinTime):
-		self.add_query_param('MinTime',MinTime)
 
 	def get_DatasetId(self):
 		return self.get_query_params().get('DatasetId')
@@ -70,23 +78,17 @@ class WhereInDimQueryRequest(RpcRequest):
 	def get_WhereInValuess(self):
 		return self.get_query_params().get('WhereInValuess')
 
-	def set_WhereInValuess(self,WhereInValuess):
-		for i in range(len(WhereInValuess)):	
-			if WhereInValuess[i] is not None:
-				self.add_query_param('WhereInValues.' + str(i + 1) , WhereInValuess[i]);
-
-	def get_MaxTime(self):
-		return self.get_query_params().get('MaxTime')
-
-	def set_MaxTime(self,MaxTime):
-		self.add_query_param('MaxTime',MaxTime)
+	def set_WhereInValuess(self, WhereInValuess):
+		for depth1 in range(len(WhereInValuess)):
+			if WhereInValuess[depth1] is not None:
+				self.add_query_param('WhereInValues.' + str(depth1 + 1) , WhereInValuess[depth1])
 
 	def get_Dimensionss(self):
 		return self.get_query_params().get('Dimensionss')
 
-	def set_Dimensionss(self,Dimensionss):
-		for i in range(len(Dimensionss)):	
-			if Dimensionss[i].get('Value') is not None:
-				self.add_query_param('Dimensions.' + str(i + 1) + '.Value' , Dimensionss[i].get('Value'))
-			if Dimensionss[i].get('Key') is not None:
-				self.add_query_param('Dimensions.' + str(i + 1) + '.Key' , Dimensionss[i].get('Key'))
+	def set_Dimensionss(self, Dimensionss):
+		for depth1 in range(len(Dimensionss)):
+			if Dimensionss[depth1].get('Value') is not None:
+				self.add_query_param('Dimensions.' + str(depth1 + 1) + '.Value', Dimensionss[depth1].get('Value'))
+			if Dimensionss[depth1].get('Key') is not None:
+				self.add_query_param('Dimensions.' + str(depth1 + 1) + '.Key', Dimensionss[depth1].get('Key'))
