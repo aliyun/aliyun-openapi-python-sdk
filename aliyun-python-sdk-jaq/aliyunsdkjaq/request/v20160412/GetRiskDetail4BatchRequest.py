67536e09dff8a6d6cddce7b8e16d22d4889eb6e6
sdk-team <sdk-team@alibabacloud.com>
2020-06-17 10:45:32 +0800

    Change dependency to aliyun-python-sdk-core.

--- aliyun-python-sdk-jaq/aliyunsdkjaq/request/v20160412/GetRiskDetail4BatchRequest.py
+++ aliyun-python-sdk-jaq/aliyunsdkjaq/request/v20160412/GetRiskDetail4BatchRequest.py
@@ -6,8 +6,8 @@
 # "License"); you may not use this file except in compliance
 # with the License.  You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
 #
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 #
 # Unless required by applicable law or agreed to in writing,
@@ -18,10 +18,12 @@
 # under the License.
 
 from aliyunsdkcore.request import RpcRequest
+
 class GetRiskDetail4BatchRequest(RpcRequest):
 
 	def __init__(self):
 		RpcRequest.__init__(self, 'jaq', '2016-04-12', 'GetRiskDetail4Batch','jaq')
+		self.set_method('POST')
 
 	def get_ItemId(self):
 		return self.get_query_params().get('ItemId')
