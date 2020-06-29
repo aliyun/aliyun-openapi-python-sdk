97d7f31adb7362e70a0957e8962c2fd21ee8391b
sdk-team <sdk-team@alibabacloud.com>
2020-06-29 15:02:07 +0800

    Supported gb28181 parent platform.

---
+++ aliyun-python-sdk-vs/aliyunsdkvs/request/v20181212/ModifyParentPlatformRequest.py
@@ -0,0 +1,98 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkvs.endpoint import endpoint_data
+
+class ModifyParentPlatformRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'vs', '2018-12-12', 'ModifyParentPlatform','vs')
+		self.set_method('POST')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+
+	def get_GbId(self):
+		return self.get_query_params().get('GbId')
+
+	def set_GbId(self,GbId):
+		self.add_query_param('GbId',GbId)
+
+	def get_ClientAuth(self):
+		return self.get_query_params().get('ClientAuth')
+
+	def set_ClientAuth(self,ClientAuth):
+		self.add_query_param('ClientAuth',ClientAuth)
+
+	def get_Description(self):
+		return self.get_query_params().get('Description')
+
+	def set_Description(self,Description):
+		self.add_query_param('Description',Description)
+
+	def get_AutoStart(self):
+		return self.get_query_params().get('AutoStart')
+
+	def set_AutoStart(self,AutoStart):
+		self.add_query_param('AutoStart',AutoStart)
+
+	def get_Id(self):
+		return self.get_query_params().get('Id')
+
+	def set_Id(self,Id):
+		self.add_query_param('Id',Id)
+
+	def get_ClientPassword(self):
+		return self.get_query_params().get('ClientPassword')
+
+	def set_ClientPassword(self,ClientPassword):
+		self.add_query_param('ClientPassword',ClientPassword)
+
+	def get_Ip(self):
+		return self.get_query_params().get('Ip')
+
+	def set_Ip(self,Ip):
+		self.add_query_param('Ip',Ip)
+
+	def get_OwnerId(self):
+		return self.get_query_params().get('OwnerId')
+
+	def set_OwnerId(self,OwnerId):
+		self.add_query_param('OwnerId',OwnerId)
+
+	def get_Port(self):
+		return self.get_query_params().get('Port')
+
+	def set_Port(self,Port):
+		self.add_query_param('Port',Port)
+
+	def get_Name(self):
+		return self.get_query_params().get('Name')
+
+	def set_Name(self,Name):
+		self.add_query_param('Name',Name)
+
+	def get_ClientUsername(self):
+		return self.get_query_params().get('ClientUsername')
+
+	def set_ClientUsername(self,ClientUsername):
+		self.add_query_param('ClientUsername',ClientUsername)
