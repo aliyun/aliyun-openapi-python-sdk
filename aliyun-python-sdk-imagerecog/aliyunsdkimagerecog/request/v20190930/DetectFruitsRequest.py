7c04f508833cdcc021be218b49091a6f4a7e22d8
sdk-team <sdk-team@alibabacloud.com>
2020-07-01 16:51:01 +0800

    SDK version 108.

--- aliyun-python-sdk-imagerecog/aliyunsdkimagerecog/request/v20190930/DetectFruitsRequest.py
+++ aliyun-python-sdk-imagerecog/aliyunsdkimagerecog/request/v20190930/DetectFruitsRequest.py
@@ -20,10 +20,10 @@
 from aliyunsdkcore.request import RpcRequest
 from aliyunsdkimagerecog.endpoint import endpoint_data
 
-class RecognizeLogoRequest(RpcRequest):
+class DetectFruitsRequest(RpcRequest):
 
 	def __init__(self):
-		RpcRequest.__init__(self, 'imagerecog', '2019-09-30', 'RecognizeLogo','imagerecog')
+		RpcRequest.__init__(self, 'imagerecog', '2019-09-30', 'DetectFruits','imagerecog')
 		self.set_method('POST')
 		if hasattr(self, "endpoint_map"):
 			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
@@ -31,10 +31,8 @@ class RecognizeLogoRequest(RpcRequest):
 			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
 
 
-	def get_Taskss(self):
-		return self.get_body_params().get('Taskss')
+	def get_ImageURL(self):
+		return self.get_body_params().get('ImageURL')
 
-	def set_Taskss(self, Taskss):
-		for depth1 in range(len(Taskss)):
-			if Taskss[depth1].get('ImageURL') is not None:
-				self.add_body_params('Tasks.' + str(depth1 + 1) + '.ImageURL', Taskss[depth1].get('ImageURL'))
+	def set_ImageURL(self,ImageURL):
+		self.add_body_params('ImageURL', ImageURL)
