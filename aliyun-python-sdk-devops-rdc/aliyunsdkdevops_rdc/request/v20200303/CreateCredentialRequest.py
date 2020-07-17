95371aedde5214999c444b02e21c9962fa3f928c
sdk-team <sdk-team@alibabacloud.com>
2020-07-17 16:41:03 +0800

    Add actions.

---
+++ aliyun-python-sdk-devops-rdc/aliyunsdkdevops_rdc/request/v20200303/CreateCredentialRequest.py
@@ -0,0 +1,62 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+
+class CreateCredentialRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'devops-rdc', '2020-03-03', 'CreateCredential')
+		self.set_method('POST')
+
+	def get_Password(self):
+		return self.get_body_params().get('Password')
+
+	def set_Password(self,Password):
+		self.add_body_params('Password', Password)
+
+	def get_Name(self):
+		return self.get_body_params().get('Name')
+
+	def set_Name(self,Name):
+		self.add_body_params('Name', Name)
+
+	def get_Type(self):
+		return self.get_body_params().get('Type')
+
+	def set_Type(self,Type):
+		self.add_body_params('Type', Type)
+
+	def get_UserPk(self):
+		return self.get_body_params().get('UserPk')
+
+	def set_UserPk(self,UserPk):
+		self.add_body_params('UserPk', UserPk)
+
+	def get_OrgId(self):
+		return self.get_body_params().get('OrgId')
+
+	def set_OrgId(self,OrgId):
+		self.add_body_params('OrgId', OrgId)
+
+	def get_UserName(self):
+		return self.get_body_params().get('UserName')
+
+	def set_UserName(self,UserName):
+		self.add_body_params('UserName', UserName)
---
+++ aliyun-python-sdk-devops-rdc/aliyunsdkdevops_rdc/request/v20200303/ListPipelinesRequest.py
@@ -0,0 +1,98 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+
+class ListPipelinesRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'devops-rdc', '2020-03-03', 'ListPipelines')
+		self.set_method('POST')
+
+	def get_PipelineName(self):
+		return self.get_body_params().get('PipelineName')
+
+	def set_PipelineName(self,PipelineName):
+		self.add_body_params('PipelineName', PipelineName)
+
+	def get_ResultStatusList(self):
+		return self.get_body_params().get('ResultStatusList')
+
+	def set_ResultStatusList(self,ResultStatusList):
+		self.add_body_params('ResultStatusList', ResultStatusList)
+
+	def get_Creators(self):
+		return self.get_body_params().get('Creators')
+
+	def set_Creators(self,Creators):
+		self.add_body_params('Creators', Creators)
+
+	def get_ExecuteEndTime(self):
+		return self.get_body_params().get('ExecuteEndTime')
+
+	def set_ExecuteEndTime(self,ExecuteEndTime):
+		self.add_body_params('ExecuteEndTime', ExecuteEndTime)
+
+	def get_UserPk(self):
+		return self.get_body_params().get('UserPk')
+
+	def set_UserPk(self,UserPk):
+		self.add_body_params('UserPk', UserPk)
+
+	def get_OrgId(self):
+		return self.get_query_params().get('OrgId')
+
+	def set_OrgId(self,OrgId):
+		self.add_query_param('OrgId',OrgId)
+
+	def get_CreateStartTime(self):
+		return self.get_body_params().get('CreateStartTime')
+
+	def set_CreateStartTime(self,CreateStartTime):
+		self.add_body_params('CreateStartTime', CreateStartTime)
+
+	def get_Operators(self):
+		return self.get_body_params().get('Operators')
+
+	def set_Operators(self,Operators):
+		self.add_body_params('Operators', Operators)
+
+	def get_PageSize(self):
+		return self.get_body_params().get('PageSize')
+
+	def set_PageSize(self,PageSize):
+		self.add_body_params('PageSize', PageSize)
+
+	def get_ExecuteStartTime(self):
+		return self.get_body_params().get('ExecuteStartTime')
+
+	def set_ExecuteStartTime(self,ExecuteStartTime):
+		self.add_body_params('ExecuteStartTime', ExecuteStartTime)
+
+	def get_PageStart(self):
+		return self.get_body_params().get('PageStart')
+
+	def set_PageStart(self,PageStart):
+		self.add_body_params('PageStart', PageStart)
+
+	def get_CreateEndTime(self):
+		return self.get_body_params().get('CreateEndTime')
+
+	def set_CreateEndTime(self,CreateEndTime):
+		self.add_body_params('CreateEndTime', CreateEndTime)
