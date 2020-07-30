7b2298be494014e385f5c042ffa36b49eadfe492
sdk-team <sdk-team@alibabacloud.com>
2020-07-30 14:11:17 +0800

    Supported ChatApp for SDK.

---
+++ aliyun-python-sdk-cams/aliyunsdkcams/request/v20200606/SendMessageRequest.py
@@ -0,0 +1,86 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkcams.endpoint import endpoint_data
+
+class SendMessageRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'cams', '2020-06-06', 'SendMessage','cams')
+		self.set_method('POST')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+
+	def get_ResourceOwnerId(self):
+		return self.get_query_params().get('ResourceOwnerId')
+
+	def set_ResourceOwnerId(self,ResourceOwnerId):
+		self.add_query_param('ResourceOwnerId',ResourceOwnerId)
+
+	def get_TemplateBodyParams(self):
+		return self.get_query_params().get('TemplateBodyParams')
+
+	def set_TemplateBodyParams(self,TemplateBodyParams):
+		self.add_query_param('TemplateBodyParams',TemplateBodyParams)
+
+	def get_Type(self):
+		return self.get_query_params().get('Type')
+
+	def set_Type(self,Type):
+		self.add_query_param('Type',Type)
+
+	def get_ChannelType(self):
+		return self.get_query_params().get('ChannelType')
+
+	def set_ChannelType(self,ChannelType):
+		self.add_query_param('ChannelType',ChannelType)
+
+	def get__From(self):
+		return self.get_query_params().get('From')
+
+	def set__From(self,_From):
+		self.add_query_param('From',_From)
+
+	def get_ResourceOwnerAccount(self):
+		return self.get_query_params().get('ResourceOwnerAccount')
+
+	def set_ResourceOwnerAccount(self,ResourceOwnerAccount):
+		self.add_query_param('ResourceOwnerAccount',ResourceOwnerAccount)
+
+	def get_OwnerId(self):
+		return self.get_query_params().get('OwnerId')
+
+	def set_OwnerId(self,OwnerId):
+		self.add_query_param('OwnerId',OwnerId)
+
+	def get_To(self):
+		return self.get_query_params().get('To')
+
+	def set_To(self,To):
+		self.add_query_param('To',To)
+
+	def get_TemplateCode(self):
+		return self.get_query_params().get('TemplateCode')
+
+	def set_TemplateCode(self,TemplateCode):
+		self.add_query_param('TemplateCode',TemplateCode)
