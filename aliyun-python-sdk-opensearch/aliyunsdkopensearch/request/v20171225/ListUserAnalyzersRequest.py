b2179a51c2a166bcd75b8d277adc4da42a412ba0
sdk-team <sdk-team@alibabacloud.com>
2020-07-30 15:19:26 +0800

    Add ABTest-related APIs.

---
+++ aliyun-python-sdk-opensearch/aliyunsdkopensearch/request/v20171225/ListUserAnalyzersRequest.py
@@ -0,0 +1,45 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RoaRequest
+from aliyunsdkopensearch.endpoint import endpoint_data
+
+class ListUserAnalyzersRequest(RoaRequest):
+
+	def __init__(self):
+		RoaRequest.__init__(self, 'OpenSearch', '2017-12-25', 'ListUserAnalyzers','opensearch')
+		self.set_uri_pattern('/v4/openapi/user-analyzers')
+		self.set_method('GET')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+
+	def get_pageSize(self):
+		return self.get_query_params().get('pageSize')
+
+	def set_pageSize(self,pageSize):
+		self.add_query_param('pageSize',pageSize)
+
+	def get_pageNumber(self):
+		return self.get_query_params().get('pageNumber')
+
+	def set_pageNumber(self,pageNumber):
+		self.add_query_param('pageNumber',pageNumber)
