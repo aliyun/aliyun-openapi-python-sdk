effec13957fbc0bdbf1c2b23d42435e460b52677
sdk-team <sdk-team@alibabacloud.com>
2020-07-30 17:07:49 +0800

    Add Smart Cloudauth API.

---
+++ aliyun-python-sdk-cloudauth/aliyunsdkcloudauth/request/v20200618/InitSmartVerifyRequest.py
@@ -0,0 +1,80 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkcloudauth.endpoint import endpoint_data
+
+class InitSmartVerifyRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Cloudauth', '2020-06-18', 'InitSmartVerify','cloudauth')
+		self.set_method('POST')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+
+	def get_Ip(self):
+		return self.get_body_params().get('Ip')
+
+	def set_Ip(self,Ip):
+		self.add_body_params('Ip', Ip)
+
+	def get_Mobile(self):
+		return self.get_body_params().get('Mobile')
+
+	def set_Mobile(self,Mobile):
+		self.add_body_params('Mobile', Mobile)
+
+	def get_UserId(self):
+		return self.get_body_params().get('UserId')
+
+	def set_UserId(self,UserId):
+		self.add_body_params('UserId', UserId)
+
+	def get_Mode(self):
+		return self.get_body_params().get('Mode')
+
+	def set_Mode(self,Mode):
+		self.add_body_params('Mode', Mode)
+
+	def get_OuterOrderNo(self):
+		return self.get_body_params().get('OuterOrderNo')
+
+	def set_OuterOrderNo(self,OuterOrderNo):
+		self.add_body_params('OuterOrderNo', OuterOrderNo)
+
+	def get_CertType(self):
+		return self.get_body_params().get('CertType')
+
+	def set_CertType(self,CertType):
+		self.add_body_params('CertType', CertType)
+
+	def get_SceneId(self):
+		return self.get_body_params().get('SceneId')
+
+	def set_SceneId(self,SceneId):
+		self.add_body_params('SceneId', SceneId)
+
+	def get_MetaInfo(self):
+		return self.get_body_params().get('MetaInfo')
+
+	def set_MetaInfo(self,MetaInfo):
+		self.add_body_params('MetaInfo', MetaInfo)
