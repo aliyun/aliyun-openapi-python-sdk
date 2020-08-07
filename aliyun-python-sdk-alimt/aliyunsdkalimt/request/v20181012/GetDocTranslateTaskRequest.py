6d514c5a69afb81049e8a91e59cbe331b45f6bcb
sdk-team <sdk-team@alibabacloud.com>
2020-08-07 17:35:57 +0800

    GetTitleDiagnose GetTitleGenerate GetImageTranslate supported.

--- aliyun-python-sdk-alimt/aliyunsdkalimt/request/v20181012/GetDocTranslateTaskRequest.py
+++ aliyun-python-sdk-alimt/aliyunsdkalimt/request/v20181012/GetDocTranslateTaskRequest.py
@@ -23,7 +23,7 @@ from aliyunsdkalimt.endpoint import endpoint_data
 class GetDocTranslateTaskRequest(RpcRequest):
 
 	def __init__(self):
-		RpcRequest.__init__(self, 'alimt', '2018-10-12', 'GetDocTranslateTask')
+		RpcRequest.__init__(self, 'alimt', '2018-10-12', 'GetDocTranslateTask','alimt')
 		self.set_method('GET')
 		if hasattr(self, "endpoint_map"):
 			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
--- aliyun-python-sdk-alimt/aliyunsdkalimt/request/v20181012/GetImageDiagnoseRequest.py
+++ aliyun-python-sdk-alimt/aliyunsdkalimt/request/v20181012/GetImageDiagnoseRequest.py
@@ -23,7 +23,7 @@ from aliyunsdkalimt.endpoint import endpoint_data
 class GetImageDiagnoseRequest(RpcRequest):
 
 	def __init__(self):
-		RpcRequest.__init__(self, 'alimt', '2018-10-12', 'GetImageDiagnose')
+		RpcRequest.__init__(self, 'alimt', '2018-10-12', 'GetImageDiagnose','alimt')
 		self.set_method('POST')
 		if hasattr(self, "endpoint_map"):
 			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
