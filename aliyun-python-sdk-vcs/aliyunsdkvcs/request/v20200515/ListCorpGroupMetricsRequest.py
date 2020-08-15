4db1bc5af7880216e13fa0f7f677bc9329e43307
sdk-team <sdk-team@alibabacloud.com>
2020-08-16 06:09:57 +0800

    Add new tags for corp group.

---
+++ aliyun-python-sdk-vcs/aliyunsdkvcs/request/v20200515/ListCorpGroupMetricsRequest.py
@@ -0,0 +1,68 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkvcs.endpoint import endpoint_data
+
+class ListCorpGroupMetricsRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Vcs', '2020-05-15', 'ListCorpGroupMetrics','vcs')
+		self.set_method('POST')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+
+	def get_GroupId(self):
+		return self.get_body_params().get('GroupId')
+
+	def set_GroupId(self,GroupId):
+		self.add_body_params('GroupId', GroupId)
+
+	def get_EndTime(self):
+		return self.get_body_params().get('EndTime')
+
+	def set_EndTime(self,EndTime):
+		self.add_body_params('EndTime', EndTime)
+
+	def get_StartTime(self):
+		return self.get_body_params().get('StartTime')
+
+	def set_StartTime(self,StartTime):
+		self.add_body_params('StartTime', StartTime)
+
+	def get_PageNumber(self):
+		return self.get_body_params().get('PageNumber')
+
+	def set_PageNumber(self,PageNumber):
+		self.add_body_params('PageNumber', PageNumber)
+
+	def get_TagCode(self):
+		return self.get_body_params().get('TagCode')
+
+	def set_TagCode(self,TagCode):
+		self.add_body_params('TagCode', TagCode)
+
+	def get_PageSize(self):
+		return self.get_body_params().get('PageSize')
+
+	def set_PageSize(self,PageSize):
+		self.add_body_params('PageSize', PageSize)
