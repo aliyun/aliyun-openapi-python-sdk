02781e96dfccf54ec585d2073f60596eb6fadcbc
sdk-team <sdk-team@alibabacloud.com>
2020-09-22 02:04:23 +0000

    DescribePrice update amount type to string.

--- aliyun-python-sdk-r-kvstore/aliyunsdkr_kvstore/request/v20150101/ModifyInstanceMinorVersionRequest.py
+++ aliyun-python-sdk-r-kvstore/aliyunsdkr_kvstore/request/v20150101/ModifyInstanceMinorVersionRequest.py
@@ -37,17 +37,17 @@ class ModifyInstanceMinorVersionRequest(RpcRequest):
 	def set_ResourceOwnerId(self,ResourceOwnerId):
 		self.add_query_param('ResourceOwnerId',ResourceOwnerId)
 
-	def get_EffectTime(self):
-		return self.get_query_params().get('EffectTime')
-
-	def set_EffectTime(self,EffectTime):
-		self.add_query_param('EffectTime',EffectTime)
-
 	def get_SecurityToken(self):
 		return self.get_query_params().get('SecurityToken')
 
 	def set_SecurityToken(self,SecurityToken):
 		self.add_query_param('SecurityToken',SecurityToken)
+
+	def get_EffectiveTime(self):
+		return self.get_query_params().get('EffectiveTime')
+
+	def set_EffectiveTime(self,EffectiveTime):
+		self.add_query_param('EffectiveTime',EffectiveTime)
 
 	def get_ResourceOwnerAccount(self):
 		return self.get_query_params().get('ResourceOwnerAccount')
