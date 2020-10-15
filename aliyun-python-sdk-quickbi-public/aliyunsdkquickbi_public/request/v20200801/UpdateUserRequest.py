ec33b86f0e7d0157a08e3ff472151e2f35647a68
sdk-team <sdk-team@alibabacloud.com>
2020-10-15 14:14:25 +0000

    Add Usergroup APIs and UserTag APIs.

---
+++ aliyun-python-sdk-quickbi-public/aliyunsdkquickbi_public/request/v20200801/UpdateUserRequest.py
@@ -0,0 +1,62 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkquickbi_public.endpoint import endpoint_data
+
+class UpdateUserRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'quickbi-public', '2020-08-01', 'UpdateUser','quickbi')
+		self.set_method('POST')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+
+	def get_AdminUser(self):
+		return self.get_query_params().get('AdminUser')
+
+	def set_AdminUser(self,AdminUser):
+		self.add_query_param('AdminUser',AdminUser)
+
+	def get_UserType(self):
+		return self.get_query_params().get('UserType')
+
+	def set_UserType(self,UserType):
+		self.add_query_param('UserType',UserType)
+
+	def get_UserId(self):
+		return self.get_query_params().get('UserId')
+
+	def set_UserId(self,UserId):
+		self.add_query_param('UserId',UserId)
+
+	def get_NickName(self):
+		return self.get_query_params().get('NickName')
+
+	def set_NickName(self,NickName):
+		self.add_query_param('NickName',NickName)
+
+	def get_AuthAdminUser(self):
+		return self.get_query_params().get('AuthAdminUser')
+
+	def set_AuthAdminUser(self,AuthAdminUser):
+		self.add_query_param('AuthAdminUser',AuthAdminUser)
---
+++ aliyun-python-sdk-quickbi-public/aliyunsdkquickbi_public/request/v20200802/UpdateUserTagValueRequest.py
@@ -0,0 +1,50 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkquickbi_public.endpoint import endpoint_data
+
+class UpdateUserTagValueRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'quickbi-public', '2020-08-02', 'UpdateUserTagValue','quickbi')
+		self.set_method('POST')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+
+	def get_TagValue(self):
+		return self.get_query_params().get('TagValue')
+
+	def set_TagValue(self,TagValue):
+		self.add_query_param('TagValue',TagValue)
+
+	def get_TagId(self):
+		return self.get_query_params().get('TagId')
+
+	def set_TagId(self,TagId):
+		self.add_query_param('TagId',TagId)
+
+	def get_UserId(self):
+		return self.get_query_params().get('UserId')
+
+	def set_UserId(self,UserId):
+		self.add_query_param('UserId',UserId)
