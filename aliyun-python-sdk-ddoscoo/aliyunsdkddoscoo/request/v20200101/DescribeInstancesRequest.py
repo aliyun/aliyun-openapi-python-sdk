aa631e18c9075f445e5d03d7a41adfa8416d7bd2
sdk-team <sdk-team@alibabacloud.com>
2020-11-24 02:49:47 +0000

    Generated 2020-01-01 for ddoscoo.

---
+++ aliyun-python-sdk-ddoscoo/aliyunsdkddoscoo/request/v20200101/DescribeInstancesRequest.py
@@ -0,0 +1,112 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkddoscoo.endpoint import endpoint_data
+
+class DescribeInstancesRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'ddoscoo', '2020-01-01', 'DescribeInstances')
+		self.set_method('POST')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+
+	def get_Edition(self):
+		return self.get_query_params().get('Edition')
+
+	def set_Edition(self,Edition):
+		self.add_query_param('Edition',Edition)
+
+	def get_Remark(self):
+		return self.get_query_params().get('Remark')
+
+	def set_Remark(self,Remark):
+		self.add_query_param('Remark',Remark)
+
+	def get_PageNumber(self):
+		return self.get_query_params().get('PageNumber')
+
+	def set_PageNumber(self,PageNumber):
+		self.add_query_param('PageNumber',PageNumber)
+
+	def get_Enabled(self):
+		return self.get_query_params().get('Enabled')
+
+	def set_Enabled(self,Enabled):
+		self.add_query_param('Enabled',Enabled)
+
+	def get_ResourceGroupId(self):
+		return self.get_query_params().get('ResourceGroupId')
+
+	def set_ResourceGroupId(self,ResourceGroupId):
+		self.add_query_param('ResourceGroupId',ResourceGroupId)
+
+	def get_PageSize(self):
+		return self.get_query_params().get('PageSize')
+
+	def set_PageSize(self,PageSize):
+		self.add_query_param('PageSize',PageSize)
+
+	def get_Tags(self):
+		return self.get_query_params().get('Tag')
+
+	def set_Tags(self, Tags):
+		for depth1 in range(len(Tags)):
+			if Tags[depth1].get('Value') is not None:
+				self.add_query_param('Tag.' + str(depth1 + 1) + '.Value', Tags[depth1].get('Value'))
+			if Tags[depth1].get('Key') is not None:
+				self.add_query_param('Tag.' + str(depth1 + 1) + '.Key', Tags[depth1].get('Key'))
+
+	def get_ExpireStartTime(self):
+		return self.get_query_params().get('ExpireStartTime')
+
+	def set_ExpireStartTime(self,ExpireStartTime):
+		self.add_query_param('ExpireStartTime',ExpireStartTime)
+
+	def get_ExpireEndTime(self):
+		return self.get_query_params().get('ExpireEndTime')
+
+	def set_ExpireEndTime(self,ExpireEndTime):
+		self.add_query_param('ExpireEndTime',ExpireEndTime)
+
+	def get_Ip(self):
+		return self.get_query_params().get('Ip')
+
+	def set_Ip(self,Ip):
+		self.add_query_param('Ip',Ip)
+
+	def get_InstanceIdss(self):
+		return self.get_query_params().get('InstanceIds')
+
+	def set_InstanceIdss(self, InstanceIdss):
+		for depth1 in range(len(InstanceIdss)):
+			if InstanceIdss[depth1] is not None:
+				self.add_query_param('InstanceIds.' + str(depth1 + 1) , InstanceIdss[depth1])
+
+	def get_Statuss(self):
+		return self.get_query_params().get('Status')
+
+	def set_Statuss(self, Statuss):
+		for depth1 in range(len(Statuss)):
+			if Statuss[depth1] is not None:
+				self.add_query_param('Status.' + str(depth1 + 1) , Statuss[depth1])
---
+++ aliyun-python-sdk-ddoscoo/aliyunsdkddoscoo/request/v20200101/ModifyWebPreciseAccessRuleRequest.py
@@ -0,0 +1,56 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkddoscoo.endpoint import endpoint_data
+
+class ModifyWebPreciseAccessRuleRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'ddoscoo', '2020-01-01', 'ModifyWebPreciseAccessRule')
+		self.set_method('POST')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+
+	def get_Expires(self):
+		return self.get_query_params().get('Expires')
+
+	def set_Expires(self,Expires):
+		self.add_query_param('Expires',Expires)
+
+	def get_Rules(self):
+		return self.get_query_params().get('Rules')
+
+	def set_Rules(self,Rules):
+		self.add_query_param('Rules',Rules)
+
+	def get_ResourceGroupId(self):
+		return self.get_query_params().get('ResourceGroupId')
+
+	def set_ResourceGroupId(self,ResourceGroupId):
+		self.add_query_param('ResourceGroupId',ResourceGroupId)
+
+	def get_Domain(self):
+		return self.get_query_params().get('Domain')
+
+	def set_Domain(self,Domain):
+		self.add_query_param('Domain',Domain)
