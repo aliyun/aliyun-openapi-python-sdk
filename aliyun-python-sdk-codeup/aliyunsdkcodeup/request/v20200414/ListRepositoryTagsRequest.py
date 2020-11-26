bfa1126ec8f6779c38d234b1dcd1a85a79ef1c94
sdk-team <sdk-team@alibabacloud.com>
2020-11-26 02:19:34 +0000

    Add tags api, include GetRepositoryTag, ListRepositoryTags, DeleteRepositoryTag.

---
+++ aliyun-python-sdk-codeup/aliyunsdkcodeup/request/v20200414/ListRepositoryTagsRequest.py
@@ -0,0 +1,75 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RoaRequest
+
+class ListRepositoryTagsRequest(RoaRequest):
+
+	def __init__(self):
+		RoaRequest.__init__(self, 'codeup', '2020-04-14', 'ListRepositoryTags')
+		self.set_uri_pattern('/api/v4/projects/[ProjectId]/repository/tags')
+		self.set_method('GET')
+
+	def get_AccessToken(self):
+		return self.get_query_params().get('AccessToken')
+
+	def set_AccessToken(self,AccessToken):
+		self.add_query_param('AccessToken',AccessToken)
+
+	def get_ShowSignature(self):
+		return self.get_query_params().get('ShowSignature')
+
+	def set_ShowSignature(self,ShowSignature):
+		self.add_query_param('ShowSignature',ShowSignature)
+
+	def get_Sort(self):
+		return self.get_query_params().get('Sort')
+
+	def set_Sort(self,Sort):
+		self.add_query_param('Sort',Sort)
+
+	def get_OrganizationId(self):
+		return self.get_query_params().get('OrganizationId')
+
+	def set_OrganizationId(self,OrganizationId):
+		self.add_query_param('OrganizationId',OrganizationId)
+
+	def get_Search(self):
+		return self.get_query_params().get('Search')
+
+	def set_Search(self,Search):
+		self.add_query_param('Search',Search)
+
+	def get_PageSize(self):
+		return self.get_query_params().get('PageSize')
+
+	def set_PageSize(self,PageSize):
+		self.add_query_param('PageSize',PageSize)
+
+	def get_Page(self):
+		return self.get_query_params().get('Page')
+
+	def set_Page(self,Page):
+		self.add_query_param('Page',Page)
+
+	def get_ProjectId(self):
+		return self.get_path_params().get('ProjectId')
+
+	def set_ProjectId(self,ProjectId):
+		self.add_path_param('ProjectId',ProjectId)
