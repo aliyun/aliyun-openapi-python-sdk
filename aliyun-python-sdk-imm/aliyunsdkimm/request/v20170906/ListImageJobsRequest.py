0c09284ec82dac1ce7f3db9e5931e69f113646dc
sdk-team <sdk-team@alibabacloud.com>
2020-12-22 06:38:44 +0000

    Minor chagnes.

--- aliyun-python-sdk-imm/aliyunsdkimm/request/v20170906/ListImageJobsRequest.py
+++ aliyun-python-sdk-imm/aliyunsdkimm/request/v20170906/ListImageJobsRequest.py
@@ -18,12 +18,18 @@
 # under the License.
 
 from aliyunsdkcore.request import RpcRequest
+from aliyunsdkimm.endpoint import endpoint_data
 
 class ListImageJobsRequest(RpcRequest):
 
 	def __init__(self):
 		RpcRequest.__init__(self, 'imm', '2017-09-06', 'ListImageJobs','imm')
 		self.set_method('POST')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
 
 	def get_MaxKeys(self):
 		return self.get_query_params().get('MaxKeys')
--- aliyun-python-sdk-imm/aliyunsdkimm/request/v20170906/OpenImmServiceRequest.py
+++ aliyun-python-sdk-imm/aliyunsdkimm/request/v20170906/OpenImmServiceRequest.py
@@ -18,12 +18,18 @@
 # under the License.
 
 from aliyunsdkcore.request import RpcRequest
+from aliyunsdkimm.endpoint import endpoint_data
 
 class OpenImmServiceRequest(RpcRequest):
 
 	def __init__(self):
 		RpcRequest.__init__(self, 'imm', '2017-09-06', 'OpenImmService','imm')
 		self.set_method('POST')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
 
 	def get_OwnerId(self):
 		return self.get_query_params().get('OwnerId')
