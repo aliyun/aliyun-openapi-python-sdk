d29c77df97c693bfaeb9b68b603ece8ac909da31
sdk-team <sdk-team@alibabacloud.com>
2020-12-31 09:56:35 +0000

    Update Open API.

--- aliyun-python-sdk-rds/aliyunsdkrds/request/v20140815/TagResourcesRequest.py
+++ aliyun-python-sdk-rds/aliyunsdkrds/request/v20140815/TagResourcesRequest.py
@@ -37,6 +37,16 @@ class TagResourcesRequest(RpcRequest):
 	def set_ResourceOwnerId(self,ResourceOwnerId):
 		self.add_query_param('ResourceOwnerId',ResourceOwnerId)
 
+	def get_Tags(self):
+		return self.get_query_params().get('Tag')
+
+	def set_Tags(self, Tags):
+		for depth1 in range(len(Tags)):
+			if Tags[depth1].get('Value') is not None:
+				self.add_query_param('Tag.' + str(depth1 + 1) + '.Value', Tags[depth1].get('Value'))
+			if Tags[depth1].get('Key') is not None:
+				self.add_query_param('Tag.' + str(depth1 + 1) + '.Key', Tags[depth1].get('Key'))
+
 	def get_ResourceIds(self):
 		return self.get_query_params().get('ResourceId')
 
@@ -62,13 +72,3 @@ class TagResourcesRequest(RpcRequest):
 
 	def set_ResourceType(self,ResourceType):
 		self.add_query_param('ResourceType',ResourceType)
-
-	def get_Tags(self):
-		return self.get_query_params().get('Tag')
-
-	def set_Tags(self, Tags):
-		for depth1 in range(len(Tags)):
-			if Tags[depth1].get('Value') is not None:
-				self.add_query_param('Tag.' + str(depth1 + 1) + '.Value', Tags[depth1].get('Value'))
-			if Tags[depth1].get('Key') is not None:
-				self.add_query_param('Tag.' + str(depth1 + 1) + '.Key', Tags[depth1].get('Key'))
