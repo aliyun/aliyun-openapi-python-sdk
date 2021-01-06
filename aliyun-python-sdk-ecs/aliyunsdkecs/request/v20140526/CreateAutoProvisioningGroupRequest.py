4dc5b6771afe7890381ca3c2ff16af23b1ee8869
sdk-team <sdk-team@alibabacloud.com>
2021-01-06 03:44:42 +0000

    Support input parameter ClientToken for CreateAutoProvisioningGroup.

--- aliyun-python-sdk-ecs/aliyunsdkecs/request/v20140526/CreateAutoProvisioningGroupRequest.py
+++ aliyun-python-sdk-ecs/aliyunsdkecs/request/v20140526/CreateAutoProvisioningGroupRequest.py
@@ -181,6 +181,12 @@ class CreateAutoProvisioningGroupRequest(RpcRequest):
 	def set_LaunchConfigurationPasswordInherit(self,LaunchConfigurationPasswordInherit):
 		self.add_query_param('LaunchConfiguration.PasswordInherit',LaunchConfigurationPasswordInherit)
 
+	def get_ClientToken(self):
+		return self.get_query_params().get('ClientToken')
+
+	def set_ClientToken(self,ClientToken):
+		self.add_query_param('ClientToken',ClientToken)
+
 	def get_LaunchConfigurationSecurityGroupId(self):
 		return self.get_query_params().get('LaunchConfiguration.SecurityGroupId')
 
