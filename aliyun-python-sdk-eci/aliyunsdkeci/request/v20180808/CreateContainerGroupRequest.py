b117e87547648c68384db33873eaf459d89403ca
wb-hxh549296 <wb-hxh549296@alibaba-inc.com>
2021-01-15 15:07:43 +0800

    支持自定义系统盘

--- aliyun-python-sdk-eci/aliyunsdkeci/request/v20180808/CreateContainerGroupRequest.py
+++ aliyun-python-sdk-eci/aliyunsdkeci/request/v20180808/CreateContainerGroupRequest.py
@@ -591,3 +591,9 @@ class CreateContainerGroupRequest(RpcRequest):
 	def set_SpotPriceLimit(self, SpotPriceLimit):
 		self.add_query_param('SpotPriceLimit', SpotPriceLimit)
 
+	def get_EphemeralStorage(self):
+	return self.get_query_params().get('EphemeralStorage')
+
+	def set_EphemeralStorage(self, EphemeralStorage):
+		self.add_query_param('EphemeralStorage', EphemeralStorage)
+
