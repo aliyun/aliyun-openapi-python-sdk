145abb7914b4970b46ceda32aea87e67709bf44f
sdk-team <sdk-team@alibabacloud.com>
2021-01-18 13:56:47 +0000

    Generated 2019-03-27 for dg.

---
+++ aliyun-python-sdk-dg/aliyunsdkdg/request/v20190327/DeleteDatabaseAccessPointRequest.py
@@ -0,0 +1,63 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkdg.endpoint import endpoint_data
+
+class DeleteDatabaseAccessPointRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'dg', '2019-03-27', 'DeleteDatabaseAccessPoint','dg')
+		self.set_protocol_type('https')
+		self.set_method('POST')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+
+	def get_VpcAZone(self):
+		return self.get_body_params().get('VpcAZone')
+
+	def set_VpcAZone(self,VpcAZone):
+		self.add_body_params('VpcAZone', VpcAZone)
+
+	def get_VpcRegionId(self):
+		return self.get_body_params().get('VpcRegionId')
+
+	def set_VpcRegionId(self,VpcRegionId):
+		self.add_body_params('VpcRegionId', VpcRegionId)
+
+	def get_VSwitchId(self):
+		return self.get_body_params().get('VSwitchId')
+
+	def set_VSwitchId(self,VSwitchId):
+		self.add_body_params('VSwitchId', VSwitchId)
+
+	def get_VpcId(self):
+		return self.get_body_params().get('VpcId')
+
+	def set_VpcId(self,VpcId):
+		self.add_body_params('VpcId', VpcId)
+
+	def get_DbInstanceId(self):
+		return self.get_body_params().get('DbInstanceId')
+
+	def set_DbInstanceId(self,DbInstanceId):
+		self.add_body_params('DbInstanceId', DbInstanceId)
---
+++ aliyun-python-sdk-dg/aliyunsdkdg/request/v20190327/ModifyGatewayRequest.py
@@ -0,0 +1,51 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkdg.endpoint import endpoint_data
+
+class ModifyGatewayRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'dg', '2019-03-27', 'ModifyGateway','dg')
+		self.set_protocol_type('https')
+		self.set_method('POST')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+
+	def get_GatewayDesc(self):
+		return self.get_body_params().get('GatewayDesc')
+
+	def set_GatewayDesc(self,GatewayDesc):
+		self.add_body_params('GatewayDesc', GatewayDesc)
+
+	def get_GatewayName(self):
+		return self.get_body_params().get('GatewayName')
+
+	def set_GatewayName(self,GatewayName):
+		self.add_body_params('GatewayName', GatewayName)
+
+	def get_GatewayId(self):
+		return self.get_body_params().get('GatewayId')
+
+	def set_GatewayId(self,GatewayId):
+		self.add_body_params('GatewayId', GatewayId)
