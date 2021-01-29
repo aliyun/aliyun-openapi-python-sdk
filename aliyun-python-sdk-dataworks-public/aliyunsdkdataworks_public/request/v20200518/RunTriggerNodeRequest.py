e5bb7909f30c689ad0bb9a3cd3d5746d3440ad3a
sdk-team <sdk-team@alibabacloud.com>
2021-01-29 02:20:50 +0000

    Generated 2020-05-18 for dataworks-public.

--- aliyun-python-sdk-dataworks-public/aliyunsdkdataworks_public/request/v20200518/RunTriggerNodeRequest.py
+++ aliyun-python-sdk-dataworks-public/aliyunsdkdataworks_public/request/v20200518/RunTriggerNodeRequest.py
@@ -24,7 +24,7 @@ class RunTriggerNodeRequest(RpcRequest):
 
 	def __init__(self):
 		RpcRequest.__init__(self, 'dataworks-public', '2020-05-18', 'RunTriggerNode')
-		self.set_method('PUT')
+		self.set_method('POST')
 		if hasattr(self, "endpoint_map"):
 			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
 		if hasattr(self, "endpoint_regional"):
