44c99246913c666d56822993c9ef2d84bf6fdf05
sdk-team <sdk-team@alibabacloud.com>
2021-01-30 10:13:14 +0000

    Update rate limit.

--- aliyun-python-sdk-dts/aliyunsdkdts/request/v20200101/DescribeMigrationJobsRequest.py
+++ aliyun-python-sdk-dts/aliyunsdkdts/request/v20200101/DescribeMigrationJobsRequest.py
@@ -61,10 +61,10 @@ class DescribeMigrationJobsRequest(RpcRequest):
 	def set_MigrationJobName(self,MigrationJobName):
 		self.add_query_param('MigrationJobName',MigrationJobName)
 
-	def get_Tag(self):
+	def get_Tags(self):
 		return self.get_query_params().get('Tag')
 
-	def set_Tag(self, Tags):
+	def set_Tags(self, Tags):
 		for depth1 in range(len(Tags)):
 			if Tags[depth1].get('Value') is not None:
 				self.add_query_param('Tag.' + str(depth1 + 1) + '.Value', Tags[depth1].get('Value'))
