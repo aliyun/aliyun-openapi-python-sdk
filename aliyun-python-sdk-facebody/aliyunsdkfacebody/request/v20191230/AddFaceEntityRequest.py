a23d8e601b6437d3ab17c5603b0e820bf7f0ab34
sdk-team <sdk-team@alibabacloud.com>
2021-02-01 09:15:23 +0000

    Release GenerateHumanSketchStyle MergeImageFace AddFaceImageTemplate QueryFaceImageTemplate DeleteFaceImageTemplate.

---
+++ aliyun-python-sdk-facebody/aliyunsdkfacebody/request/v20191230/AddFaceEntityRequest.py
@@ -0,0 +1,50 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkfacebody.endpoint import endpoint_data
+
+class AddFaceEntityRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'facebody', '2019-12-30', 'AddFaceEntity','facebody')
+		self.set_method('POST')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+
+	def get_EntityId(self):
+		return self.get_body_params().get('EntityId')
+
+	def set_EntityId(self,EntityId):
+		self.add_body_params('EntityId', EntityId)
+
+	def get_Labels(self):
+		return self.get_body_params().get('Labels')
+
+	def set_Labels(self,Labels):
+		self.add_body_params('Labels', Labels)
+
+	def get_DbName(self):
+		return self.get_body_params().get('DbName')
+
+	def set_DbName(self,DbName):
+		self.add_body_params('DbName', DbName)
