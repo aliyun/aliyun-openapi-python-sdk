c32156ecdb7e6692a003605dbf11e77195673886
sdk-team <sdk-team@alibabacloud.com>
2021-02-07 10:01:43 +0000

    Support patch manager apis.

---
+++ aliyun-python-sdk-oos/aliyunsdkoos/request/v20190601/UpdateStateConfigurationRequest.py
@@ -0,0 +1,86 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkoos.endpoint import endpoint_data
+
+class UpdateStateConfigurationRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'oos', '2019-06-01', 'UpdateStateConfiguration','oos')
+		self.set_method('POST')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+
+	def get_ScheduleType(self):
+		return self.get_query_params().get('ScheduleType')
+
+	def set_ScheduleType(self,ScheduleType):
+		self.add_query_param('ScheduleType',ScheduleType)
+
+	def get_ClientToken(self):
+		return self.get_query_params().get('ClientToken')
+
+	def set_ClientToken(self,ClientToken):
+		self.add_query_param('ClientToken',ClientToken)
+
+	def get_Description(self):
+		return self.get_query_params().get('Description')
+
+	def set_Description(self,Description):
+		self.add_query_param('Description',Description)
+
+	def get_Targets(self):
+		return self.get_query_params().get('Targets')
+
+	def set_Targets(self,Targets):
+		self.add_query_param('Targets',Targets)
+
+	def get_ScheduleExpression(self):
+		return self.get_query_params().get('ScheduleExpression')
+
+	def set_ScheduleExpression(self,ScheduleExpression):
+		self.add_query_param('ScheduleExpression',ScheduleExpression)
+
+	def get_ConfigureMode(self):
+		return self.get_query_params().get('ConfigureMode')
+
+	def set_ConfigureMode(self,ConfigureMode):
+		self.add_query_param('ConfigureMode',ConfigureMode)
+
+	def get_Tags(self):
+		return self.get_query_params().get('Tags')
+
+	def set_Tags(self,Tags):
+		self.add_query_param('Tags',Tags)
+
+	def get_Parameters(self):
+		return self.get_query_params().get('Parameters')
+
+	def set_Parameters(self,Parameters):
+		self.add_query_param('Parameters',Parameters)
+
+	def get_StateConfigurationId(self):
+		return self.get_query_params().get('StateConfigurationId')
+
+	def set_StateConfigurationId(self,StateConfigurationId):
+		self.add_query_param('StateConfigurationId',StateConfigurationId)
