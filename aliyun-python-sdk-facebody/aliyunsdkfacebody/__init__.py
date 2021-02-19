748ffb62b0b1b6494b4a39785dd99a421c33d135
sdk-team <sdk-team@alibabacloud.com>
2021-02-19 08:47:17 +0000

    Release GenerateHumanAnimeStyle CountCrowd.

--- aliyun-python-sdk-facebody/aliyunsdkfacebody/__init__.py
+++ aliyun-python-sdk-facebody/aliyunsdkfacebody/__init__.py
@@ -1,6 +1,10 @@
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 __version__ = '1.2.13'
+=======
+__version__ = '1.2.2'
+>>>>>>> Stashed changes
 =======
 __version__ = '1.2.2'
 >>>>>>> Stashed changes
