4d148ac9cd5543fde3cd46d037b4f0bbbcedb11a
sdk-team <sdk-team@alibabacloud.com>
2021-02-19 08:35:50 +0000

    Generated 2019-03-08 for ivision.

---
+++ aliyun-python-sdk-ivision/aliyunsdkivision/request/v20190308/GetAlgorithmHistogramsRequest.py
@@ -0,0 +1,56 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkivision.endpoint import endpoint_data
+
+class GetAlgorithmHistogramsRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'ivision', '2019-03-08', 'GetAlgorithmHistograms','ivision')
+		self.set_method('POST')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+
+	def get_EndDate(self):
+		return self.get_query_params().get('EndDate')
+
+	def set_EndDate(self,EndDate):
+		self.add_query_param('EndDate',EndDate)
+
+	def get_StartDate(self):
+		return self.get_query_params().get('StartDate')
+
+	def set_StartDate(self,StartDate):
+		self.add_query_param('StartDate',StartDate)
+
+	def get_AlgorithmCode(self):
+		return self.get_query_params().get('AlgorithmCode')
+
+	def set_AlgorithmCode(self,AlgorithmCode):
+		self.add_query_param('AlgorithmCode',AlgorithmCode)
+
+	def get_AggregateType(self):
+		return self.get_query_params().get('AggregateType')
+
+	def set_AggregateType(self,AggregateType):
+		self.add_query_param('AggregateType',AggregateType)
