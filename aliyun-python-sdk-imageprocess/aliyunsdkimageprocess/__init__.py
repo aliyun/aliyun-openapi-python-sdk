eeb2a4806f886c171592df4c60bd7c6b14e3ef78
sdk-team <sdk-team@alibabacloud.com>
2021-02-19 08:55:41 +0000

    Update DetectLungNodule.

--- aliyun-python-sdk-imageprocess/aliyunsdkimageprocess/__init__.py
+++ aliyun-python-sdk-imageprocess/aliyunsdkimageprocess/__init__.py
@@ -1,5 +1,9 @@
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 __version__ = '1.0.5'
+=======
+__version__ = '1.0.8'
+>>>>>>> Stashed changes
 =======
 __version__ = '1.0.8'
 >>>>>>> Stashed changes
