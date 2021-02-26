0ff1f7fe523fccec8dbc7d7aadab2ccbffffa135
sdk-team <sdk-team@alibabacloud.com>
2021-02-26 06:04:37 +0000

    New version.

---
+++ aliyun-python-sdk-workbench-ide/aliyunsdkworkbench_ide/request/v20210121/GetAppPackageUploadUrlRequest.py
@@ -0,0 +1,50 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+
+class GetAppPackageUploadUrlRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Workbench-ide', '2021-01-21', 'GetAppPackageUploadUrl')
+		self.set_method('POST')
+
+	def get_AppId(self):
+		return self.get_query_params().get('AppId')
+
+	def set_AppId(self,AppId):
+		self.add_query_param('AppId',AppId)
+
+	def get_CurrentOrgId(self):
+		return self.get_query_params().get('CurrentOrgId')
+
+	def set_CurrentOrgId(self,CurrentOrgId):
+		self.add_query_param('CurrentOrgId',CurrentOrgId)
+
+	def get_PackageName(self):
+		return self.get_query_params().get('PackageName')
+
+	def set_PackageName(self,PackageName):
+		self.add_query_param('PackageName',PackageName)
+
+	def get_PackageSize(self):
+		return self.get_query_params().get('PackageSize')
+
+	def set_PackageSize(self,PackageSize):
+		self.add_query_param('PackageSize',PackageSize)
