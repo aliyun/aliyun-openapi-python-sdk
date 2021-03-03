45b9ecc669a8a96fc4d1bfd48ca9886442719688
sdk-team <sdk-team@alibabacloud.com>
2021-03-03 02:43:44 +0000

    Update ScreenChestCT.

--- aliyun-python-sdk-imageprocess/aliyunsdkimageprocess/__init__.py
+++ aliyun-python-sdk-imageprocess/aliyunsdkimageprocess/__init__.py
@@ -1,9 +1,1 @@
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-__version__ = '1.0.5'
-=======
-__version__ = '1.0.8'
->>>>>>> Stashed changes
-=======
-__version__ = '1.0.8'
->>>>>>> Stashed changes
+__version__ = '1.0.11'
