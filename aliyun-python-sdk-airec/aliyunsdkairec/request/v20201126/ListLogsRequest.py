9c78a5cf493fc587716ba9f626a1d9222a566a6b
sdk-team <sdk-team@alibabacloud.com>
2021-03-30 10:23:31 +0000

    Generated 2020-11-26 for Airec.

---
+++ aliyun-python-sdk-airec/aliyunsdkairec/request/v20201126/ListLogsRequest.py
@@ -0,0 +1,69 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RoaRequest
+from aliyunsdkairec.endpoint import endpoint_data
+
+class ListLogsRequest(RoaRequest):
+
+	def __init__(self):
+		RoaRequest.__init__(self, 'Airec', '2020-11-26', 'ListLogs','airec')
+		self.set_uri_pattern('/v2/openapi/instances/[instanceId]/logs')
+		self.set_method('GET')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+
+	def get_instanceId(self):
+		return self.get_path_params().get('instanceId')
+
+	def set_instanceId(self,instanceId):
+		self.add_path_param('instanceId',instanceId)
+
+	def get_size(self):
+		return self.get_query_params().get('size')
+
+	def set_size(self,size):
+		self.add_query_param('size',size)
+
+	def get_queryParams(self):
+		return self.get_query_params().get('queryParams')
+
+	def set_queryParams(self,queryParams):
+		self.add_query_param('queryParams',queryParams)
+
+	def get_endTime(self):
+		return self.get_query_params().get('endTime')
+
+	def set_endTime(self,endTime):
+		self.add_query_param('endTime',endTime)
+
+	def get_startTime(self):
+		return self.get_query_params().get('startTime')
+
+	def set_startTime(self,startTime):
+		self.add_query_param('startTime',startTime)
+
+	def get_page(self):
+		return self.get_query_params().get('page')
+
+	def set_page(self,page):
+		self.add_query_param('page',page)
