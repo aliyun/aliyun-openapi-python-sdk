c3cb3c956853a9e93939690bc16cb184fb67f8e4
sdk-team <sdk-team@alibabacloud.com>
2021-04-17 02:36:08 +0000

    Update RunMedQA.

--- aliyun-python-sdk-imageprocess/aliyunsdkimageprocess/request/v20200320/RunMedQARequest.py
+++ aliyun-python-sdk-imageprocess/aliyunsdkimageprocess/request/v20200320/RunMedQARequest.py
@@ -31,20 +31,56 @@ class RunMedQARequest(RpcRequest):
 			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
 
 
-	def get_Question(self):
-		return self.get_body_params().get('Question')
+	def get_SessionId(self):
+		return self.get_body_params().get('SessionId')
 
-	def set_Question(self,Question):
-		self.add_body_params('Question', Question)
-
-	def get_OrgId(self):
-		return self.get_body_params().get('OrgId')
-
-	def set_OrgId(self,OrgId):
-		self.add_body_params('OrgId', OrgId)
+	def set_SessionId(self,SessionId):
+		self.add_body_params('SessionId', SessionId)
 
 	def get_OrgName(self):
 		return self.get_body_params().get('OrgName')
 
 	def set_OrgName(self,OrgName):
 		self.add_body_params('OrgName', OrgName)
+
+	def get_AnswerImageDataLists(self):
+		return self.get_body_params().get('AnswerImageDataList')
+
+	def set_AnswerImageDataLists(self, AnswerImageDataLists):
+		for depth1 in range(len(AnswerImageDataLists)):
+			if AnswerImageDataLists[depth1].get('AnswerImageData') is not None:
+				self.add_body_params('AnswerImageDataList.' + str(depth1 + 1) + '.AnswerImageData', AnswerImageDataLists[depth1].get('AnswerImageData'))
+
+	def get_AnswerTextLists(self):
+		return self.get_body_params().get('AnswerTextList')
+
+	def set_AnswerTextLists(self, AnswerTextLists):
+		for depth1 in range(len(AnswerTextLists)):
+			if AnswerTextLists[depth1].get('AnswerText') is not None:
+				self.add_body_params('AnswerTextList.' + str(depth1 + 1) + '.AnswerText', AnswerTextLists[depth1].get('AnswerText'))
+
+	def get_Department(self):
+		return self.get_body_params().get('Department')
+
+	def set_Department(self,Department):
+		self.add_body_params('Department', Department)
+
+	def get_AnswerImageURLLists(self):
+		return self.get_body_params().get('AnswerImageURLList')
+
+	def set_AnswerImageURLLists(self, AnswerImageURLLists):
+		for depth1 in range(len(AnswerImageURLLists)):
+			if AnswerImageURLLists[depth1].get('AnswerImageURL') is not None:
+				self.add_body_params('AnswerImageURLList.' + str(depth1 + 1) + '.AnswerImageURL', AnswerImageURLLists[depth1].get('AnswerImageURL'))
+
+	def get_QuestionType(self):
+		return self.get_body_params().get('QuestionType')
+
+	def set_QuestionType(self,QuestionType):
+		self.add_body_params('QuestionType', QuestionType)
+
+	def get_OrgId(self):
+		return self.get_body_params().get('OrgId')
+
+	def set_OrgId(self,OrgId):
+		self.add_body_params('OrgId', OrgId)
