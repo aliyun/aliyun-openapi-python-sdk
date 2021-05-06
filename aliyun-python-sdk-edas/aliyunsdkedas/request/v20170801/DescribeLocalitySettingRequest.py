ae8a910a72535d4f0206239775100cdd76149dde
sdk-team <sdk-team@alibabacloud.com>
2021-05-06 07:22:07 +0000

    Modify API GetAppDeployment.

---
+++ aliyun-python-sdk-edas/aliyunsdkedas/request/v20170801/DescribeLocalitySettingRequest.py
@@ -0,0 +1,51 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RoaRequest
+from aliyunsdkedas.endpoint import endpoint_data
+
+class DescribeLocalitySettingRequest(RoaRequest):
+
+	def __init__(self):
+		RoaRequest.__init__(self, 'Edas', '2017-08-01', 'DescribeLocalitySetting','Edas')
+		self.set_uri_pattern('/pop/sp/applications/locality/setting')
+		self.set_method('GET')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+
+	def get_NamespaceId(self):
+		return self.get_query_params().get('NamespaceId')
+
+	def set_NamespaceId(self,NamespaceId):
+		self.add_query_param('NamespaceId',NamespaceId)
+
+	def get_AppId(self):
+		return self.get_query_params().get('AppId')
+
+	def set_AppId(self,AppId):
+		self.add_query_param('AppId',AppId)
+
+	def get_Region(self):
+		return self.get_query_params().get('Region')
+
+	def set_Region(self,Region):
+		self.add_query_param('Region',Region)
