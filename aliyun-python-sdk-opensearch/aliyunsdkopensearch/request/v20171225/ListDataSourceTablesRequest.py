6954e3d62ddd795391b9d62647fec66966c08fec
sdk-team <sdk-team@alibabacloud.com>
2021-05-10 04:26:18 +0000

    Add API GenerateMergedTable.

---
+++ aliyun-python-sdk-opensearch/aliyunsdkopensearch/request/v20171225/ListDataSourceTablesRequest.py
@@ -0,0 +1,45 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RoaRequest
+from aliyunsdkopensearch.endpoint import endpoint_data
+
+class ListDataSourceTablesRequest(RoaRequest):
+
+	def __init__(self):
+		RoaRequest.__init__(self, 'OpenSearch', '2017-12-25', 'ListDataSourceTables','opensearch')
+		self.set_uri_pattern('/v4/openapi/assist/data-sources/[dataSourceType]/tables')
+		self.set_method('GET')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+
+	def get_dataSourceType(self):
+		return self.get_path_params().get('dataSourceType')
+
+	def set_dataSourceType(self,dataSourceType):
+		self.add_path_param('dataSourceType',dataSourceType)
+
+	def get_params(self):
+		return self.get_query_params().get('params')
+
+	def set_params(self,params):
+		self.add_query_param('params',params)
