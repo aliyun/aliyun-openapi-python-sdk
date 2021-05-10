5866cd94b9a44c0a7118165287faf5f99041ee59
sdk-team <sdk-team@alibabacloud.com>
2021-05-10 02:40:02 +0000

    Delete Api.

---
+++ aliyun-python-sdk-dcdn/aliyunsdkdcdn/request/v20180115/DescribeDcdnSecFuncInfoRequest.py
@@ -0,0 +1,50 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkdcdn.endpoint import endpoint_data
+
+class DescribeDcdnSecFuncInfoRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'dcdn', '2018-01-15', 'DescribeDcdnSecFuncInfo')
+		self.set_method('POST')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+
+	def get_Lang(self):
+		return self.get_query_params().get('Lang')
+
+	def set_Lang(self,Lang):
+		self.add_query_param('Lang',Lang)
+
+	def get_OwnerId(self):
+		return self.get_query_params().get('OwnerId')
+
+	def set_OwnerId(self,OwnerId):
+		self.add_query_param('OwnerId',OwnerId)
+
+	def get_SecFuncType(self):
+		return self.get_query_params().get('SecFuncType')
+
+	def set_SecFuncType(self,SecFuncType):
+		self.add_query_param('SecFuncType',SecFuncType)
