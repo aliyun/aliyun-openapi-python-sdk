7666a08d1fb5aed2b6f36460f8288a8bacc6f2f3
sdk-team <sdk-team@alibabacloud.com>
2021-05-19 11:04:24 +0000

    Update API.

--- aliyun-python-sdk-qualitycheck/aliyunsdkqualitycheck/request/v20190115/GetScoreInfoRequest.py
+++ aliyun-python-sdk-qualitycheck/aliyunsdkqualitycheck/request/v20190115/GetScoreInfoRequest.py
@@ -18,18 +18,12 @@
 # under the License.
 
 from aliyunsdkcore.request import RpcRequest
-from aliyunsdkqualitycheck.endpoint import endpoint_data
 
 class GetScoreInfoRequest(RpcRequest):
 
 	def __init__(self):
-		RpcRequest.__init__(self, 'Qualitycheck', '2019-01-15', 'GetScoreInfo','Qualitycheck')
+		RpcRequest.__init__(self, 'Qualitycheck', '2019-01-15', 'GetScoreInfo')
 		self.set_method('POST')
-		if hasattr(self, "endpoint_map"):
-			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
-		if hasattr(self, "endpoint_regional"):
-			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
-
 
 	def get_JsonStr(self):
 		return self.get_query_params().get('JsonStr')
