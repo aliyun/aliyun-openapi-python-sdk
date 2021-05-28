a80a80ddcae7ff648ac839cbd4d8e544b878d9ee
sdk-team <sdk-team@alibabacloud.com>
2021-05-28 03:54:43 +0000

    Create Lindorm Open api.

--- aliyun-python-sdk-hitsdb/aliyunsdkhitsdb/request/v20170601/RenameHiTSDBInstanceAliasRequest.py
+++ aliyun-python-sdk-hitsdb/aliyunsdkhitsdb/request/v20170601/RenameHiTSDBInstanceAliasRequest.py
@@ -20,10 +20,10 @@
 from aliyunsdkcore.request import RpcRequest
 from aliyunsdkhitsdb.endpoint import endpoint_data
 
-class UpdateInstanceIpWhiteListRequest(RpcRequest):
+class RenameHiTSDBInstanceAliasRequest(RpcRequest):
 
 	def __init__(self):
-		RpcRequest.__init__(self, 'hitsdb', '2020-06-15', 'UpdateInstanceIpWhiteList','hitsdb')
+		RpcRequest.__init__(self, 'hitsdb', '2017-06-01', 'RenameHiTSDBInstanceAlias','hitsdb')
 		self.set_method('POST')
 		if hasattr(self, "endpoint_map"):
 			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
@@ -55,26 +55,26 @@ class UpdateInstanceIpWhiteListRequest(RpcRequest):
 	def set_OwnerAccount(self,OwnerAccount):
 		self.add_query_param('OwnerAccount',OwnerAccount)
 
+	def get_InstanceAlias(self):
+		return self.get_query_params().get('InstanceAlias')
+
+	def set_InstanceAlias(self,InstanceAlias):
+		self.add_query_param('InstanceAlias',InstanceAlias)
+
 	def get_OwnerId(self):
 		return self.get_query_params().get('OwnerId')
 
 	def set_OwnerId(self,OwnerId):
 		self.add_query_param('OwnerId',OwnerId)
 
-	def get_GroupName(self):
-		return self.get_query_params().get('GroupName')
-
-	def set_GroupName(self,GroupName):
-		self.add_query_param('GroupName',GroupName)
-
-	def get_SecurityIpList(self):
-		return self.get_query_params().get('SecurityIpList')
-
-	def set_SecurityIpList(self,SecurityIpList):
-		self.add_query_param('SecurityIpList',SecurityIpList)
-
 	def get_InstanceId(self):
 		return self.get_query_params().get('InstanceId')
 
 	def set_InstanceId(self,InstanceId):
 		self.add_query_param('InstanceId',InstanceId)
+
+	def get_AppKey(self):
+		return self.get_query_params().get('AppKey')
+
+	def set_AppKey(self,AppKey):
+		self.add_query_param('AppKey',AppKey)
