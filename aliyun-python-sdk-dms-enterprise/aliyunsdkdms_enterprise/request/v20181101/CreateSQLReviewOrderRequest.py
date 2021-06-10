8d7c69486c4c0f10ca1566d86750e1793cb0bc47
sdk-team <sdk-team@alibabacloud.com>
2021-06-10 14:06:52 +0000

    Support SQLReview Order API.

---
+++ aliyun-python-sdk-dms-enterprise/aliyunsdkdms_enterprise/request/v20181101/CreateSQLReviewOrderRequest.py
@@ -0,0 +1,54 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkdms_enterprise.endpoint import endpoint_data
+import json
+
+class CreateSQLReviewOrderRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'dms-enterprise', '2018-11-01', 'CreateSQLReviewOrder','dms-enterprise')
+		self.set_method('POST')
+
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+	def get_Tid(self): # Long
+		return self.get_query_params().get('Tid')
+
+	def set_Tid(self, Tid):  # Long
+		self.add_query_param('Tid', Tid)
+	def get_Param(self): # Struct
+		return self.get_query_params().get('Param')
+
+	def set_Param(self, Param):  # Struct
+		self.add_query_param("Param", json.dumps(Param))
+	def get_Comment(self): # String
+		return self.get_query_params().get('Comment')
+
+	def set_Comment(self, Comment):  # String
+		self.add_query_param('Comment', Comment)
+	def get_RelatedUserList(self): # Array
+		return self.get_query_params().get('RelatedUserList')
+
+	def set_RelatedUserList(self, RelatedUserList):  # Array
+		self.add_query_param("RelatedUserList", json.dumps(RelatedUserList))
