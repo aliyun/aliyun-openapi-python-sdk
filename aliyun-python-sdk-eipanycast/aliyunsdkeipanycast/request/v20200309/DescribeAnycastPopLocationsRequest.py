7a41e08510ec2e563d7fb04d66d8a4c35e538d66
sdk-team <sdk-team@alibabacloud.com>
2021-07-07 02:32:44 +0000

    Supported AAA for BBB.

---
+++ aliyun-python-sdk-eipanycast/aliyunsdkeipanycast/request/v20200309/DescribeAnycastPopLocationsRequest.py
@@ -0,0 +1,38 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkeipanycast.endpoint import endpoint_data
+
+class DescribeAnycastPopLocationsRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Eipanycast', '2020-03-09', 'DescribeAnycastPopLocations','eipanycast')
+		self.set_method('POST')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+
+	def get_ServiceLocation(self):
+		return self.get_query_params().get('ServiceLocation')
+
+	def set_ServiceLocation(self,ServiceLocation):
+		self.add_query_param('ServiceLocation',ServiceLocation)
