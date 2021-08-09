f20afb8e1b80786604774f9b513cc00ffa7c8ded
sdk-team <sdk-team@alibabacloud.com>
2021-08-09 06:54:57 +0000

    SubmitURLUploadJob.

--- aliyun-python-sdk-mts/aliyunsdkmts/request/v20140618/SubmitURLUploadJobRequest.py
+++ aliyun-python-sdk-mts/aliyunsdkmts/request/v20140618/SubmitURLUploadJobRequest.py
@@ -52,3 +52,8 @@ class SubmitURLUploadJobRequest(RpcRequest):
 
 	def set_UserData(self, UserData):  # String
 		self.add_query_param('UserData', UserData)
+	def get_Region(self): # String
+		return self.get_query_params().get('Region')
+
+	def set_Region(self, Region):  # String
+		self.add_query_param('Region', Region)
