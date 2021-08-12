196069f265db199b0bb49c62663379e89fad6903
sdk-team <sdk-team@alibabacloud.com>
2021-08-12 02:07:08 +0000

    Support CreateProxy, DeleteProxy, GetProxy, ListProxies, CreateProxyAccess, DeleteProxyAccess, ListProxyAccesses, InspectProxyAccessSecret Safety Protection Management APIs.

---
+++ aliyun-python-sdk-dms-enterprise/aliyunsdkdms_enterprise/request/v20181101/CreateProxyRequest.py
@@ -0,0 +1,53 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkdms_enterprise.endpoint import endpoint_data
+
+class CreateProxyRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'dms-enterprise', '2018-11-01', 'CreateProxy','dms-enterprise')
+		self.set_method('POST')
+
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+	def get_Tid(self): # Long
+		return self.get_query_params().get('Tid')
+
+	def set_Tid(self, Tid):  # Long
+		self.add_query_param('Tid', Tid)
+	def get_Password(self): # String
+		return self.get_query_params().get('Password')
+
+	def set_Password(self, Password):  # String
+		self.add_query_param('Password', Password)
+	def get_InstanceId(self): # Long
+		return self.get_query_params().get('InstanceId')
+
+	def set_InstanceId(self, InstanceId):  # Long
+		self.add_query_param('InstanceId', InstanceId)
+	def get_Username(self): # String
+		return self.get_query_params().get('Username')
+
+	def set_Username(self, Username):  # String
+		self.add_query_param('Username', Username)
