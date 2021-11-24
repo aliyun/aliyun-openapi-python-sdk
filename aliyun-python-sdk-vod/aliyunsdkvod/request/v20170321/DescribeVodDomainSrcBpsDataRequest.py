07ba67a8e147d4c9b016412f2a6620b2db6004af
sdk-team <sdk-team@alibabacloud.com>
2021-11-24 03:51:26 +0000

    Set API DescribeVodDomainSrcBpsData to public.

--- aliyun-python-sdk-vod/aliyunsdkvod/request/v20170321/DescribeVodDomainSrcBpsDataRequest.py
+++ aliyun-python-sdk-vod/aliyunsdkvod/request/v20170321/DescribeVodDomainSrcBpsDataRequest.py
@@ -20,10 +20,10 @@
 from aliyunsdkcore.request import RpcRequest
 from aliyunsdkvod.endpoint import endpoint_data
 
-class DeleteDetectionTemplateRequest(RpcRequest):
+class DescribeVodDomainSrcBpsDataRequest(RpcRequest):
 
 	def __init__(self):
-		RpcRequest.__init__(self, 'vod', '2017-03-21', 'DeleteDetectionTemplate','vod')
+		RpcRequest.__init__(self, 'vod', '2017-03-21', 'DescribeVodDomainSrcBpsData','vod')
 		self.set_method('POST')
 		if hasattr(self, "endpoint_map"):
 			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
@@ -31,8 +31,32 @@ class DeleteDetectionTemplateRequest(RpcRequest):
 			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
 
 
-	def get_TemplateId(self):
-		return self.get_query_params().get('TemplateId')
+	def get_StartTime(self):
+		return self.get_query_params().get('StartTime')
 
-	def set_TemplateId(self,TemplateId):
-		self.add_query_param('TemplateId',TemplateId)
+	def set_StartTime(self,StartTime):
+		self.add_query_param('StartTime',StartTime)
+
+	def get_DomainName(self):
+		return self.get_query_params().get('DomainName')
+
+	def set_DomainName(self,DomainName):
+		self.add_query_param('DomainName',DomainName)
+
+	def get_EndTime(self):
+		return self.get_query_params().get('EndTime')
+
+	def set_EndTime(self,EndTime):
+		self.add_query_param('EndTime',EndTime)
+
+	def get_OwnerId(self):
+		return self.get_query_params().get('OwnerId')
+
+	def set_OwnerId(self,OwnerId):
+		self.add_query_param('OwnerId',OwnerId)
+
+	def get_Interval(self):
+		return self.get_query_params().get('Interval')
+
+	def set_Interval(self,Interval):
+		self.add_query_param('Interval',Interval)
