e0474e262fbc49c5b6449d8420a472f28ac13981
sdk-team <sdk-team@alibabacloud.com>
2021-12-22 12:58:57 +0000

    Public V431.

---
+++ aliyun-python-sdk-rsimganalys/aliyunsdkrsimganalys/request/v20190801/CreateTaskRequest.py
@@ -0,0 +1,92 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkrsimganalys.endpoint import endpoint_data
+
+class CreateTaskRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'rsimganalys', '2019-08-01', 'CreateTask','rsimganalys')
+		self.set_method('POST')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+
+	def get_Confidence(self):
+		return self.get_query_params().get('Confidence')
+
+	def set_Confidence(self,Confidence):
+		self.add_query_param('Confidence',Confidence)
+
+	def get_FilterValue(self):
+		return self.get_query_params().get('FilterValue')
+
+	def set_FilterValue(self,FilterValue):
+		self.add_query_param('FilterValue',FilterValue)
+
+	def get_Description(self):
+		return self.get_query_params().get('Description')
+
+	def set_Description(self,Description):
+		self.add_query_param('Description',Description)
+
+	def get_ShpFilter(self):
+		return self.get_query_params().get('ShpFilter')
+
+	def set_ShpFilter(self,ShpFilter):
+		self.add_query_param('ShpFilter',ShpFilter)
+
+	def get_ProductType(self):
+		return self.get_query_params().get('ProductType')
+
+	def set_ProductType(self,ProductType):
+		self.add_query_param('ProductType',ProductType)
+
+	def get_ZoneList(self):
+		return self.get_query_params().get('ZoneList')
+
+	def set_ZoneList(self,ZoneList):
+		self.add_query_param('ZoneList',ZoneList)
+
+	def get_SrcImageId(self):
+		return self.get_query_params().get('SrcImageId')
+
+	def set_SrcImageId(self,SrcImageId):
+		self.add_query_param('SrcImageId',SrcImageId)
+
+	def get_Appkey(self):
+		return self.get_query_params().get('Appkey')
+
+	def set_Appkey(self,Appkey):
+		self.add_query_param('Appkey',Appkey)
+
+	def get_DstImageId(self):
+		return self.get_query_params().get('DstImageId')
+
+	def set_DstImageId(self,DstImageId):
+		self.add_query_param('DstImageId',DstImageId)
+
+	def get_JobName(self):
+		return self.get_query_params().get('JobName')
+
+	def set_JobName(self,JobName):
+		self.add_query_param('JobName',JobName)
---
+++ aliyun-python-sdk-rsimganalys/aliyunsdkrsimganalys/request/v20190801/ListTasksRequest.py
@@ -0,0 +1,74 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkrsimganalys.endpoint import endpoint_data
+
+class ListTasksRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'rsimganalys', '2019-08-01', 'ListTasks','rsimganalys')
+		self.set_method('POST')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+
+	def get_SubmitTime(self):
+		return self.get_query_params().get('SubmitTime')
+
+	def set_SubmitTime(self,SubmitTime):
+		self.add_query_param('SubmitTime',SubmitTime)
+
+	def get_RunStatus(self):
+		return self.get_query_params().get('RunStatus')
+
+	def set_RunStatus(self,RunStatus):
+		self.add_query_param('RunStatus',RunStatus)
+
+	def get_ProductType(self):
+		return self.get_query_params().get('ProductType')
+
+	def set_ProductType(self,ProductType):
+		self.add_query_param('ProductType',ProductType)
+
+	def get_PageNo(self):
+		return self.get_query_params().get('PageNo')
+
+	def set_PageNo(self,PageNo):
+		self.add_query_param('PageNo',PageNo)
+
+	def get_PageSize(self):
+		return self.get_query_params().get('PageSize')
+
+	def set_PageSize(self,PageSize):
+		self.add_query_param('PageSize',PageSize)
+
+	def get_Appkey(self):
+		return self.get_query_params().get('Appkey')
+
+	def set_Appkey(self,Appkey):
+		self.add_query_param('Appkey',Appkey)
+
+	def get_JobName(self):
+		return self.get_query_params().get('JobName')
+
+	def set_JobName(self,JobName):
+		self.add_query_param('JobName',JobName)
