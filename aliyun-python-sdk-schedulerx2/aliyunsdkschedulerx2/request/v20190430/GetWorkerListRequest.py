101e29a005ceb0357d9dce06ac6e9f28431e32a3
sdk-team <sdk-team@alibabacloud.com>
2021-12-30 10:42:49 +0000

    Supported Get Worker List.

--- aliyun-python-sdk-schedulerx2/aliyunsdkschedulerx2/request/v20190430/GetWorkerListRequest.py
+++ aliyun-python-sdk-schedulerx2/aliyunsdkschedulerx2/request/v20190430/GetWorkerListRequest.py
@@ -20,10 +20,10 @@
 from aliyunsdkcore.request import RpcRequest
 from aliyunsdkschedulerx2.endpoint import endpoint_data
 
-class ExecuteJobRequest(RpcRequest):
+class GetWorkerListRequest(RpcRequest):
 
 	def __init__(self):
-		RpcRequest.__init__(self, 'schedulerx2', '2019-04-30', 'ExecuteJob')
+		RpcRequest.__init__(self, 'schedulerx2', '2019-04-30', 'GetWorkerList')
 		self.set_method('GET')
 
 		if hasattr(self, "endpoint_map"):
@@ -36,43 +36,13 @@ class ExecuteJobRequest(RpcRequest):
 
 	def set_NamespaceSource(self, NamespaceSource):  # String
 		self.add_query_param('NamespaceSource', NamespaceSource)
-	def get_CheckJobStatus(self): # Boolean
-		return self.get_query_params().get('CheckJobStatus')
-
-	def set_CheckJobStatus(self, CheckJobStatus):  # Boolean
-		self.add_query_param('CheckJobStatus', CheckJobStatus)
 	def get_GroupId(self): # String
 		return self.get_query_params().get('GroupId')
 
 	def set_GroupId(self, GroupId):  # String
 		self.add_query_param('GroupId', GroupId)
-	def get_Label(self): # String
-		return self.get_query_params().get('Label')
-
-	def set_Label(self, Label):  # String
-		self.add_query_param('Label', Label)
-	def get_DesignateType(self): # Integer
-		return self.get_query_params().get('DesignateType')
-
-	def set_DesignateType(self, DesignateType):  # Integer
-		self.add_query_param('DesignateType', DesignateType)
-	def get_JobId(self): # Long
-		return self.get_query_params().get('JobId')
-
-	def set_JobId(self, JobId):  # Long
-		self.add_query_param('JobId', JobId)
 	def get_Namespace(self): # String
 		return self.get_query_params().get('Namespace')
 
 	def set_Namespace(self, Namespace):  # String
 		self.add_query_param('Namespace', Namespace)
-	def get_Worker(self): # String
-		return self.get_query_params().get('Worker')
-
-	def set_Worker(self, Worker):  # String
-		self.add_query_param('Worker', Worker)
-	def get_InstanceParameters(self): # String
-		return self.get_query_params().get('InstanceParameters')
-
-	def set_InstanceParameters(self, InstanceParameters):  # String
-		self.add_query_param('InstanceParameters', InstanceParameters)
