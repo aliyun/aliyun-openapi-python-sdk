4ad08d98c6579eff7e7f7bcfb5470d52946a32ac
sdk-team <sdk-team@alibabacloud.com>
2022-04-22 03:40:53 +0000

    Iot_20180120 old version sdk.

---
+++ aliyun-python-sdk-iot/aliyunsdkiot/request/v20180120/CreateSoundCodeScheduleRequest.py
@@ -0,0 +1,62 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkiot.endpoint import endpoint_data
+
+class CreateSoundCodeScheduleRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Iot', '2018-01-20', 'CreateSoundCodeSchedule')
+		self.set_method('POST')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+
+	def get_Description(self):
+		return self.get_body_params().get('Description')
+
+	def set_Description(self,Description):
+		self.add_body_params('Description', Description)
+
+	def get_StartDate(self):
+		return self.get_body_params().get('StartDate')
+
+	def set_StartDate(self,StartDate):
+		self.add_body_params('StartDate', StartDate)
+
+	def get_IotInstanceId(self):
+		return self.get_body_params().get('IotInstanceId')
+
+	def set_IotInstanceId(self,IotInstanceId):
+		self.add_body_params('IotInstanceId', IotInstanceId)
+
+	def get_EndDate(self):
+		return self.get_body_params().get('EndDate')
+
+	def set_EndDate(self,EndDate):
+		self.add_body_params('EndDate', EndDate)
+
+	def get_Name(self):
+		return self.get_body_params().get('Name')
+
+	def set_Name(self,Name):
+		self.add_body_params('Name', Name)
---
+++ aliyun-python-sdk-iot/aliyunsdkiot/request/v20180120/UpdateSchedulePeriodRequest.py
@@ -0,0 +1,68 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkiot.endpoint import endpoint_data
+
+class UpdateSchedulePeriodRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Iot', '2018-01-20', 'UpdateSchedulePeriod')
+		self.set_method('POST')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+
+	def get_PeriodCode(self):
+		return self.get_body_params().get('PeriodCode')
+
+	def set_PeriodCode(self,PeriodCode):
+		self.add_body_params('PeriodCode', PeriodCode)
+
+	def get_Description(self):
+		return self.get_body_params().get('Description')
+
+	def set_Description(self,Description):
+		self.add_body_params('Description', Description)
+
+	def get_StartTime(self):
+		return self.get_body_params().get('StartTime')
+
+	def set_StartTime(self,StartTime):
+		self.add_body_params('StartTime', StartTime)
+
+	def get_IotInstanceId(self):
+		return self.get_body_params().get('IotInstanceId')
+
+	def set_IotInstanceId(self,IotInstanceId):
+		self.add_body_params('IotInstanceId', IotInstanceId)
+
+	def get_EndTime(self):
+		return self.get_body_params().get('EndTime')
+
+	def set_EndTime(self,EndTime):
+		self.add_body_params('EndTime', EndTime)
+
+	def get_SoundCodeContent(self):
+		return self.get_body_params().get('SoundCodeContent')
+
+	def set_SoundCodeContent(self,SoundCodeContent):
+		self.add_body_params('SoundCodeContent', SoundCodeContent)
