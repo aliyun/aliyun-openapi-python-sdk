7b249679c5a634feec4bf5607f47badeb026fb15
sdk-team <sdk-team@alibabacloud.com>
2022-07-20 12:20:13 +0000

    Change getFileDetectResult.

---
+++ aliyun-python-sdk-sas/aliyunsdksas/request/v20181203/ModifyClearLogstoreStorageRequest.py
@@ -0,0 +1,53 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdksas.endpoint import endpoint_data
+
+class ModifyClearLogstoreStorageRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Sas', '2018-12-03', 'ModifyClearLogstoreStorage')
+		self.set_method('POST')
+
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+	def get_From(self): # String
+		return self.get_query_params().get('From')
+
+	def set_From(self, _From):  # String
+		self.add_query_param('From', _From)
+	def get_Lang(self): # String
+		return self.get_query_params().get('Lang')
+
+	def set_Lang(self, Lang):  # String
+		self.add_query_param('Lang', Lang)
+	def get_UserLogStore(self): # String
+		return self.get_query_params().get('UserLogStore')
+
+	def set_UserLogStore(self, UserLogStore):  # String
+		self.add_query_param('UserLogStore', UserLogStore)
+	def get_UserProject(self): # String
+		return self.get_query_params().get('UserProject')
+
+	def set_UserProject(self, UserProject):  # String
+		self.add_query_param('UserProject', UserProject)
