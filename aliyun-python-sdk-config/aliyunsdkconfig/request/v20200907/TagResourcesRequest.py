0d0e4703abb1a14fbee0caa081242e4c2dea0d49
sdk-team <sdk-team@alibabacloud.com>
2022-07-21 11:37:11 +0000

    Support PreConfigRule.

---
+++ aliyun-python-sdk-config/aliyunsdkconfig/request/v20200907/TagResourcesRequest.py
@@ -0,0 +1,49 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkconfig.endpoint import endpoint_data
+
+class TagResourcesRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Config', '2020-09-07', 'TagResources')
+		self.set_method('POST')
+
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+	def get_ResourceIds(self): # RepeatList
+		return self.get_body_params().get('ResourceId')
+
+	def set_ResourceIds(self, ResourceId):  # RepeatList
+		for depth1 in range(len(ResourceId)):
+			self.add_body_params('ResourceId.' + str(depth1 + 1), ResourceId[depth1])
+	def get_ResourceType(self): # String
+		return self.get_body_params().get('ResourceType')
+
+	def set_ResourceType(self, ResourceType):  # String
+		self.add_body_params('ResourceType', ResourceType)
+	def get_Tag(self): # String
+		return self.get_body_params().get('Tag')
+
+	def set_Tag(self, Tag):  # String
+		self.add_body_params('Tag', Tag)
