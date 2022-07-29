81ef3284af750f206e79dd79fb4f5f40b986d910
sdk-team <sdk-team@alibabacloud.com>
2022-07-29 09:37:09 +0000

    Change ListCheckInstanceResult Public.

---
+++ aliyun-python-sdk-sas/aliyunsdksas/request/v20181203/AddInstallCodeRequest.py
@@ -0,0 +1,58 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdksas.endpoint import endpoint_data
+
+class AddInstallCodeRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Sas', '2018-12-03', 'AddInstallCode')
+		self.set_method('POST')
+
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+	def get_ExpiredDate(self): # Long
+		return self.get_query_params().get('ExpiredDate')
+
+	def set_ExpiredDate(self, ExpiredDate):  # Long
+		self.add_query_param('ExpiredDate', ExpiredDate)
+	def get_Os(self): # String
+		return self.get_query_params().get('Os')
+
+	def set_Os(self, Os):  # String
+		self.add_query_param('Os', Os)
+	def get_GroupId(self): # Long
+		return self.get_query_params().get('GroupId')
+
+	def set_GroupId(self, GroupId):  # Long
+		self.add_query_param('GroupId', GroupId)
+	def get_OnlyImage(self): # Boolean
+		return self.get_query_params().get('OnlyImage')
+
+	def set_OnlyImage(self, OnlyImage):  # Boolean
+		self.add_query_param('OnlyImage', OnlyImage)
+	def get_VendorName(self): # String
+		return self.get_query_params().get('VendorName')
+
+	def set_VendorName(self, VendorName):  # String
+		self.add_query_param('VendorName', VendorName)
