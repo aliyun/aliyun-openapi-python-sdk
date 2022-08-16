9757c84f71dfd720fff13f5ef23dbd669d4d567a
sdk-team <sdk-team@alibabacloud.com>
2022-08-16 08:56:30 +0000

    Publish GetNatTopN API.

---
+++ aliyun-python-sdk-nis/aliyunsdknis/request/v20211216/GetNatTopNRequest.py
@@ -0,0 +1,57 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+
+class GetNatTopNRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'nis', '2021-12-16', 'GetNatTopN','networkana')
+		self.set_method('POST')
+
+	def get_Ip(self): # String
+		return self.get_query_params().get('Ip')
+
+	def set_Ip(self, Ip):  # String
+		self.add_query_param('Ip', Ip)
+	def get_EndTime(self): # Long
+		return self.get_query_params().get('EndTime')
+
+	def set_EndTime(self, EndTime):  # Long
+		self.add_query_param('EndTime', EndTime)
+	def get_OrderBy(self): # String
+		return self.get_query_params().get('OrderBy')
+
+	def set_OrderBy(self, OrderBy):  # String
+		self.add_query_param('OrderBy', OrderBy)
+	def get_BeginTime(self): # Long
+		return self.get_query_params().get('BeginTime')
+
+	def set_BeginTime(self, BeginTime):  # Long
+		self.add_query_param('BeginTime', BeginTime)
+	def get_TopN(self): # Integer
+		return self.get_query_params().get('TopN')
+
+	def set_TopN(self, TopN):  # Integer
+		self.add_query_param('TopN', TopN)
+	def get_NatGatewayId(self): # String
+		return self.get_query_params().get('NatGatewayId')
+
+	def set_NatGatewayId(self, NatGatewayId):  # String
+		self.add_query_param('NatGatewayId', NatGatewayId)
