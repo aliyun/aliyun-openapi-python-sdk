c7435b9c3060e9b405e3262e98c30d1120455f06
sdk-team <sdk-team@alibabacloud.com>
2022-09-29 10:01:12 +0000

    Cc5g.

---
+++ aliyun-python-sdk-cc5g/aliyunsdkcc5g/request/v20220314/ListDiagnoseInfoForSingleCardRequest.py
@@ -0,0 +1,62 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+
+class ListDiagnoseInfoForSingleCardRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'CC5G', '2022-03-14', 'ListDiagnoseInfoForSingleCard','fivegcc')
+		self.set_method('POST')
+
+	def get_Source(self): # String
+		return self.get_query_params().get('Source')
+
+	def set_Source(self, Source):  # String
+		self.add_query_param('Source', Source)
+	def get_NextToken(self): # String
+		return self.get_query_params().get('NextToken')
+
+	def set_NextToken(self, NextToken):  # String
+		self.add_query_param('NextToken', NextToken)
+	def get_SourceType(self): # String
+		return self.get_query_params().get('SourceType')
+
+	def set_SourceType(self, SourceType):  # String
+		self.add_query_param('SourceType', SourceType)
+	def get_RegionNo(self): # String
+		return self.get_query_params().get('RegionNo')
+
+	def set_RegionNo(self, RegionNo):  # String
+		self.add_query_param('RegionNo', RegionNo)
+	def get_WirelessCloudConnectorId(self): # String
+		return self.get_query_params().get('WirelessCloudConnectorId')
+
+	def set_WirelessCloudConnectorId(self, WirelessCloudConnectorId):  # String
+		self.add_query_param('WirelessCloudConnectorId', WirelessCloudConnectorId)
+	def get_MaxResults(self): # Integer
+		return self.get_query_params().get('MaxResults')
+
+	def set_MaxResults(self, MaxResults):  # Integer
+		self.add_query_param('MaxResults', MaxResults)
+	def get_Status(self): # String
+		return self.get_query_params().get('Status')
+
+	def set_Status(self, Status):  # String
+		self.add_query_param('Status', Status)
