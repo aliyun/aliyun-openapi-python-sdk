78fb6ce73f73eff07463151b488474deff7a0c00
sdk-team <sdk-team@alibabacloud.com>
2022-10-19 08:42:59 +0000

    Supported more language for resourcemanager.

---
+++ aliyun-python-sdk-resourcemanager/aliyunsdkresourcemanager/request/v20200331/DeleteAccountRequest.py
@@ -0,0 +1,45 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkresourcemanager.endpoint import endpoint_data
+import json
+
+class DeleteAccountRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'ResourceManager', '2020-03-31', 'DeleteAccount')
+		self.set_protocol_type('https')
+		self.set_method('POST')
+
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+	def get_AbandonableCheckId(self): # Array
+		return self.get_query_params().get('AbandonableCheckId')
+
+	def set_AbandonableCheckId(self, AbandonableCheckId):  # Array
+		self.add_query_param("AbandonableCheckId", json.dumps(AbandonableCheckId))
+	def get_AccountId(self): # String
+		return self.get_query_params().get('AccountId')
+
+	def set_AccountId(self, AccountId):  # String
+		self.add_query_param('AccountId', AccountId)
