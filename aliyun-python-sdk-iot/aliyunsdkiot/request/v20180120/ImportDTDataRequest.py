53be350f117d4a322dbf9a7a5843952afcbfd2ce
sdk-team <sdk-team@alibabacloud.com>
2022-10-27 10:34:21 +0000

    Iot_20180120 Old sdk.

---
+++ aliyun-python-sdk-iot/aliyunsdkiot/request/v20180120/ImportDTDataRequest.py
@@ -0,0 +1,54 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkiot.endpoint import endpoint_data
+
+class ImportDTDataRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Iot', '2018-01-20', 'ImportDTData')
+		self.set_method('POST')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+
+	def get_IotInstanceId(self):
+		return self.get_body_params().get('IotInstanceId')
+
+	def set_IotInstanceId(self,IotInstanceId):
+		self.add_body_params('IotInstanceId', IotInstanceId)
+
+	def get_ProductKey(self):
+		return self.get_body_params().get('ProductKey')
+
+	def set_ProductKey(self,ProductKey):
+		self.add_body_params('ProductKey', ProductKey)
+
+	def get_Itemss(self):
+		return self.get_body_params().get('Items')
+
+	def set_Itemss(self, Itemss):
+		for depth1 in range(len(Itemss)):
+			if Itemss[depth1].get('DeviceName') is not None:
+				self.add_body_params('Items.' + str(depth1 + 1) + '.DeviceName', Itemss[depth1].get('DeviceName'))
+			if Itemss[depth1].get('Params') is not None:
+				self.add_body_params('Items.' + str(depth1 + 1) + '.Params', Itemss[depth1].get('Params'))
