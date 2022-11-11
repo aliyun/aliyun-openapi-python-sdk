c1e7dbf53c762ca590f36a41835ee604d78cc330
sdk-team <sdk-team@alibabacloud.com>
2022-11-11 06:11:14 +0000

    Release CustomizeClassifyImage.

---
+++ aliyun-python-sdk-viapi-regen/aliyunsdkviapi_regen/request/v20211119/GetServiceInvokeRequest.py
@@ -0,0 +1,54 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkviapi_regen.endpoint import endpoint_data
+import json
+
+class GetServiceInvokeRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'viapi-regen', '2021-11-19', 'GetServiceInvoke','selflearning')
+		self.set_method('POST')
+
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+	def get_StartTime(self): # Long
+		return self.get_body_params().get('StartTime')
+
+	def set_StartTime(self, StartTime):  # Long
+		self.add_body_params('StartTime', StartTime)
+	def get_Id(self): # Long
+		return self.get_body_params().get('Id')
+
+	def set_Id(self, Id):  # Long
+		self.add_body_params('Id', Id)
+	def get_EndTime(self): # Long
+		return self.get_body_params().get('EndTime')
+
+	def set_EndTime(self, EndTime):  # Long
+		self.add_body_params('EndTime', EndTime)
+	def get_CallerParentIdList(self): # Array
+		return self.get_body_params().get('CallerParentIdList')
+
+	def set_CallerParentIdList(self, CallerParentIdList):  # Array
+		self.add_body_params("CallerParentIdList", json.dumps(CallerParentIdList))
