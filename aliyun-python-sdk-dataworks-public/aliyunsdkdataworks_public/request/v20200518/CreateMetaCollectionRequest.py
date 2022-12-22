28edfc8c9b85ba2a20ee3c6522561d4385f517c1
sdk-team <sdk-team@alibabacloud.com>
2022-12-22 02:31:44 +0000

    Generated 2020-05-18 for dataworks-public.

---
+++ aliyun-python-sdk-dataworks-public/aliyunsdkdataworks_public/request/v20200518/CreateMetaCollectionRequest.py
@@ -0,0 +1,53 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkdataworks_public.endpoint import endpoint_data
+
+class CreateMetaCollectionRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'dataworks-public', '2020-05-18', 'CreateMetaCollection')
+		self.set_method('POST')
+
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+	def get_ParentQualifiedName(self): # String
+		return self.get_query_params().get('ParentQualifiedName')
+
+	def set_ParentQualifiedName(self, ParentQualifiedName):  # String
+		self.add_query_param('ParentQualifiedName', ParentQualifiedName)
+	def get_CollectionType(self): # String
+		return self.get_query_params().get('CollectionType')
+
+	def set_CollectionType(self, CollectionType):  # String
+		self.add_query_param('CollectionType', CollectionType)
+	def get_Name(self): # String
+		return self.get_query_params().get('Name')
+
+	def set_Name(self, Name):  # String
+		self.add_query_param('Name', Name)
+	def get_Comment(self): # String
+		return self.get_query_params().get('Comment')
+
+	def set_Comment(self, Comment):  # String
+		self.add_query_param('Comment', Comment)
