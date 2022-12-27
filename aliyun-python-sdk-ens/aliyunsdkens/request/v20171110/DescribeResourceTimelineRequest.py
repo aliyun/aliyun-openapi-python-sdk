d0fd4365aa1a252f617a67586356d4dc0f2ae4c6
sdk-team <sdk-team@alibabacloud.com>
2022-12-27 05:56:06 +0000

    Support Snapshot API.

---
+++ aliyun-python-sdk-ens/aliyunsdkens/request/v20171110/DescribeResourceTimelineRequest.py
@@ -0,0 +1,42 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+
+class DescribeResourceTimelineRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Ens', '2017-11-10', 'DescribeResourceTimeline','ens')
+		self.set_method('GET')
+
+	def get_Uuid(self): # String
+		return self.get_query_params().get('Uuid')
+
+	def set_Uuid(self, Uuid):  # String
+		self.add_query_param('Uuid', Uuid)
+	def get_EndTime(self): # String
+		return self.get_query_params().get('EndTime')
+
+	def set_EndTime(self, EndTime):  # String
+		self.add_query_param('EndTime', EndTime)
+	def get_BeginTime(self): # String
+		return self.get_query_params().get('BeginTime')
+
+	def set_BeginTime(self, BeginTime):  # String
+		self.add_query_param('BeginTime', BeginTime)
--- aliyun-python-sdk-ens/aliyunsdkens/request/v20171110/ReinitInstancesRequest.py
+++ aliyun-python-sdk-ens/aliyunsdkens/request/v20171110/ReinitInstancesRequest.py
@@ -19,34 +19,25 @@
 
 from aliyunsdkcore.request import RpcRequest
 
-class ModifyInstanceAttributeRequest(RpcRequest):
+class ReinitInstancesRequest(RpcRequest):
 
 	def __init__(self):
-		RpcRequest.__init__(self, 'Ens', '2017-11-10', 'ModifyInstanceAttribute','ens')
+		RpcRequest.__init__(self, 'Ens', '2017-11-10', 'ReinitInstances','ens')
 		self.set_method('POST')
 
-	def get_UserData(self): # String
-		return self.get_query_params().get('UserData')
+	def get_ImageId(self): # String
+		return self.get_query_params().get('ImageId')
 
-	def set_UserData(self, UserData):  # String
-		self.add_query_param('UserData', UserData)
+	def set_ImageId(self, ImageId):  # String
+		self.add_query_param('ImageId', ImageId)
 	def get_Password(self): # String
 		return self.get_query_params().get('Password')
 
 	def set_Password(self, Password):  # String
 		self.add_query_param('Password', Password)
-	def get_HostName(self): # String
-		return self.get_query_params().get('HostName')
+	def get_InstanceIds(self): # Array
+		return self.get_query_params().get('InstanceIds')
 
-	def set_HostName(self, HostName):  # String
-		self.add_query_param('HostName', HostName)
-	def get_InstanceId(self): # String
-		return self.get_query_params().get('InstanceId')
-
-	def set_InstanceId(self, InstanceId):  # String
-		self.add_query_param('InstanceId', InstanceId)
-	def get_InstanceName(self): # String
-		return self.get_query_params().get('InstanceName')
-
-	def set_InstanceName(self, InstanceName):  # String
-		self.add_query_param('InstanceName', InstanceName)
+	def set_InstanceIds(self, InstanceIds):  # Array
+		for index1, value1 in enumerate(InstanceIds):
+			self.add_query_param('InstanceIds.' + str(index1 + 1), value1)
