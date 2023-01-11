9e998616b5a764dc603e8f3730d4f457e53165b2
sdk-team <sdk-team@alibabacloud.com>
2023-01-11 09:19:46 +0000

    Add OnsMessageDetail for query message body.

---
+++ aliyun-python-sdk-ons/aliyunsdkons/request/v20190214/OnsMessageDetailRequest.py
@@ -0,0 +1,48 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkons.endpoint import endpoint_data
+
+class OnsMessageDetailRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Ons', '2019-02-14', 'OnsMessageDetail','ons')
+		self.set_method('GET')
+
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+	def get_MsgId(self): # String
+		return self.get_query_params().get('MsgId')
+
+	def set_MsgId(self, MsgId):  # String
+		self.add_query_param('MsgId', MsgId)
+	def get_InstanceId(self): # String
+		return self.get_query_params().get('InstanceId')
+
+	def set_InstanceId(self, InstanceId):  # String
+		self.add_query_param('InstanceId', InstanceId)
+	def get_Topic(self): # String
+		return self.get_query_params().get('Topic')
+
+	def set_Topic(self, Topic):  # String
+		self.add_query_param('Topic', Topic)
