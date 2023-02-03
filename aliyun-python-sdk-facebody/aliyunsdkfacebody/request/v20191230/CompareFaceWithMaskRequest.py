48ec609e6325faa5fa3415f85cf887b4cf816d87
sdk-team <sdk-team@alibabacloud.com>
2023-02-03 05:50:10 +0000

    Release CompareFaceWithMask DetectInfraredLivingFace.

---
+++ aliyun-python-sdk-facebody/aliyunsdkfacebody/request/v20191230/CompareFaceWithMaskRequest.py
@@ -0,0 +1,48 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkfacebody.endpoint import endpoint_data
+
+class CompareFaceWithMaskRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'facebody', '2019-12-30', 'CompareFaceWithMask','facebody')
+		self.set_method('POST')
+
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+	def get_QualityScoreThreshold(self): # Float
+		return self.get_body_params().get('QualityScoreThreshold')
+
+	def set_QualityScoreThreshold(self, QualityScoreThreshold):  # Float
+		self.add_body_params('QualityScoreThreshold', QualityScoreThreshold)
+	def get_ImageURLB(self): # String
+		return self.get_body_params().get('ImageURLB')
+
+	def set_ImageURLB(self, ImageURLB):  # String
+		self.add_body_params('ImageURLB', ImageURLB)
+	def get_ImageURLA(self): # String
+		return self.get_body_params().get('ImageURLA')
+
+	def set_ImageURLA(self, ImageURLA):  # String
+		self.add_body_params('ImageURLA', ImageURLA)
