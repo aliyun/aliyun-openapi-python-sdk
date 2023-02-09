831cdd11c7c32d9c134655b55284478743c947c2
sdk-team <sdk-team@alibabacloud.com>
2023-02-09 02:30:56 +0000

    Release GenerateSuperResolutionImage.

---
+++ aliyun-python-sdk-imageenhan/aliyunsdkimageenhan/request/v20190930/GenerateCartoonizedImageRequest.py
@@ -0,0 +1,48 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkimageenhan.endpoint import endpoint_data
+
+class GenerateCartoonizedImageRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'imageenhan', '2019-09-30', 'GenerateCartoonizedImage','imageenhan')
+		self.set_method('POST')
+
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+	def get_ImageType(self): # String
+		return self.get_body_params().get('ImageType')
+
+	def set_ImageType(self, ImageType):  # String
+		self.add_body_params('ImageType', ImageType)
+	def get_Index(self): # String
+		return self.get_body_params().get('Index')
+
+	def set_Index(self, Index):  # String
+		self.add_body_params('Index', Index)
+	def get_ImageUrl(self): # String
+		return self.get_body_params().get('ImageUrl')
+
+	def set_ImageUrl(self, ImageUrl):  # String
+		self.add_body_params('ImageUrl', ImageUrl)
