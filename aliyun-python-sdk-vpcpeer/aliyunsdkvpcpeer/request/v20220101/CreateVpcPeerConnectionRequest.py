7bdccc77fa09022c9f28e24b75eb93e0557bca91
sdk-team <sdk-team@alibabacloud.com>
2023-02-24 02:18:01 +0000

    VpcPeer Support Tag.

---
+++ aliyun-python-sdk-vpcpeer/aliyunsdkvpcpeer/request/v20220101/CreateVpcPeerConnectionRequest.py
@@ -0,0 +1,73 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkvpcpeer.endpoint import endpoint_data
+
+class CreateVpcPeerConnectionRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'VpcPeer', '2022-01-01', 'CreateVpcPeerConnection','vpcpeer')
+		self.set_method('POST')
+
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+	def get_ClientToken(self): # String
+		return self.get_body_params().get('ClientToken')
+
+	def set_ClientToken(self, ClientToken):  # String
+		self.add_body_params('ClientToken', ClientToken)
+	def get_Description(self): # String
+		return self.get_body_params().get('Description')
+
+	def set_Description(self, Description):  # String
+		self.add_body_params('Description', Description)
+	def get_AcceptingAliUid(self): # Long
+		return self.get_body_params().get('AcceptingAliUid')
+
+	def set_AcceptingAliUid(self, AcceptingAliUid):  # Long
+		self.add_body_params('AcceptingAliUid', AcceptingAliUid)
+	def get_AcceptingRegionId(self): # String
+		return self.get_body_params().get('AcceptingRegionId')
+
+	def set_AcceptingRegionId(self, AcceptingRegionId):  # String
+		self.add_body_params('AcceptingRegionId', AcceptingRegionId)
+	def get_DryRun(self): # Boolean
+		return self.get_body_params().get('DryRun')
+
+	def set_DryRun(self, DryRun):  # Boolean
+		self.add_body_params('DryRun', DryRun)
+	def get_AcceptingVpcId(self): # String
+		return self.get_body_params().get('AcceptingVpcId')
+
+	def set_AcceptingVpcId(self, AcceptingVpcId):  # String
+		self.add_body_params('AcceptingVpcId', AcceptingVpcId)
+	def get_VpcId(self): # String
+		return self.get_body_params().get('VpcId')
+
+	def set_VpcId(self, VpcId):  # String
+		self.add_body_params('VpcId', VpcId)
+	def get_Name(self): # String
+		return self.get_body_params().get('Name')
+
+	def set_Name(self, Name):  # String
+		self.add_body_params('Name', Name)
---
+++ aliyun-python-sdk-vpcpeer/aliyunsdkvpcpeer/request/v20220101/ListVpcPeerConnectionsRequest.py
@@ -0,0 +1,68 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkvpcpeer.endpoint import endpoint_data
+
+class ListVpcPeerConnectionsRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'VpcPeer', '2022-01-01', 'ListVpcPeerConnections','vpcpeer')
+		self.set_method('POST')
+
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+	def get_NextToken(self): # String
+		return self.get_body_params().get('NextToken')
+
+	def set_NextToken(self, NextToken):  # String
+		self.add_body_params('NextToken', NextToken)
+	def get_Tagss(self): # RepeatList
+		return self.get_query_params().get('Tags')
+
+	def set_Tagss(self, Tags):  # RepeatList
+		for depth1 in range(len(Tags)):
+			if Tags[depth1].get('Key') is not None:
+				self.add_query_param('Tags.' + str(depth1 + 1) + '.Key', Tags[depth1].get('Key'))
+			if Tags[depth1].get('Value') is not None:
+				self.add_query_param('Tags.' + str(depth1 + 1) + '.Value', Tags[depth1].get('Value'))
+	def get_InstanceId(self): # String
+		return self.get_body_params().get('InstanceId')
+
+	def set_InstanceId(self, InstanceId):  # String
+		self.add_body_params('InstanceId', InstanceId)
+	def get_VpcId(self): # Array
+		return self.get_body_params().get('VpcId')
+
+	def set_VpcId(self, VpcId):  # Array
+		for index1, value1 in enumerate(VpcId):
+			self.add_body_params('VpcId.' + str(index1 + 1), value1)
+	def get_Name(self): # String
+		return self.get_body_params().get('Name')
+
+	def set_Name(self, Name):  # String
+		self.add_body_params('Name', Name)
+	def get_MaxResults(self): # Integer
+		return self.get_body_params().get('MaxResults')
+
+	def set_MaxResults(self, MaxResults):  # Integer
+		self.add_body_params('MaxResults', MaxResults)
