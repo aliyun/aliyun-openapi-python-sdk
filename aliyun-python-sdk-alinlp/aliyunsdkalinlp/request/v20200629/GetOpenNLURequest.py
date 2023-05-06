8643008a644469bef65b07063c93a7f1d0c1ad20
sdk-team <sdk-team@alibabacloud.com>
2023-05-06 07:40:20 +0000

    Supported more api for user.

---
+++ aliyun-python-sdk-alinlp/aliyunsdkalinlp/request/v20200629/GetOpenNLURequest.py
@@ -0,0 +1,58 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkalinlp.endpoint import endpoint_data
+
+class GetOpenNLURequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'alinlp', '2020-06-29', 'GetOpenNLU','alinlp')
+		self.set_method('POST')
+
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+	def get_Sentence(self): # String
+		return self.get_body_params().get('Sentence')
+
+	def set_Sentence(self, Sentence):  # String
+		self.add_body_params('Sentence', Sentence)
+	def get_Labels(self): # String
+		return self.get_body_params().get('Labels')
+
+	def set_Labels(self, Labels):  # String
+		self.add_body_params('Labels', Labels)
+	def get_Task(self): # String
+		return self.get_body_params().get('Task')
+
+	def set_Task(self, Task):  # String
+		self.add_body_params('Task', Task)
+	def get_ServiceCode(self): # String
+		return self.get_body_params().get('ServiceCode')
+
+	def set_ServiceCode(self, ServiceCode):  # String
+		self.add_body_params('ServiceCode', ServiceCode)
+	def get_Examples(self): # String
+		return self.get_body_params().get('Examples')
+
+	def set_Examples(self, Examples):  # String
+		self.add_body_params('Examples', Examples)
