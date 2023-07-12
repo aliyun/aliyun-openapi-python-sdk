8d64d4fd046954f0b309276d4e23dd1223660ffb
sdk-team <sdk-team@alibabacloud.com>
2023-07-12 06:31:53 +0000

    Iot_20180120 TeaDSL SDK.

---
+++ aliyun-python-sdk-iot/aliyunsdkiot/request/v20180120/AddDeviceToSharePromotionRequest.py
@@ -0,0 +1,60 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkiot.endpoint import endpoint_data
+
+class AddDeviceToSharePromotionRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Iot', '2018-01-20', 'AddDeviceToSharePromotion')
+		self.set_method('POST')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+
+	def get_SharePromotionActivityId(self):
+		return self.get_body_params().get('SharePromotionActivityId')
+
+	def set_SharePromotionActivityId(self,SharePromotionActivityId):
+		self.add_body_params('SharePromotionActivityId', SharePromotionActivityId)
+
+	def get_DeviceSimpleInfoLists(self):
+		return self.get_body_params().get('DeviceSimpleInfoList')
+
+	def set_DeviceSimpleInfoLists(self, DeviceSimpleInfoLists):
+		for depth1 in range(len(DeviceSimpleInfoLists)):
+			if DeviceSimpleInfoLists[depth1].get('DeviceName') is not None:
+				self.add_body_params('DeviceSimpleInfoList.' + str(depth1 + 1) + '.DeviceName', DeviceSimpleInfoLists[depth1].get('DeviceName'))
+			if DeviceSimpleInfoLists[depth1].get('ProductKey') is not None:
+				self.add_body_params('DeviceSimpleInfoList.' + str(depth1 + 1) + '.ProductKey', DeviceSimpleInfoLists[depth1].get('ProductKey'))
+
+	def get_IotInstanceId(self):
+		return self.get_body_params().get('IotInstanceId')
+
+	def set_IotInstanceId(self,IotInstanceId):
+		self.add_body_params('IotInstanceId', IotInstanceId)
+
+	def get_ShareTaskCode(self):
+		return self.get_body_params().get('ShareTaskCode')
+
+	def set_ShareTaskCode(self,ShareTaskCode):
+		self.add_body_params('ShareTaskCode', ShareTaskCode)
---
+++ aliyun-python-sdk-iot/aliyunsdkiot/request/v20180120/ModifyOTAFirmwareRequest.py
@@ -0,0 +1,68 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkiot.endpoint import endpoint_data
+
+class ModifyOTAFirmwareRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Iot', '2018-01-20', 'ModifyOTAFirmware')
+		self.set_method('POST')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+
+	def get_FirmwareUdi(self):
+		return self.get_query_params().get('FirmwareUdi')
+
+	def set_FirmwareUdi(self,FirmwareUdi):
+		self.add_query_param('FirmwareUdi',FirmwareUdi)
+
+	def get_FirmwareDesc(self):
+		return self.get_query_params().get('FirmwareDesc')
+
+	def set_FirmwareDesc(self,FirmwareDesc):
+		self.add_query_param('FirmwareDesc',FirmwareDesc)
+
+	def get_IotInstanceId(self):
+		return self.get_query_params().get('IotInstanceId')
+
+	def set_IotInstanceId(self,IotInstanceId):
+		self.add_query_param('IotInstanceId',IotInstanceId)
+
+	def get_FirmwareName(self):
+		return self.get_query_params().get('FirmwareName')
+
+	def set_FirmwareName(self,FirmwareName):
+		self.add_query_param('FirmwareName',FirmwareName)
+
+	def get_FirmwareId(self):
+		return self.get_query_params().get('FirmwareId')
+
+	def set_FirmwareId(self,FirmwareId):
+		self.add_query_param('FirmwareId',FirmwareId)
+
+	def get_ProductKey(self):
+		return self.get_query_params().get('ProductKey')
+
+	def set_ProductKey(self,ProductKey):
+		self.add_query_param('ProductKey',ProductKey)
