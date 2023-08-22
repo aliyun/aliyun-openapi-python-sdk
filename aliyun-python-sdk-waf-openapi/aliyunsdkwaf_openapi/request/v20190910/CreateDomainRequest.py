fe677eae9fd40a96f4c705990f1addcb53837269
sdk-team <sdk-team@alibabacloud.com>
2023-08-22 06:19:37 +0000

    Upgrade SDK.

--- aliyun-python-sdk-waf-openapi/aliyunsdkwaf_openapi/request/v20190910/CreateDomainRequest.py
+++ aliyun-python-sdk-waf-openapi/aliyunsdkwaf_openapi/request/v20190910/CreateDomainRequest.py
@@ -36,6 +36,11 @@ class CreateDomainRequest(RpcRequest):
 
 	def set_IpFollowStatus(self, IpFollowStatus):  # Integer
 		self.add_query_param('IpFollowStatus', IpFollowStatus)
+	def get_Keepalive(self): # Boolean
+		return self.get_query_params().get('Keepalive')
+
+	def set_Keepalive(self, Keepalive):  # Boolean
+		self.add_query_param('Keepalive', Keepalive)
 	def get_SniHost(self): # String
 		return self.get_query_params().get('SniHost')
 
@@ -61,46 +66,26 @@ class CreateDomainRequest(RpcRequest):
 
 	def set_WriteTime(self, WriteTime):  # Integer
 		self.add_query_param('WriteTime', WriteTime)
-	def get_SniStatus(self): # Integer
-		return self.get_query_params().get('SniStatus')
-
-	def set_SniStatus(self, SniStatus):  # Integer
-		self.add_query_param('SniStatus', SniStatus)
 	def get_AccessHeaderMode(self): # Integer
 		return self.get_query_params().get('AccessHeaderMode')
 
 	def set_AccessHeaderMode(self, AccessHeaderMode):  # Integer
 		self.add_query_param('AccessHeaderMode', AccessHeaderMode)
-	def get_AccessType(self): # String
-		return self.get_query_params().get('AccessType')
-
-	def set_AccessType(self, AccessType):  # String
-		self.add_query_param('AccessType', AccessType)
-	def get_LogHeaders(self): # String
-		return self.get_query_params().get('LogHeaders')
-
-	def set_LogHeaders(self, LogHeaders):  # String
-		self.add_query_param('LogHeaders', LogHeaders)
 	def get_AccessHeaders(self): # String
 		return self.get_query_params().get('AccessHeaders')
 
 	def set_AccessHeaders(self, AccessHeaders):  # String
 		self.add_query_param('AccessHeaders', AccessHeaders)
-	def get_ConnectionTime(self): # Integer
-		return self.get_query_params().get('ConnectionTime')
+	def get_KeepaliveTimeout(self): # Integer
+		return self.get_query_params().get('KeepaliveTimeout')
 
-	def set_ConnectionTime(self, ConnectionTime):  # Integer
-		self.add_query_param('ConnectionTime', ConnectionTime)
+	def set_KeepaliveTimeout(self, KeepaliveTimeout):  # Integer
+		self.add_query_param('KeepaliveTimeout', KeepaliveTimeout)
 	def get_ClusterType(self): # Integer
 		return self.get_query_params().get('ClusterType')
 
 	def set_ClusterType(self, ClusterType):  # Integer
 		self.add_query_param('ClusterType', ClusterType)
-	def get_CloudNativeInstances(self): # String
-		return self.get_query_params().get('CloudNativeInstances')
-
-	def set_CloudNativeInstances(self, CloudNativeInstances):  # String
-		self.add_query_param('CloudNativeInstances', CloudNativeInstances)
 	def get_HttpsRedirect(self): # Integer
 		return self.get_query_params().get('HttpsRedirect')
 
@@ -111,21 +96,11 @@ class CreateDomainRequest(RpcRequest):
 
 	def set_InstanceId(self, InstanceId):  # String
 		self.add_query_param('InstanceId', InstanceId)
-	def get_SourceIps(self): # String
-		return self.get_query_params().get('SourceIps')
-
-	def set_SourceIps(self, SourceIps):  # String
-		self.add_query_param('SourceIps', SourceIps)
 	def get_Domain(self): # String
 		return self.get_query_params().get('Domain')
 
 	def set_Domain(self, Domain):  # String
 		self.add_query_param('Domain', Domain)
-	def get_IsAccessProduct(self): # Integer
-		return self.get_query_params().get('IsAccessProduct')
-
-	def set_IsAccessProduct(self, IsAccessProduct):  # Integer
-		self.add_query_param('IsAccessProduct', IsAccessProduct)
 	def get_ReadTime(self): # Integer
 		return self.get_query_params().get('ReadTime')
 
@@ -136,6 +111,51 @@ class CreateDomainRequest(RpcRequest):
 
 	def set_HttpsPort(self, HttpsPort):  # String
 		self.add_query_param('HttpsPort', HttpsPort)
+	def get_SniStatus(self): # Integer
+		return self.get_query_params().get('SniStatus')
+
+	def set_SniStatus(self, SniStatus):  # Integer
+		self.add_query_param('SniStatus', SniStatus)
+	def get_Retry(self): # Boolean
+		return self.get_query_params().get('Retry')
+
+	def set_Retry(self, Retry):  # Boolean
+		self.add_query_param('Retry', Retry)
+	def get_KeepaliveRequests(self): # Integer
+		return self.get_query_params().get('KeepaliveRequests')
+
+	def set_KeepaliveRequests(self, KeepaliveRequests):  # Integer
+		self.add_query_param('KeepaliveRequests', KeepaliveRequests)
+	def get_AccessType(self): # String
+		return self.get_query_params().get('AccessType')
+
+	def set_AccessType(self, AccessType):  # String
+		self.add_query_param('AccessType', AccessType)
+	def get_LogHeaders(self): # String
+		return self.get_query_params().get('LogHeaders')
+
+	def set_LogHeaders(self, LogHeaders):  # String
+		self.add_query_param('LogHeaders', LogHeaders)
+	def get_ConnectionTime(self): # Integer
+		return self.get_query_params().get('ConnectionTime')
+
+	def set_ConnectionTime(self, ConnectionTime):  # Integer
+		self.add_query_param('ConnectionTime', ConnectionTime)
+	def get_CloudNativeInstances(self): # String
+		return self.get_query_params().get('CloudNativeInstances')
+
+	def set_CloudNativeInstances(self, CloudNativeInstances):  # String
+		self.add_query_param('CloudNativeInstances', CloudNativeInstances)
+	def get_SourceIps(self): # String
+		return self.get_query_params().get('SourceIps')
+
+	def set_SourceIps(self, SourceIps):  # String
+		self.add_query_param('SourceIps', SourceIps)
+	def get_IsAccessProduct(self): # Integer
+		return self.get_query_params().get('IsAccessProduct')
+
+	def set_IsAccessProduct(self, IsAccessProduct):  # Integer
+		self.add_query_param('IsAccessProduct', IsAccessProduct)
 	def get_LoadBalancing(self): # Integer
 		return self.get_query_params().get('LoadBalancing')
 
