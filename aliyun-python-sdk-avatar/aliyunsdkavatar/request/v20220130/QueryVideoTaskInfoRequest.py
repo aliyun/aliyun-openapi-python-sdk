9462bb4691d96635c16fc6fdca9bae6f47750b14
sdk-team <sdk-team@alibabacloud.com>
2023-08-28 08:34:53 +0000

    add make stage, preview url

---
+++ aliyun-python-sdk-avatar/aliyunsdkavatar/request/v20220130/QueryVideoTaskInfoRequest.py
@@ -0,0 +1,73 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+import json
+
+class QueryVideoTaskInfoRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'avatar', '2022-01-30', 'QueryVideoTaskInfo')
+		self.set_method('POST')
+
+	def get_App(self): # Struct
+		return self.get_query_params().get('App')
+
+	def set_App(self, App):  # Struct
+		self.add_query_param("App", json.dumps(App))
+	def get_Title(self): # String
+		return self.get_query_params().get('Title')
+
+	def set_Title(self, Title):  # String
+		self.add_query_param('Title', Title)
+	def get_Type(self): # Integer
+		return self.get_query_params().get('Type')
+
+	def set_Type(self, Type):  # Integer
+		self.add_query_param('Type', Type)
+	def get_OrderById(self): # String
+		return self.get_query_params().get('OrderById')
+
+	def set_OrderById(self, OrderById):  # String
+		self.add_query_param('OrderById', OrderById)
+	def get_PageNo(self): # Integer
+		return self.get_query_params().get('PageNo')
+
+	def set_PageNo(self, PageNo):  # Integer
+		self.add_query_param('PageNo', PageNo)
+	def get_TenantId(self): # Long
+		return self.get_query_params().get('TenantId')
+
+	def set_TenantId(self, TenantId):  # Long
+		self.add_query_param('TenantId', TenantId)
+	def get_PageSize(self): # Integer
+		return self.get_query_params().get('PageSize')
+
+	def set_PageSize(self, PageSize):  # Integer
+		self.add_query_param('PageSize', PageSize)
+	def get_TaskUuid(self): # String
+		return self.get_query_params().get('TaskUuid')
+
+	def set_TaskUuid(self, TaskUuid):  # String
+		self.add_query_param('TaskUuid', TaskUuid)
+	def get_Status(self): # Integer
+		return self.get_query_params().get('Status')
+
+	def set_Status(self, Status):  # Integer
+		self.add_query_param('Status', Status)
