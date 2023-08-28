65fc455a5fdbe8a92f971834f0d41237efbfb584
sdk-team <sdk-team@alibabacloud.com>
2023-08-28 02:27:37 +0000

    Support elastic network interface.

---
+++ aliyun-python-sdk-eflo/aliyunsdkeflo/request/v20220530/CreateElasticNetworkInterfaceRequest.py
@@ -0,0 +1,52 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+
+class CreateElasticNetworkInterfaceRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'eflo', '2022-05-30', 'CreateElasticNetworkInterface','eflo')
+		self.set_method('POST')
+
+	def get_Description(self): # String
+		return self.get_body_params().get('Description')
+
+	def set_Description(self, Description):  # String
+		self.add_body_params('Description', Description)
+	def get_VSwitchId(self): # String
+		return self.get_body_params().get('VSwitchId')
+
+	def set_VSwitchId(self, VSwitchId):  # String
+		self.add_body_params('VSwitchId', VSwitchId)
+	def get_VpcId(self): # String
+		return self.get_body_params().get('VpcId')
+
+	def set_VpcId(self, VpcId):  # String
+		self.add_body_params('VpcId', VpcId)
+	def get_ZoneId(self): # String
+		return self.get_body_params().get('ZoneId')
+
+	def set_ZoneId(self, ZoneId):  # String
+		self.add_body_params('ZoneId', ZoneId)
+	def get_NodeId(self): # String
+		return self.get_body_params().get('NodeId')
+
+	def set_NodeId(self, NodeId):  # String
+		self.add_body_params('NodeId', NodeId)
