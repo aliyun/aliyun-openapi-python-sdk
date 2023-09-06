894ebbbf3821805735e2fb729ad8cea4514fb6e8
Jackson Tian <shyvo1987@gmail.com>
2023-09-07 00:59:11 +0800

    lint: improve coding style of shell scripts

--- run_all_test.sh
+++ run_all_test.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 
-export PYTHONPATH=$PYTHONPATH:`ls | grep aliyun-python-sdk- | xargs | sed 's/ /:/g'`
-for i in `ls | grep aliyun-python-sdk-`; do
-    echo "Adding $i to PYTHONPATH ..."
-    export PYTHONPATH=$PYTHONPATH:`pwd`/$i > /dev/null
+export PYTHONPATH="$PYTHONPATH":$(ls | grep aliyun-python-sdk- | xargs | sed 's/ /:/g')
+for i in $(ls | grep aliyun-python-sdk-); do
+    # Add to PYTHONPATH
+    export PYTHONPATH="$PYTHONPATH":$(pwd)/"$i" > /dev/null
 done
 
 coverage run -a --source="./aliyun-python-sdk-core/aliyunsdkcore" --branch -m pytest python-sdk-functional-test
