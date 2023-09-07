49f25e54f86802365e8f54080474528b3734bf78
Jackson Tian <shyvo1987@gmail.com>
2023-09-07 12:16:10 +0800

    cleanup: remove core-v3 because core support python2/3 both

--- aliyun-python-sdk-core-v3/tests/acs_exception/test_error_msg.py
+++
@@ -1,9 +0,0 @@
-from tests import unittest
-
-from aliyunsdkcore.acs_exception.error_msg import get_msg
-
-
-class TestErrorMessage(unittest.TestCase):
-
-    def test_get_msg(self):
-        self.assertEqual(get_msg("SDK_INVALID_REGION_ID"), "Can not find endpoint to access.")
