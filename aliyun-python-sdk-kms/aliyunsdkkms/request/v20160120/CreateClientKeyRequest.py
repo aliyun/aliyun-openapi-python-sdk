9f55247bd4e6a944e3b8f0da7ee08b1fcdd0952a
sdk-team <sdk-team@alibabacloud.com>
2023-09-11 09:03:11 +0000

    add Instance, NetworkRule, Policy, ApplicationAccessPoint, ClientKey OpenAPIs

---
+++ aliyun-python-sdk-kms/aliyunsdkkms/request/v20160120/CreateClientKeyRequest.py
@@ -0,0 +1,54 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkkms.endpoint import endpoint_data
+
+class CreateClientKeyRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Kms', '2016-01-20', 'CreateClientKey','kms')
+		self.set_protocol_type('https')
+		self.set_method('POST')
+
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+	def get_NotBefore(self): # String
+		return self.get_query_params().get('NotBefore')
+
+	def set_NotBefore(self, NotBefore):  # String
+		self.add_query_param('NotBefore', NotBefore)
+	def get_NotAfter(self): # String
+		return self.get_query_params().get('NotAfter')
+
+	def set_NotAfter(self, NotAfter):  # String
+		self.add_query_param('NotAfter', NotAfter)
+	def get_Password(self): # String
+		return self.get_query_params().get('Password')
+
+	def set_Password(self, Password):  # String
+		self.add_query_param('Password', Password)
+	def get_AapName(self): # String
+		return self.get_query_params().get('AapName')
+
+	def set_AapName(self, AapName):  # String
+		self.add_query_param('AapName', AapName)
