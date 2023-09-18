7a85f75211061e134e5fcfcc11203ec5ac9ade85
sdk-team <sdk-team@alibabacloud.com>
2023-09-18 08:55:03 +0000

    Update by SDK platform.

--- aliyun-python-sdk-dds/aliyunsdkdds/request/v20151201/CreateGlobalSecurityIPGroupRequest.py
+++ aliyun-python-sdk-dds/aliyunsdkdds/request/v20151201/CreateGlobalSecurityIPGroupRequest.py
@@ -18,12 +18,18 @@
 # under the License.
 
 from aliyunsdkcore.request import RpcRequest
+from aliyunsdkdds.endpoint import endpoint_data
 
 class CreateGlobalSecurityIPGroupRequest(RpcRequest):
 
 	def __init__(self):
 		RpcRequest.__init__(self, 'Dds', '2015-12-01', 'CreateGlobalSecurityIPGroup','dds')
 		self.set_method('POST')
+
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
 
 	def get_ResourceOwnerId(self): # Long
 		return self.get_query_params().get('ResourceOwnerId')
