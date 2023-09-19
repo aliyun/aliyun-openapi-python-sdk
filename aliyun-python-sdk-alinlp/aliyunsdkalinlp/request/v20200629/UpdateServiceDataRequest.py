e67d8bd06e89c9bf3c6fe53ed2aa3ffcd5d55d3c
sdk-team <sdk-team@alibabacloud.com>
2023-09-19 07:41:54 +0000

    Generated 2020-06-29 for alinlp.

---
+++ aliyun-python-sdk-alinlp/aliyunsdkalinlp/request/v20200629/UpdateServiceDataRequest.py
@@ -0,0 +1,44 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkalinlp.endpoint import endpoint_data
+import json
+
+class UpdateServiceDataRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'alinlp', '2020-06-29', 'UpdateServiceData','alinlp')
+		self.set_method('POST')
+
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+	def get_ServiceId(self): # Long
+		return self.get_body_params().get('ServiceId')
+
+	def set_ServiceId(self, ServiceId):  # Long
+		self.add_body_params('ServiceId', ServiceId)
+	def get_Conditions(self): # Map
+		return self.get_body_params().get('Conditions')
+
+	def set_Conditions(self, Conditions):  # Map
+		self.add_body_params("Conditions", json.dumps(Conditions))
