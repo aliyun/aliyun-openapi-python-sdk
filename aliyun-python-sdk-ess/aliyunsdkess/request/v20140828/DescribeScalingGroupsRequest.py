505eefdf453abbf85e1ddd9ba55ecc38ae39bab8
sdk-team <sdk-team@alibabacloud.com>
2023-09-25 08:15:01 +0000

    DescribeScalingGroups support Tag filter.

--- aliyun-python-sdk-ess/aliyunsdkess/request/v20140828/DescribeScalingGroupsRequest.py
+++ aliyun-python-sdk-ess/aliyunsdkess/request/v20140828/DescribeScalingGroupsRequest.py
@@ -46,6 +46,15 @@ class DescribeScalingGroupsRequest(RpcRequest):
 
 	def set_GroupType(self, GroupType):  # String
 		self.add_query_param('GroupType', GroupType)
+	def get_Tags(self): # RepeatList
+		return self.get_query_params().get('Tag')
+
+	def set_Tags(self, Tag):  # RepeatList
+		for depth1 in range(len(Tag)):
+			if Tag[depth1].get('Value') is not None:
+				self.add_query_param('Tag.' + str(depth1 + 1) + '.Value', Tag[depth1].get('Value'))
+			if Tag[depth1].get('Key') is not None:
+				self.add_query_param('Tag.' + str(depth1 + 1) + '.Key', Tag[depth1].get('Key'))
 	def get_OwnerId(self): # Long
 		return self.get_query_params().get('OwnerId')
 
