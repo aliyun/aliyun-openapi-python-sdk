02f86dda84791d6fbc8fb7737d583e75e4666bee
sdk-team <sdk-team@alibabacloud.com>
2023-10-12 06:31:19 +0000

    Generated 2019-09-01 for OceanBasePro.

---
+++ aliyun-python-sdk-oceanbasepro/aliyunsdkoceanbasepro/request/v20190901/DescribeMetricsDataRequest.py
@@ -0,0 +1,78 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkoceanbasepro.endpoint import endpoint_data
+
+class DescribeMetricsDataRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'OceanBasePro', '2019-09-01', 'DescribeMetricsData','oceanbase')
+		self.set_method('POST')
+
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+	def get_GroupByLabels(self): # String
+		return self.get_query_params().get('GroupByLabels')
+
+	def set_GroupByLabels(self, GroupByLabels):  # String
+		self.add_query_param('GroupByLabels', GroupByLabels)
+	def get_StartTime(self): # String
+		return self.get_query_params().get('StartTime')
+
+	def set_StartTime(self, StartTime):  # String
+		self.add_query_param('StartTime', StartTime)
+	def get_Limit(self): # String
+		return self.get_query_params().get('Limit')
+
+	def set_Limit(self, Limit):  # String
+		self.add_query_param('Limit', Limit)
+	def get_SortOrder(self): # String
+		return self.get_query_params().get('SortOrder')
+
+	def set_SortOrder(self, SortOrder):  # String
+		self.add_query_param('SortOrder', SortOrder)
+	def get_SortMetricKey(self): # String
+		return self.get_query_params().get('SortMetricKey')
+
+	def set_SortMetricKey(self, SortMetricKey):  # String
+		self.add_query_param('SortMetricKey', SortMetricKey)
+	def get_EndTime(self): # String
+		return self.get_query_params().get('EndTime')
+
+	def set_EndTime(self, EndTime):  # String
+		self.add_query_param('EndTime', EndTime)
+	def get_Labels(self): # String
+		return self.get_query_params().get('Labels')
+
+	def set_Labels(self, Labels):  # String
+		self.add_query_param('Labels', Labels)
+	def get_InstanceId(self): # String
+		return self.get_query_params().get('InstanceId')
+
+	def set_InstanceId(self, InstanceId):  # String
+		self.add_query_param('InstanceId', InstanceId)
+	def get_Metrics(self): # String
+		return self.get_query_params().get('Metrics')
+
+	def set_Metrics(self, Metrics):  # String
+		self.add_query_param('Metrics', Metrics)
