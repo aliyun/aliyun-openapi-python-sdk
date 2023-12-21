0d00a43130ddc6136054b3b48fb7c47cc29e2505
sdk-team <sdk-team@alibabacloud.com>
2023-12-21 11:53:15 +0000

    Add api about ABMetric, ABMetricGroup, CalculationJob.

---
+++ aliyun-python-sdk-pairecservice/aliyunsdkpairecservice/request/v20221213/ListInstanceResourcesRequest.py
@@ -0,0 +1,48 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RoaRequest
+
+class ListInstanceResourcesRequest(RoaRequest):
+
+	def __init__(self):
+		RoaRequest.__init__(self, 'PaiRecService', '2022-12-13', 'ListInstanceResources')
+		self.set_uri_pattern('/api/v1/instances/[InstanceId]/resources')
+		self.set_method('GET')
+
+	def get_InstanceId(self): # String
+		return self.get_path_params().get('InstanceId')
+
+	def set_InstanceId(self, InstanceId):  # String
+		self.add_path_param('InstanceId', InstanceId)
+	def get_Category(self): # String
+		return self.get_query_params().get('Category')
+
+	def set_Category(self, Category):  # String
+		self.add_query_param('Category', Category)
+	def get_Type(self): # String
+		return self.get_query_params().get('Type')
+
+	def set_Type(self, Type):  # String
+		self.add_query_param('Type', Type)
+	def get_Group(self): # String
+		return self.get_query_params().get('Group')
+
+	def set_Group(self, Group):  # String
+		self.add_query_param('Group', Group)
