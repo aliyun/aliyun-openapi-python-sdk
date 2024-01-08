871572d61cf87bb8006f16df35dee9a3870a43ae
sdk-team <sdk-team@alibabacloud.com>
2024-01-08 08:21:09 +0000

    Support share resource to service for RS.

---
+++ aliyun-python-sdk-resourcesharing/aliyunsdkresourcesharing/request/v20200110/CreateResourceShareRequest.py
@@ -0,0 +1,65 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkresourcesharing.endpoint import endpoint_data
+
+class CreateResourceShareRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'ResourceSharing', '2020-01-10', 'CreateResourceShare','ressharing')
+		self.set_protocol_type('https')
+		self.set_method('POST')
+
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+	def get_ResourceShareName(self): # String
+		return self.get_query_params().get('ResourceShareName')
+
+	def set_ResourceShareName(self, ResourceShareName):  # String
+		self.add_query_param('ResourceShareName', ResourceShareName)
+	def get_Targetss(self): # RepeatList
+		return self.get_query_params().get('Targets')
+
+	def set_Targetss(self, Targets):  # RepeatList
+		for depth1 in range(len(Targets)):
+			self.add_query_param('Targets.' + str(depth1 + 1), Targets[depth1])
+	def get_Resourcess(self): # RepeatList
+		return self.get_query_params().get('Resources')
+
+	def set_Resourcess(self, Resources):  # RepeatList
+		for depth1 in range(len(Resources)):
+			if Resources[depth1].get('ResourceId') is not None:
+				self.add_query_param('Resources.' + str(depth1 + 1) + '.ResourceId', Resources[depth1].get('ResourceId'))
+			if Resources[depth1].get('ResourceType') is not None:
+				self.add_query_param('Resources.' + str(depth1 + 1) + '.ResourceType', Resources[depth1].get('ResourceType'))
+	def get_AllowExternalTargets(self): # Boolean
+		return self.get_query_params().get('AllowExternalTargets')
+
+	def set_AllowExternalTargets(self, AllowExternalTargets):  # Boolean
+		self.add_query_param('AllowExternalTargets', AllowExternalTargets)
+	def get_PermissionNamess(self): # RepeatList
+		return self.get_query_params().get('PermissionNames')
+
+	def set_PermissionNamess(self, PermissionNames):  # RepeatList
+		for depth1 in range(len(PermissionNames)):
+			self.add_query_param('PermissionNames.' + str(depth1 + 1), PermissionNames[depth1])
---
+++ aliyun-python-sdk-resourcesharing/aliyunsdkresourcesharing/request/v20200110/ListSharedResourcesRequest.py
@@ -0,0 +1,71 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkresourcesharing.endpoint import endpoint_data
+
+class ListSharedResourcesRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'ResourceSharing', '2020-01-10', 'ListSharedResources','ressharing')
+		self.set_protocol_type('https')
+		self.set_method('POST')
+
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+	def get_NextToken(self): # String
+		return self.get_query_params().get('NextToken')
+
+	def set_NextToken(self, NextToken):  # String
+		self.add_query_param('NextToken', NextToken)
+	def get_ResourceShareIdss(self): # RepeatList
+		return self.get_query_params().get('ResourceShareIds')
+
+	def set_ResourceShareIdss(self, ResourceShareIds):  # RepeatList
+		for depth1 in range(len(ResourceShareIds)):
+			self.add_query_param('ResourceShareIds.' + str(depth1 + 1), ResourceShareIds[depth1])
+	def get_ResourceOwner(self): # String
+		return self.get_query_params().get('ResourceOwner')
+
+	def set_ResourceOwner(self, ResourceOwner):  # String
+		self.add_query_param('ResourceOwner', ResourceOwner)
+	def get_ResourceType(self): # String
+		return self.get_query_params().get('ResourceType')
+
+	def set_ResourceType(self, ResourceType):  # String
+		self.add_query_param('ResourceType', ResourceType)
+	def get_Target(self): # String
+		return self.get_query_params().get('Target')
+
+	def set_Target(self, Target):  # String
+		self.add_query_param('Target', Target)
+	def get_MaxResults(self): # Integer
+		return self.get_query_params().get('MaxResults')
+
+	def set_MaxResults(self, MaxResults):  # Integer
+		self.add_query_param('MaxResults', MaxResults)
+	def get_ResourceIdss(self): # RepeatList
+		return self.get_query_params().get('ResourceIds')
+
+	def set_ResourceIdss(self, ResourceIds):  # RepeatList
+		for depth1 in range(len(ResourceIds)):
+			self.add_query_param('ResourceIds.' + str(depth1 + 1), ResourceIds[depth1])
