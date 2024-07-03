3cf7bc2b59034515cfaa9e970690fc23172792e0
sdk-team <sdk-team@alibabacloud.com>
2024-07-03 02:53:16 +0000

    Generated 2023-01-20 for csas.

---
+++ aliyun-python-sdk-csas/aliyunsdkcsas/request/v20230120/ListNacUserCertRequest.py
@@ -0,0 +1,68 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+
+class ListNacUserCertRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'csas', '2023-01-20', 'ListNacUserCert')
+		self.set_protocol_type('https')
+		self.set_method('POST')
+
+	def get_EndTime(self): # Long
+		return self.get_query_params().get('EndTime')
+
+	def set_EndTime(self, EndTime):  # Long
+		self.add_query_param('EndTime', EndTime)
+	def get_CurrentPage(self): # String
+		return self.get_query_params().get('CurrentPage')
+
+	def set_CurrentPage(self, CurrentPage):  # String
+		self.add_query_param('CurrentPage', CurrentPage)
+	def get_StartTime(self): # Long
+		return self.get_query_params().get('StartTime')
+
+	def set_StartTime(self, StartTime):  # Long
+		self.add_query_param('StartTime', StartTime)
+	def get_DeviceType(self): # String
+		return self.get_query_params().get('DeviceType')
+
+	def set_DeviceType(self, DeviceType):  # String
+		self.add_query_param('DeviceType', DeviceType)
+	def get_PageSize(self): # String
+		return self.get_query_params().get('PageSize')
+
+	def set_PageSize(self, PageSize):  # String
+		self.add_query_param('PageSize', PageSize)
+	def get_Department(self): # String
+		return self.get_query_params().get('Department')
+
+	def set_Department(self, Department):  # String
+		self.add_query_param('Department', Department)
+	def get_Username(self): # String
+		return self.get_query_params().get('Username')
+
+	def set_Username(self, Username):  # String
+		self.add_query_param('Username', Username)
+	def get_Status(self): # String
+		return self.get_query_params().get('Status')
+
+	def set_Status(self, Status):  # String
+		self.add_query_param('Status', Status)
