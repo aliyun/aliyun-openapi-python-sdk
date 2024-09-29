6df1951bfe5b9431c941d32c7031c827755bf5f6
sdk-team <sdk-team@alibabacloud.com>
2024-09-29 14:43:29 +0000

    Generated 2021-12-21 for wss.

---
+++ aliyun-python-sdk-wss/aliyunsdkwss/request/v20211221/DescribePackageDeductionsRequest.py
@@ -0,0 +1,54 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+
+class DescribePackageDeductionsRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'wss', '2021-12-21', 'DescribePackageDeductions')
+		self.set_method('POST')
+
+	def get_PageNum(self): # Integer
+		return self.get_query_params().get('PageNum')
+
+	def set_PageNum(self, PageNum):  # Integer
+		self.add_query_param('PageNum', PageNum)
+	def get_ResourceType(self): # String
+		return self.get_query_params().get('ResourceType')
+
+	def set_ResourceType(self, ResourceType):  # String
+		self.add_query_param('ResourceType', ResourceType)
+	def get_PackageIdss(self): # RepeatList
+		return self.get_query_params().get('PackageIds')
+
+	def set_PackageIdss(self, PackageIds):  # RepeatList
+		for depth1 in range(len(PackageIds)):
+			self.add_query_param('PackageIds.' + str(depth1 + 1), PackageIds[depth1])
+	def get_InstanceIdss(self): # RepeatList
+		return self.get_query_params().get('InstanceIds')
+
+	def set_InstanceIdss(self, InstanceIds):  # RepeatList
+		for depth1 in range(len(InstanceIds)):
+			self.add_query_param('InstanceIds.' + str(depth1 + 1), InstanceIds[depth1])
+	def get_PageSize(self): # Integer
+		return self.get_query_params().get('PageSize')
+
+	def set_PageSize(self, PageSize):  # Integer
+		self.add_query_param('PageSize', PageSize)
