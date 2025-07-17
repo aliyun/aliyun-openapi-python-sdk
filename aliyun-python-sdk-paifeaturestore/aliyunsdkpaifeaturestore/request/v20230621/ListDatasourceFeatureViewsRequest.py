09595bb3b137746af7052dd3fb3b3ec3837174af
sdk-team <sdk-team@alibabacloud.com>
2025-07-17 04:09:03 +0000

    Update GetInstance and ListInstances.

---
+++ aliyun-python-sdk-paifeaturestore/aliyunsdkpaifeaturestore/request/v20230621/ListDatasourceFeatureViewsRequest.py
@@ -0,0 +1,104 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RoaRequest
+
+class ListDatasourceFeatureViewsRequest(RoaRequest):
+
+	def __init__(self):
+		RoaRequest.__init__(self, 'PaiFeatureStore', '2023-06-21', 'ListDatasourceFeatureViews')
+		self.set_protocol_type('https')
+		self.set_uri_pattern('/api/v1/instances/[InstanceId]/datasources/[DatasourceId]/featureviews')
+		self.set_method('GET')
+
+	def get_Type(self): # String
+		return self.get_query_params().get('Type')
+
+	def set_Type(self, Type):  # String
+		self.add_query_param('Type', Type)
+	def get_StartDate(self): # String
+		return self.get_query_params().get('StartDate')
+
+	def set_StartDate(self, StartDate):  # String
+		self.add_query_param('StartDate', StartDate)
+	def get_PageNumber(self): # Integer
+		return self.get_query_params().get('PageNumber')
+
+	def set_PageNumber(self, PageNumber):  # Integer
+		self.add_query_param('PageNumber', PageNumber)
+	def get_PageSize(self): # Integer
+		return self.get_query_params().get('PageSize')
+
+	def set_PageSize(self, PageSize):  # Integer
+		self.add_query_param('PageSize', PageSize)
+	def get_ProjectId(self): # String
+		return self.get_query_params().get('ProjectId')
+
+	def set_ProjectId(self, ProjectId):  # String
+		self.add_query_param('ProjectId', ProjectId)
+	def get_Order(self): # String
+		return self.get_query_params().get('Order')
+
+	def set_Order(self, Order):  # String
+		self.add_query_param('Order', Order)
+	def get_All(self): # Boolean
+		return self.get_query_params().get('All')
+
+	def set_All(self, All):  # Boolean
+		self.add_query_param('All', All)
+	def get_ProjectName(self): # String
+		return self.get_query_params().get('ProjectName')
+
+	def set_ProjectName(self, ProjectName):  # String
+		self.add_query_param('ProjectName', ProjectName)
+	def get_ShowStorageUsage(self): # Boolean
+		return self.get_query_params().get('ShowStorageUsage')
+
+	def set_ShowStorageUsage(self, ShowStorageUsage):  # Boolean
+		self.add_query_param('ShowStorageUsage', ShowStorageUsage)
+	def get_Verbose(self): # Boolean
+		return self.get_query_params().get('Verbose')
+
+	def set_Verbose(self, Verbose):  # Boolean
+		self.add_query_param('Verbose', Verbose)
+	def get_InstanceId(self): # String
+		return self.get_path_params().get('InstanceId')
+
+	def set_InstanceId(self, InstanceId):  # String
+		self.add_path_param('InstanceId', InstanceId)
+	def get_EndDate(self): # String
+		return self.get_query_params().get('EndDate')
+
+	def set_EndDate(self, EndDate):  # String
+		self.add_query_param('EndDate', EndDate)
+	def get_DatasourceId(self): # String
+		return self.get_path_params().get('DatasourceId')
+
+	def set_DatasourceId(self, DatasourceId):  # String
+		self.add_path_param('DatasourceId', DatasourceId)
+	def get_Name(self): # String
+		return self.get_query_params().get('Name')
+
+	def set_Name(self, Name):  # String
+		self.add_query_param('Name', Name)
+	def get_SortBy(self): # String
+		return self.get_query_params().get('SortBy')
+
+	def set_SortBy(self, SortBy):  # String
+		self.add_query_param('SortBy', SortBy)
