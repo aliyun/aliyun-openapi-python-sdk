2f98ef31364ffa069bf64f0a1b19a6dd6f2e5ae9
sdk-team <sdk-team@alibabacloud.com>
2025-07-29 11:41:45 +0000

    Add DataService OpenAPIs.

---
+++ aliyun-python-sdk-dataphin-public/aliyunsdkdataphin_public/request/v20230630/GetDataServiceApiCallTrendRequest.py
@@ -0,0 +1,48 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+
+class GetDataServiceApiCallTrendRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'dataphin-public', '2023-06-30', 'GetDataServiceApiCallTrend')
+		self.set_protocol_type('https')
+		self.set_method('POST')
+
+	def get_OpTenantId(self): # Long
+		return self.get_query_params().get('OpTenantId')
+
+	def set_OpTenantId(self, OpTenantId):  # Long
+		self.add_query_param('OpTenantId', OpTenantId)
+	def get_EndTime(self): # String
+		return self.get_query_params().get('EndTime')
+
+	def set_EndTime(self, EndTime):  # String
+		self.add_query_param('EndTime', EndTime)
+	def get_StartTime(self): # String
+		return self.get_query_params().get('StartTime')
+
+	def set_StartTime(self, StartTime):  # String
+		self.add_query_param('StartTime', StartTime)
+	def get_ProjectId(self): # Integer
+		return self.get_query_params().get('ProjectId')
+
+	def set_ProjectId(self, ProjectId):  # Integer
+		self.add_query_param('ProjectId', ProjectId)
